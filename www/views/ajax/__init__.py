e2ad6d84fad275a039f911d0088a17cd0262cb12
zhangbin <elviszhang2002@gmail.com>
2015-06-09 13:14:08 +0800

    引入账户类

--- www/views/ajax/__init__.py
+++ www/views/ajax/__init__.py
@@ -1,4 +1,5 @@
 from perms import ServiceIdentity, TenantIdentity, InviteServiceUser
 from service_actions import *
+from account_actions import *
 
 __all__ = ('ServiceIdentity', 'InviteServiceUser', 'TenantIdentity')
