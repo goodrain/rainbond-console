6fdf4d5b8002accc5669228a1fb4b01546537970
zhangbin <elviszhang2002@gmail.com>
2015-06-10 15:38:25 +0800

    中文问题

--- www/utils/mail.py
+++ www/utils/mail.py
@@ -7,25 +7,5 @@ def send_invite_mail_withHtml(to, html_content):
     send_mail(u'\u6fc0\u6d3b\u4f60\u7684\u597d\u96e8\u4e91\u5e73\u53f0\u8d26\u53f7', "", 'no-reply@goodrain.com', [to], fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=html_content)
     
 def create_invite_mail_content(link_url):
-    content = u"尊敬的用户您好，"
-    content = content + "<br/>"
-    content = content + u"非常感谢您申请试用 好雨云平台！ 请点击下面的链接完成注册:"
-    content = content + "<br/>"
-    content = content + u"注册链接: <a target='_blank' href=" + link_url + ">注册好友云平台</a>"
-    content = content + "<br/>"
-    content = content + u"我们的服务在一定的资源范围内永久免费！内测阶段也可以申请增加免费资源，增加的资源在产品正式版上线后也不会另收费用哦！另外参与内测并提交问题报告的用户，正式上线后还会有更多的福利。"
-    content = content + "<br/>"
-    content = content + u"我们的文档及博客正在建设中，以后会陆续发布一系列好雨云平台的使用教程和技巧，欢迎关注！"
-    content = content + "<br/>"
-    content = content + u"您在使用过程中遇到的任何问题，或者对平台有任何建议，都可以通过以下途径提交反馈。对于提出高质量的反馈的用户，还有精美礼品等待您！"
-    content = content + "<br/>"
-    content = content + "Email： ares@goodrain.com"
-    content = content + "<br/>"
-    content = content + u"微信公众号：goodrain-cloud "
-    content = content + "<br/>"
-    content = content + u"联系电话：13621236261"
-    content = content + "<br/>"
-    content = content + u"再次感谢您关注我们的产品！"
-    content = content + "<br/>"
-    content = content + u"好雨科技 (Goodrain Inc.) CEO 刘凡"
+    content = "test"
     return content
