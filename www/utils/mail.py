b1c606a8b66fd5d8c9fc823fa30f6f7705fc1f33
zhangbin <elviszhang2002@gmail.com>
2015-06-10 10:32:54 +0800

    邀请邮件

--- www/utils/mail.py
+++ www/utils/mail.py
@@ -2,4 +2,4 @@ from django.core.mail import send_mail
 
 
 def send_invite_mail(to, content):
-    send_mail('Invite mail', content, 'no-reply@goodrain.com', [to])
+    send_mail('激活你的好雨云平台账号', content, 'no-reply@goodrain.com', [to])
--- www/views/ajax/perms.py
+++ www/views/ajax/perms.py
@@ -116,10 +116,34 @@ class InviteTenantUser(AuthedView):
         self.tenant_pk = Tenants.objects.get(tenant_name=self.tenantName).pk
         pass
 
+
     def invite_content(self, email, tenant_name, identity):
         domain = self.request.META.get('HTTP_HOST')
         mail_body = AuthCode.encode(','.join([email, tenant_name, identity]), 'goodrain')
-        return 'http://{0}/invite?key={1}'.format(domain, mail_body)
+        link_url = 'http://{0}/invite?key={1}'.format(domain, mail_body)
+                
+        content = "尊敬的用户您好，"
+        content = content + "<br/>"
+        content = content + "非常感谢您申请试用 好雨云平台！ 请点击下面的链接完成注册:"
+        content = content + "<br/>"
+        content = content + "注册链接: " + link_url
+        content = content + "<br/>"
+        content = content + "我们的服务在一定的资源范围内永久免费！内测阶段也可以申请增加免费资源，增加的资源在产品正式版上线后也不会另收费用哦！另外参与内测并提交问题报告的用户，正式上线后还会有更多的福利。"
+        content = content + "<br/>"
+        content = content + "我们的文档及博客正在建设中，以后会陆续发布一系列好雨云平台的使用教程和技巧，欢迎关注！"
+        content = content + "<br/>"
+        content = content + "您在使用过程中遇到的任何问题，或者对平台有任何建议，都可以通过以下途径提交反馈。对于提出高质量的反馈的用户，还有精美礼品等待您！"
+        content = content + "<br/>"
+        content = content + "Email： ares@goodrain.com"
+        content = content + "<br/>"
+        content = content + "微信公众号：goodrain-cloud "
+        content = content + "<br/>"
+        content = content + "联系电话：13621236261"
+        content = content + "<br/>"
+        content = content + "再次感谢您关注我们的产品！"
+        content = content + "<br/>"
+        content = content + "好雨科技 (Goodrain Inc.) CEO 刘凡"
+        return content
 
     @perm_required('tenant.perm_setting')
     def post(self, request, *args, **kwargs):
@@ -137,8 +161,8 @@ class InviteTenantUser(AuthedView):
                 PermRelTenant.objects.create(user_id=user.user_id, tenant_id=self.tenant_pk, identity=identity)
                 result['desc'] = u"已向{0}授权".format(user.nick_name)
         except Users.DoesNotExist:
-            #user = Users.objects.create(email=email, password='unset', is_active=False)
-            #PermRelTenant.objects.create(user_id=user.user_id, tenant_id=self.tenant_pk, identity=identity)
+            # user = Users.objects.create(email=email, password='unset', is_active=False)
+            # PermRelTenant.objects.create(user_id=user.user_id, tenant_id=self.tenant_pk, identity=identity)
             send_invite_mail(email, self.invite_content(email, self.tenantName))
             result['desc'] = u'已向{0}发送邀请邮件'.format(email)
 
