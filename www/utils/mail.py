75221885e208913d6694a06b5ddfab66acf64c32
zhangbin <elviszhang2002@gmail.com>
2015-06-10 14:33:08 +0800

    debug

--- www/utils/mail.py
+++ www/utils/mail.py
@@ -1,5 +1,9 @@
 from django.core.mail import send_mail
 
+import sys
+reload(sys)
+sys.setdefaultencoding('UTF-8')
+
 class MailUtil(object):
             
     def get_inviteContent(self, link_url):
@@ -30,5 +34,5 @@ class MailUtil(object):
         send_mail("invite user", content, 'no-reply@goodrain.com', [to])
         
     def send_invite_mail_withHtml(to, html_content):
-        send_mail(u'\u6fc0\u6d3b\u4f60\u7684\u597d\u96e8\u4e91\u5e73\u53f0\u8d26\u53f7', "", 'no-reply@goodrain.com', [to], fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=html_content)
+        send_mail('test', "", 'no-reply@goodrain.com', [to], fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=html_content)
     
