2f95abab97097be39bed2bc3123b23f3e31024e3
zhangbin <elviszhang2002@gmail.com>
2015-06-10 11:50:04 +0800

    邮件标题

--- www/utils/mail.py
+++ www/utils/mail.py
@@ -2,4 +2,4 @@ from django.core.mail import send_mail
 
 
 def send_invite_mail(to, content):
-    send_mail(u"激活你的好雨云平台账号", content, 'no-reply@goodrain.com', [to])
+    send_mail("invite user", content, 'no-reply@goodrain.com', [to])
