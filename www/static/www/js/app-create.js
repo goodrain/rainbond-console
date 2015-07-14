4a18b87b8d5e3f65eed73ffed9985cfc5087f7e1
wangjj <wangjiajun33wjj@126.com>
2015-07-14 16:50:58 +0800

    创建应用修改

--- www/static/www/js/app-create.js
+++ www/static/www/js/app-create.js
@@ -159,8 +159,8 @@ function loadRepos(_url){
                                 projectVersion(service_code_from,service_code_id,clone_url);
                            }
                            // $('#create_codestore_notice').removeClass('alert-danger').addClass('alert-info').html("正在读取项目分支信息");
-                           // $('#create_codestore_notice').slideDown();
                        }
+                        $('#create_codestore_notice').slideUp();
                    }
                });           
              }else{
