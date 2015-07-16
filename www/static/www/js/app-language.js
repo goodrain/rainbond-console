dd5acc69ff9f918962ff0f19963623c7983aee6b
goodrain <thor@goodrain.com>
2015-07-16 11:06:00 +0800

    node.js 提交判断

--- www/static/www/js/app-language.js
+++ www/static/www/js/app-language.js
@@ -7,6 +7,14 @@ $(function() {
 				if(this.checked)
 					checkedValue= checkedValue +","+$(this).val() 
 			});
+			var language = $('#language').val();
+			if(language=="Node.js"){
+				var service_server = $('#service_server').val();
+				if(service_server==""){
+					alert("启动命令不能为空")
+					return false;
+				}
+			}
 			$("#language_btn").attr('disabled', "true")
 			var tenantName = $('#tenantName').val();
 			var service_name = $('#service_name').val();
