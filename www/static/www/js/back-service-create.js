a450fa923b9dca8947f7bfac9500bfd947921873
goodrain <thor@goodrain.com>
2015-09-06 18:34:42 +0800

    update

--- www/static/www/js/back-service-create.js
+++ www/static/www/js/back-service-create.js
@@ -66,21 +66,16 @@ $(function(){
     			var dataObj = msg;
     			if (dataObj["status"] == "notexist"){
     				swal("所选的服务类型不存在");
-    				$("#back_service_finished").removeAttr('disabled')
     			} else if (dataObj["status"] == "owed"){
     				swal("余额不足请及时充值")
     			} else if (dataObj["status"] == "exist") {
     				swal("服务名已存在");
-    				$("#back_service_finished").removeAttr('disabled')
     			} else if (dataObj["status"] == "over_memory") {
     				swal("免费资源已达上限，不能创建");
-    				$("#back_service_finished").removeAttr('disabled')
     			} else if (dataObj["status"] == "over_money") {
     				swal("余额不足，不能创建");
-    				$("#back_service_finished").removeAttr('disabled')
     			} else if (dataObj["status"] == "empty") {
-    				swal("服务名称不能为空");
-    				$("#back_service_finished").removeAttr('disabled')
+    				swal("服务名称不能为空");    				
     			}else if (dataObj["status"] == "success") {
     				service_alias = dataObj["service_alias"]
     				window.location.href = "/apps/" + tenantName + "/" + service_alias + "/detail/";
@@ -88,6 +83,7 @@ $(function(){
     				swal("创建失败");
     				$("#back_service_finished").removeAttr('disabled')
                 }
+    			$("#back_service_finished").removeAttr('disabled')
     		},
     		error : function() {
     			swal("系统异常,请重试");
