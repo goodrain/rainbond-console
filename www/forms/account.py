dc4c9b72e7763fa42af84df69ef18892ae08058f
goodrain <thor@goodrain.com>
2015-09-07 10:00:54 +0800

    新增aws-bj-1

--- www/forms/account.py
+++ www/forms/account.py
@@ -261,7 +261,8 @@ class RegisterForm(forms.Form):
         label="",
         choices=(
             ('1', '机房'),
-            ('ucloud_bj_1', 'UCloud北京')
+            ('ucloud_bj_1', 'UCloud北京'),
+            ('aws-bj-1', 'Amazon北京'),
         ),
         widget=forms.Select
     )
