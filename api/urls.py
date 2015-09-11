026b92c4d951a5b1f01c505cc890bd0269b6655e
qisy <messi@goodrain.com>
2015-09-11 10:10:39 +0800

    支持token

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url, include
-
+from rest_framework.authtoken import views
 from api.views.services import SelectedServiceView
 from api.views.tenants.services import TenantServiceStaticsView, TenantHibernateView, TenantCloseRestartView, TenantView, AllTenantView
 
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     url(r'^tenants/services/close-restart$', TenantCloseRestartView.as_view()),
     url(r'^tenants/member$', TenantView.as_view()),
     url(r'^tenants/all-members$', AllTenantView.as_view()),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth', views.obtain_auth_token),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 )
