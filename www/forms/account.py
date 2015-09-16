2ea94df7a7a2d4fd8abdc43794aa9f23221ab4bd
qisy <messi@goodrain.com>
2015-09-16 17:57:52 +0800

    add yamaxun

--- www/forms/account.py
+++ www/forms/account.py
@@ -285,8 +285,8 @@ class RegisterForm(forms.Form):
     machine_region = forms.ChoiceField(
         label="",
         choices=(
-            ('ucloud-bj-1', 'Ucloud[北京]'),
-            #('aws-bj-1', '亚马逊[北京]'),
+            #('ucloud-bj-1', 'Ucloud[北京]'),
+            ('aws-bj-1', '亚马逊[北京]'),
             ('0', {'label': 'DigitalOcean[新加坡](正在建设)', 'disabled': True}),
         ),
         initial='1',
