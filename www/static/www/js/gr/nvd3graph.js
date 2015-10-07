b504e4c4d89f04d0f5c21136590e9235737bea93
qisy <messi@goodrain.com>
2015-10-07 11:11:04 +0800

    增加周期选择

--- www/static/www/js/gr/nvd3graph.js
+++ www/static/www/js/gr/nvd3graph.js
@@ -3,38 +3,34 @@
     var tenantName = $('#mytags').attr('tenant');
     var serviceAlias = $('#mytags').attr('service');
 
-    $('.graph').each(function() {
-      var graph_id = $(this).attr('id');
-        $.ajax({
-          url: '/ajax/' + tenantName + '/' + serviceAlias + '/graph',
-          method: "POST",
-          data: {"csrfmiddlewaretoken":csrftoken, "graph_id":graph_id},
-          success: function (event) {
-              makeChart(graph_id, event);
-          },
-              
-          statusCode: {
-            403: function(event) {
-              swal("你没有此权限！");
-            }
-          },
-              
-        });
+    default_start = $('#graph-period').children('option:selected').val();
+    getGraphs(default_start);
+
+    $('#graph-period').on('change',function() {
+          var start = $(this).children('option:selected').val();
+          getGraphs(start);
     });
 
-    /*var data1 = [
-      {
-        "key": "memory",
-        "values": [[1443431546000, 852], [1443431576000, 109], [1443431606000, 225], [1443431636000, 485], [1443431666000, 830], [1443431696000, 246], [1443431726000, 931], [1443431756000, 724], [1443431786000, 264], [1443431816000, 901], [1443431846000, 418], [1443431876000, 877], [1443431906000, 453], [1443431936000, 127], [1443431966000, 137], [1443431996000, 440], [1443432026000, 595], [1443432056000, 184], [1443432086000, 945], [1443432116000, 300], [1443432146000, 111], [1443432176000, 882], [1443432206000, 232], [1443432236000, 184], [1443432266000, 664], [1443432296000, 327], [1443432326000, 272], [1443432356000, 728], [1443432386000, 408], [1443432416000, 358]]
-      }
-    ]
-
-    var data2 = [
-      {
-        "key": "disk",
-        "values": [[1443431546000, 142], [1443431576000, 120], [1443431606000, 104], [1443431636000, 104], [1443431666000, 101], [1443431696000, 139], [1443431726000, 111], [1443431756000, 127], [1443431786000, 145], [1443431816000, 108], [1443431846000, 119], [1443431876000, 144], [1443431906000, 106], [1443431936000, 146], [1443431966000, 143], [1443431996000, 111], [1443432026000, 142], [1443432056000, 149], [1443432086000, 114], [1443432116000, 145], [1443432146000, 132], [1443432176000, 128], [1443432206000, 108], [1443432236000, 121], [1443432266000, 110], [1443432296000, 113], [1443432326000, 111], [1443432356000, 112], [1443432386000, 132], [1443432416000, 140]]
-      },
-    ]*/
+    function getGraphs(start) {
+      $('.graph').each(function() {
+        var graph_id = $(this).attr('id');
+          $.ajax({
+            url: '/ajax/' + tenantName + '/' + serviceAlias + '/graph',
+            method: "POST",
+            data: {"csrfmiddlewaretoken":csrftoken, "graph_id":graph_id, "start": start},
+            success: function (event) {
+                makeChart(graph_id, event);
+            },
+                
+            statusCode: {
+              403: function(event) {
+                swal("你没有此权限！");
+              }
+            },
+                
+          });
+      });
+    };
 
     function makeChart(graph_id, event) {
       nv.addGraph(function() {
--- www/views/ajax/graph.py
+++ www/views/ajax/graph.py
@@ -42,13 +42,13 @@ class ServiceGraph(AuthedView):
             data['values'].append([increase_time(i), randint(100, 1000)])
         return [data]
 
-    def get_tsdb_data(self, graph_key):
+    def get_tsdb_data(self, graph_key, start):
         data = {"key": graph_key, "values": []}
         metric = self.metric_map.get(graph_key, None)
 
         if metric is not None:
             query_data = self.tsdb_client.query(
-                self.tenant.region, metric, tenant=self.tenant.tenant_name, service=self.service.service_alias)
+                self.tenant.region, metric, start=start, tenant=self.tenant.tenant_name, service=self.service.service_alias)
 
             if query_data is None:
                 return None
@@ -63,13 +63,14 @@ class ServiceGraph(AuthedView):
     @perm_required('view_service')
     def post(self, request, *args, **kwargs):
         graph_id = request.POST.get('graph_id', None)
+        start = request.POST.get('start', None)
         if graph_id is None:
             return JsonResponse({"ok": False, "info": "need graph_id filed"}, status=500)
         else:
             graph_key = graph_id.replace('-stat', '')
             result = self.template.copy()
             result['data'] = self.random_data(graph_key)
-            data = self.get_tsdb_data(graph_key)
+            data = self.get_tsdb_data(graph_key, start)
             if data is not None:
                 result['data'] = data
                 return JsonResponse(result, status=200)
