b07f417bf3df60ef4507c5e0361325b2be8da745
qisy <messi@goodrain.com>
2015-10-19 14:55:05 +0800

    冗余参数

--- goodrain_web/base.py
+++ goodrain_web/base.py
@@ -54,14 +54,16 @@ class BaseHttpClient(object):
         else:
             return res, body
 
-    def _request(self, url, method, headers=None, body=None):
+    def _request(self, url, method, headers=None, body=None, client=None, *args, **kwargs):
         retry_count = 2
+        if client is None:
+            client = self.http
         while retry_count:
             try:
                 if body is None:
-                    response, content = self.http.request(url, method, headers=headers)
+                    response, content = client.request(url, method, headers=headers)
                 else:
-                    response, content = self.http.request(url, method, headers=headers, body=body)
+                    response, content = client.request(url, method, headers=headers, body=body)
 
                 if len(content) > 1000:
                     record_content = '%s  .....ignore.....' % content[:1000]
@@ -87,31 +89,31 @@ class BaseHttpClient(object):
                     logger.exception('client_error', e)
                     raise self.ApiSocketError(self.apitype, url, method, Dict({"status": 101}), {"type": "connect error", "error": str(e)})
 
-    def _get(self, url, headers):
-        response, content = self._request(url, 'GET', headers=headers)
+    def _get(self, url, headers, *args, **kwargs):
+        response, content = self._request(url, 'GET', headers=headers, *args, **kwargs)
         res, body = self._check_status(url, 'GET', response, content)
         return res, body
 
-    def _post(self, url, headers, body=None):
+    def _post(self, url, headers, body=None, *args, **kwargs):
         if body is not None:
-            response, content = self._request(url, 'POST', headers=headers, body=body)
+            response, content = self._request(url, 'POST', headers=headers, body=body, *args, **kwargs)
         else:
-            response, content = self._request(url, 'POST', headers=headers)
+            response, content = self._request(url, 'POST', headers=headers, *args, **kwargs)
         res, body = self._check_status(url, 'POST', response, content)
         return res, body
 
-    def _put(self, url, headers, body=None):
+    def _put(self, url, headers, body=None, *args, **kwargs):
         if body is not None:
-            response, content = self._request(url, 'PUT', headers=headers, body=body)
+            response, content = self._request(url, 'PUT', headers=headers, body=body, *args, **kwargs)
         else:
-            response, content = self._request(url, 'PUT', headers=headers)
+            response, content = self._request(url, 'PUT', headers=headers, *args, **kwargs)
         res, body = self._check_status(url, 'PUT', response, content)
         return res, body
 
-    def _delete(self, url, headers, body=None):
+    def _delete(self, url, headers, body=None, *args, **kwargs):
         if body is not None:
-            response, content = self._request(url, 'DELETE', headers=headers, body=body)
+            response, content = self._request(url, 'DELETE', headers=headers, body=body, *args, **kwargs)
         else:
-            response, content = self._request(url, 'DELETE', headers=headers)
+            response, content = self._request(url, 'DELETE', headers=headers, *args, **kwargs)
         res, body = self._check_status(url, 'DELETE', response, content)
         return res, body
--- www/service_http.py
+++ www/service_http.py
@@ -1,7 +1,7 @@
 import json
 from django.conf import settings
 
-from goodrain_web.base import BaseHttpClient
+from goodrain_web.base import BaseHttpClient, httplib2
 
 import logging
 
@@ -17,153 +17,164 @@ class RegionServiceApi(BaseHttpClient):
         self.region_map = {}
         region_service_infos = settings.REGION_SERVICE_API
         for region_service_info in region_service_infos:
-            self.region_map[region_service_info["region_name"]] = region_service_info["url"]
+            client_info = {"url": region_service_info["url"]}
+            if 'proxy' in region_service_info:
+                client_info['client'] = self.make_proxy_http(region_service_info)
+            else:
+                client_info['client'] = httplib2.Http(timeout=25)
+            self.region_map[region_service_info["region_name"]] = client_info
+
+    def _request(self, *args, **kwargs):
+        region = kwargs.get('region')
+        client = self.region_map[region]['client']
+        response, content = super(RegionServiceApi, self)._request(client=client, *args, **kwargs)
+        return response, content
 
     def create_service(self, region, tenant, body):
-        url = self.region_map[region] + "/v1/tenants/" + tenant + "/services"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/tenants/" + tenant + "/services"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def update_service(self, region, service_id, data):
-        url = self.region_map[region] + "/v1/services/" + service_id
-        res, body = self._put(url, self.default_headers, json.dumps(data))
+        url = self.region_map[region]['url'] + "/v1/services/" + service_id
+        res, body = self._put(url, self.default_headers, json.dumps(data), region=region)
         return res, body
 
     def build_service(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/build/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/build/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def check_service_status(self, region, service_id):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/status/"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/status/"
+        res, body = self._post(url, self.default_headers, region=region)
         return body
 
     def restart(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/restart/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/restart/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def stop(self, region, service_id):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/stop/"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/stop/"
+        res, body = self._post(url, self.default_headers, region=region)
         return body
 
     def delete(self, region, service_id):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/delete/"
-        res, body = self._delete(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/delete/"
+        res, body = self._delete(url, self.default_headers, region=region)
         return body
 
     def check_status(self, region, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/status/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/status/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def get_log(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/log/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/log/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def get_userlog(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/userlog/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/userlog/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def verticalUpgrade(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/vertical/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/vertical/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def horizontalUpgrade(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/horizontal/"
-        res, body = self._put(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/horizontal/"
+        res, body = self._put(url, self.default_headers, body, region=region)
         return body
 
     def addUserDomain(self, region, body):
-        url = self.region_map[region] + "/v1/lb/user-domains"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/lb/user-domains"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def deleteUserDomain(self, region, body):
-        url = self.region_map[region] + "/v1/lb/delete-domains-rule"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/lb/delete-domains-rule"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def changeMemory(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/" + service_id + "/language"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/" + service_id + "/language"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def pause(self, region, tenant_id):
-        url = self.region_map[region] + "/v1/tenants/" + tenant_id + "/pause"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/tenants/" + tenant_id + "/pause"
+        res, body = self._post(url, self.default_headers, region=region)
         return body
 
     def unpause(self, region, tenant_id):
-        url = self.region_map[region] + "/v1/tenants/" + tenant_id + "/unpause"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/tenants/" + tenant_id + "/unpause"
+        res, body = self._post(url, self.default_headers, region=region)
         return body
 
     def writeToRegionBeanstalk(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/beanstalk/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/beanstalk/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def createServiceDependency(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/dependency/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/dependency/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def cancelServiceDependency(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/dependency/"
-        res, body = self._put(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/dependency/"
+        res, body = self._put(url, self.default_headers, body, region=region)
         return body
 
     def createServiceEnv(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/env-var/"
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/env-var/"
         logger.debug("api.region", "function: {0}, {1}".format('createServiceEnv', url))
-        res, body = self._post(url, self.default_headers, body)
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def getTenantRunningServiceId(self, region, tenant_id):
-        url = self.region_map[region] + "/v1/tenants/" + tenant_id + "/running-service"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/tenants/" + tenant_id + "/running-service"
+        res, body = self._post(url, self.default_headers, region=region)
         return body["data"]
 
     def updateTenantServiceStatus(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/status-update/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/status-update/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body["old_status"]
 
     def systemPause(self, region, tenant_id):
-        url = self.region_map[region] + "/v1/tenants/" + tenant_id + "/system-pause"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/tenants/" + tenant_id + "/system-pause"
+        res, body = self._post(url, self.default_headers, region=region)
         return body
 
     def systemUnpause(self, region, tenant_id):
-        url = self.region_map[region] + "/v1/tenants/" + tenant_id + "/system-unpause"
-        res, body = self._post(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/tenants/" + tenant_id + "/system-unpause"
+        res, body = self._post(url, self.default_headers, region=region)
         return body
 
     def modifyServiceProtocol(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/port-mapping/"
-        res, body = self._post(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/port-mapping/"
+        res, body = self._post(url, self.default_headers, body, region=region)
         return body
 
     def findMappingPort(self, region, service_id):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/port-mapping/"
-        res, body = self._get(url, self.default_headers)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/port-mapping/"
+        res, body = self._get(url, self.default_headers, region=region)
         return body
 
     def bindingMappingPortIp(self, region, service_id, body):
-        url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/port-mapping/"
-        res, body = self._put(url, self.default_headers, body)
+        url = self.region_map[region]['url'] + "/v1/services/lifecycle/" + service_id + "/port-mapping/"
+        res, body = self._put(url, self.default_headers, body, region=region)
         return body
 
     def opentsdbQuery(self, region, start, queries):
-        url = self.region_map[region] + "/v1/statistic/opentsdb/query"
+        url = self.region_map[region]['url'] + "/v1/statistic/opentsdb/query"
         data = {"start": start, "queries": queries}
-        res, body = self._post(url, self.default_headers, json.dumps(data))
+        res, body = self._post(url, self.default_headers, json.dumps(data), region=region)
         try:
             dps = body[0]['dps']
             return dps
@@ -173,12 +184,12 @@ class RegionServiceApi(BaseHttpClient):
             return None
 
     def get_tenants(self, region):
-        url = self.region_map[region] + '/v1/tenants'
-        res, body = self._get(url, self.default_headers)
+        url = self.region_map[region]['url'] + '/v1/tenants'
+        res, body = self._get(url, self.default_headers, region=region)
         return res, body
 
     def create_tenant(self, region, tenant_name, tenant_id):
-        url = self.region_map[region] + '/v1/tenants'
+        url = self.region_map[region]['url'] + '/v1/tenants'
         data = {"tenant_id": tenant_id, "tenant_name": tenant_name}
-        res, body = self._post(url, self.default_headers, json.dumps(data))
+        res, body = self._post(url, self.default_headers, json.dumps(data), region=region)
         return res, body
