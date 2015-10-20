0f11ff35e37398ff47e28254ce73cd3d38a7ffb4
goodrain <thor@goodrain.com>
2015-10-20 17:04:15 +0800

    merge

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url, include
 from rest_framework.authtoken import views
 from api.views.services import SelectedServiceView, ServiceEnvVarView
 from api.views.tenants.services import TenantServiceStaticsView, TenantHibernateView, TenantView, AllTenantView, GitCheckCodeView
+from api.views.tenants import move
 
 urlpatterns = patterns(
     '',
@@ -10,7 +11,11 @@ urlpatterns = patterns(
     url(r'^tenants/services/hibernate$', TenantHibernateView.as_view()),
     url(r'^tenants/member$', TenantView.as_view()),
     url(r'^tenants/all-members$', AllTenantView.as_view()),
-    url(r'^tenants/services/codecheck', GitCheckCodeView.as_view()),
+    url(r'^services/rsync/env$', ServiceEnvVarView.as_view()),
+    url(r'^tenants/(?P<tenantId>[a-z0-9\-]+)/move/stop_prepare$', move.TenantStopView.as_view()),
+    url(r'^tenants/(?P<tenantId>[a-z0-9\-]+)/move/start$', move.TenantStartView.as_view()),
+    url(r'^tenants/(?P<tenantId>[a-z0-9\-]+)/move/follow_up$', move.TenantFollowUpView.as_view()),
+    url(r'^tenants/(?P<tenantId>[a-z0-9\-]+)/move/update$', move.TenantMoveUpdateView.as_view()),
     # url(r'^services/rsync/env$', ServiceEnvVarView.as_view()),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth', views.obtain_auth_token),
