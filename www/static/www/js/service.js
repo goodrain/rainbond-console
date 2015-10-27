9ab9af3a86848048faafda115931af756bf1bcc3
goodrain <thor@goodrain.com>
2015-10-27 17:57:05 +0800

    js修改

--- www/static/www/js/service.js
+++ www/static/www/js/service.js
@@ -32,7 +32,7 @@ function service_oneKeyDeploy(categroy, serviceAlias, tenantName, isreload) {
 						+ "/detail/"
 				window.open(forurl, target = "_parent")
 			} else if (dataObj["status"] == "often") {
-				swal("上次部署正在进行中，请稍后再试")
+				swal("部署正在进行中，请稍后")
 			} else if (dataObj["status"] == "over_memory") {
 				swal("免费资源已达上限，不能升级")
 			} else if (dataObj["status"] == "over_money") {
@@ -76,7 +76,7 @@ function service_my_onOperation(service_id, service_alias, tenantName) {
 			if (dataObj["status"] == "success") {
 				swal("操作成功")
 			} else if (dataObj["status"] == "often") {
-				swal("上次操作正在进行中，稍后再试")
+				swal("操作正在进行中，请稍后")
 			} else if (dataObj["status"] == "owed") {
 				swal("余额不足请及时充值")
 			} else if (dataObj["status"] == "over_memory") {
@@ -117,7 +117,7 @@ function service_onOperation(service_id, service_alias, tenantName) {
 			if (dataObj["status"] == "success") {
 				swal("操作成功")
 			} else if (dataObj["status"] == "often") {
-				swal("上次操作正在进行中，稍后再试")
+				swal("操作正在进行中，请稍后")
 			} else if (dataObj["status"] == "owed") {
 				swal("余额不足请及时充值")
 			} else if (dataObj["status"] == "over_memory") {
@@ -201,7 +201,7 @@ function service_upgrade(tenantName, service_alias) {
 			} else if (dataObj["status"] == "owed") {
 				swal("余额不足请及时充值")
 			} else if (dataObj["status"] == "often") {
-				swal("上次操作正在进行中，稍后再试")
+				swal("操作正在进行中，请稍后")
 			} else if (dataObj["status"] == "over_memory") {
 				swal("免费资源已达上限，不能升级")
 			} else if (dataObj["status"] == "over_money") {
@@ -236,7 +236,7 @@ function app_upgrade(tenantName, service_alias) {
 				} else if (dataObj["status"] == "owed") {
 					swal("余额不足请及时充值")
 				} else if (dataObj["status"] == "often") {
-					swal("上次操作正在进行中，稍后再试")
+					swal("操作正在进行中，请稍后")
 				} else if (dataObj["status"] == "over_memory") {
 					swal("免费资源已达上限，不能升级")
 				} else if (dataObj["status"] == "over_money") {
@@ -340,7 +340,7 @@ function service_protocol(opt_type, action, tenantName, service_alias) {
 					window.location.href = window.location.href
 				}
 			} else if (dataObj["status"] == "often") {
-				swal("上次操作正在进行中，稍后再试")
+				swal("操作正在进行中，请稍后")
 			} else if (dataObj["status"] == "owed") {
 				swal("余额不足请及时充值")
 			} else if (dataObj["status"] == "over_memory") {
