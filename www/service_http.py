4ad3e39c6cfdf50f36b1edba4a21384cf8c9ab25
goodrain <thor@goodrain.com>
2015-10-28 15:22:22 +0800

    debug

--- www/service_http.py
+++ www/service_http.py
@@ -74,7 +74,7 @@ class RegionServiceApi(BaseHttpClient):
         res, body = self._post(url, self.default_headers)
         return body
     
-    def get_compile_log(self, region, service_id):
+    def get_compile_log(self, region, service_id, body):
         url = self.region_map[region] + "/v1/services/lifecycle/" + service_id + "/compile-log/"
         res, body = self._post(url, self.default_headers)
         return body
