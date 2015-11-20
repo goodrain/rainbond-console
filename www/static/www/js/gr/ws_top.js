e23f9521bfa78479b1e5d2e7a4fff8a2e2a50400
qisy <messi@goodrain.com>
2015-11-20 19:57:16 +0800

    实时测试

--- www/static/www/js/gr/ws_top.js
+++ www/static/www/js/gr/ws_top.js
@@ -7,10 +7,35 @@
 
         extPushWebSocketClient.prototype = {
             onMessage : function(msg) {
-                console.log(msg)
+                event = $.parseJSON(msg);
+                console.log(event);
+                update_table(event);
             }
         }
+
         var connect = new extPushWebSocketConnect('wss://mpush-' + region + '.goodrain.com:6060/websocket');
         connect.init(new extPushWebSocketClient(), topic, "submsg", "submsg","123456789","987654321");
+
+        function update_table (event) {
+          var columns = [];
+          $('#rtm-' + event.name + ' thead th').each(function() {
+                var name = $(this).attr("name");
+                columns.push(name);
+            }
+          )
+          
+          var table_body = []
+          for (var o in event.data) {
+            table_body.push('<tr>');
+            for (var n in columns) {
+              value = event.data[o][columns[n]];
+              table_body.push('<td>' + value + '</td>')
+            }    
+            table_body.push('</tr>');
+          }
+          
+          var tbody = table_body.join("");
+          $('#rtm-' + event.name + ' tbody').html(tbody);
+        }
     });
 })(jQuery);
