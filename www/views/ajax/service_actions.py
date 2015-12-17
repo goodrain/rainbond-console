5dbc9770dbf9fd52285730f44c452ac0848baf6e
goodrain <thor@goodrain.com>
2015-12-17 17:59:37 +0800

    服务内部端口

--- www/views/ajax/service_actions.py
+++ www/views/ajax/service_actions.py
@@ -267,7 +267,6 @@ class ServiceManage(AuthedView):
                                 while assinportNum > 0:
                                     service_port = service_port + 1 
                                     assinportNum = TenantServiceEnvVar.objects.filter(tenant_id=self.tenant.tenant_id, is_change=False, attr_value=service_port).count()
-                                service_port = temport + 1
                             baseService.saveServiceEnvVar(
                                 self.tenant.tenant_id, self.service.service_id, u"连接地址", temp_key + "_HOST", "127.0.0.1", False)
                             baseService.saveServiceEnvVar(
