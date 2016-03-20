cbbb0269de14981188f969b2e20b13d350888340
qisy <messi@goodrain.com>
2016-03-20 13:19:41 +0800

    Decimal

--- www/views/ajax/account_actions.py
+++ www/views/ajax/account_actions.py
@@ -2,6 +2,7 @@
 import datetime
 import json
 from datetime import date
+from decimal import Decimal
 from dateutil.relativedelta import relativedelta
 
 from django.views.decorators.cache import never_cache
@@ -21,9 +22,11 @@ from django.template.defaultfilters import length
 logger = logging.getLogger('default')
 monitorhook = MonitorHook()
 
-RechargeTypeMap = {"alipay":u"支付宝", "100send50":u"充100送10", "weixin100":u"微信注册送100", "rechargesend":u"充多少送多少"}
+RechargeTypeMap = {"alipay": u"支付宝", "100send50": u"充100送10", "weixin100": u"微信注册送100", "rechargesend": u"充多少送多少"}
 
-class AccountBill(AuthedView):    
+
+class AccountBill(AuthedView):
+
     @perm_required('tenant_account')
     def post(self, request, *args, **kwargs):
         data = {}
@@ -52,7 +55,9 @@ class AccountBill(AuthedView):
             data["status"] = "failure"
         return JsonResponse(data, status=500)
 
+
 class AccountRecharging(AuthedView):
+
     @perm_required('tenant_account')
     def post(self, request, *args, **kwargs):
         try:
@@ -60,15 +65,15 @@ class AccountRecharging(AuthedView):
             action = request.POST["action"]
         except Exception as e:
             logger.exception(e)
-            
+
     @perm_required('tenant_account')
     def get(self, request, *args, **kwargs):
         context = self.get_context()
         context["tenantName"] = self.tenantName
-        context['serviceAlias'] = self.serviceAlias        
+        context['serviceAlias'] = self.serviceAlias
         date_scope = request.GET.get("datescope", "7")
         per_page = request.GET.get("perpage", "10")
-        page = request.GET.get("page", "1")        
+        page = request.GET.get("page", "1")
         context["date_scope"] = date_scope
         context["curpage"] = page
         context["per_page"] = per_page
@@ -82,7 +87,7 @@ class AccountRecharging(AuthedView):
                 startTime = start.strftime('%Y-%m-%d') + " 00:00:00"
                 recharges = TenantRecharge.objects.filter(tenant_id=self.tenant.tenant_id, time__range=(startTime, endTime))
             else:
-                recharges = TenantRecharge.objects.filter(tenant_id=self.tenant.tenant_id)                                        
+                recharges = TenantRecharge.objects.filter(tenant_id=self.tenant.tenant_id)
             paginator = JuncheePaginator(recharges, int(per_page))
             tenantRecharges = paginator.page(int(page))
             context["rechargeTypeMap"] = RechargeTypeMap
@@ -90,9 +95,10 @@ class AccountRecharging(AuthedView):
         except Exception as e:
             logger.exception(e)
         return TemplateResponse(self.request, "www/recharge-list.html", context)
-          
+
 
 class AccountQuery(AuthedView):
+
     @perm_required('tenant_account')
     def get(self, request, *args, **kwargs):
         context = self.get_context()
@@ -100,7 +106,7 @@ class AccountQuery(AuthedView):
         context['serviceAlias'] = self.serviceAlias
         date_scope = request.GET.get("datescope", "1")
         per_page = request.GET.get("perpage", "24")
-        page = request.GET.get("page", "1")        
+        page = request.GET.get("page", "1")
         context["datescope"] = date_scope
         context["per_page"] = per_page
         try:
@@ -123,7 +129,7 @@ class AccountQuery(AuthedView):
 
 
 class PayModelInfo(AuthedView):
-    
+
     @perm_required('tenant_account')
     def post(self, request, *args, **kwargs):
         result = {}
@@ -140,8 +146,8 @@ class PayModelInfo(AuthedView):
             if period > 0 and regions.index(region_name) >= 0:
                 feerule = settings.REGION_FEE_RULE[region_name]
                 logger.debug(feerule)
-                one1 = float(feerule["memory_money"]) * int(buy_memory) 
-                one2 = float(feerule["disk_money"]) * int(buy_disk) 
+                one1 = float(feerule["memory_money"]) * int(buy_memory)
+                one2 = float(feerule["disk_money"]) * int(buy_disk)
                 one3 = float(feerule["net_money"]) * int(buy_net)
                 onehour = one1 + one2
                 buy_money = 0
@@ -152,15 +158,16 @@ class PayModelInfo(AuthedView):
                     tmp_perod = period / 12
                 else:
                     buy_money = onehour * 24 * period * 2 * 30 + one3
-                                        
+
                 needTotalMoney = round(buy_money, 2)
                 logger.debug(needTotalMoney)
                 tenant = Tenants.objects.get(tenant_id=tenant_id)
                 if tenant.balance > needTotalMoney:
-                    tenant.balance = tenant.balance - needTotalMoney
+                    tenant.balance = tenant.balance - Decimal(needTotalMoney)
                     tenant.save()
                     logger.debug(tenant_id + "cost money" + str(needTotalMoney))
-                    TenantConsume(tenant_id=tenant_id, total_memory=int(buy_memory) * int(buy_period), cost_money=needTotalMoney, payed_money=needTotalMoney, pay_status='payed').save()
+                    TenantConsume(tenant_id=tenant_id, total_memory=int(buy_memory) * int(buy_period),
+                                  cost_money=needTotalMoney, payed_money=needTotalMoney, pay_status='payed').save()
                     statTime = datetime.datetime.now() + datetime.timedelta(hours=1)
                     start_time = statTime.strftime("%Y-%m-%d %H:00:00")
                     endTime = datetime.datetime.now() + relativedelta(months=int(buy_period))
