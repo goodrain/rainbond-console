7d9b2df5a28c74d84c4cc74a8e7677d65b373dc4
zhangbin <elviszhang2002@gmail.com>
2016-04-06 10:10:27 +0800

    日志配置

--- goodrain_web/conf/www_com.py
+++ goodrain_web/conf/www_com.py
@@ -6,7 +6,7 @@ TEMPLATE_DEBUG = False
 
 ZMQ_LOG_ADDRESS = 'tcp://10.0.1.11:9341'
 
-DEFAULT_HANDLERS = ['file_handler']
+DEFAULT_HANDLERS = ['zmq_handler']
 
 EMAIL_HOST = 'smtp.ym.163.com'
 EMAIL_PORT = 465
