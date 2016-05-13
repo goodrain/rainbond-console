e482ed049032a28abcf146c3f2a3bb474fc25737
qisy <messi@goodrain.com>
2016-05-13 17:16:56 +0800

    release简化

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gunicorn goodrain_web.wsgi --max-requests=5000 --reload --debug --workers=4 --log-file - --access-logfile - --error-logfile -
+if [ $1 = "bash" ];then
+    exec /bin/bash
+elif [ $1 = "version" ];then
+    echo $RELEASE_TAG
+else
+    gunicorn goodrain_web.wsgi --max-requests=5000 --reload --debug --workers=4 --log-file - --access-logfile - --error-logfile -
+fi
