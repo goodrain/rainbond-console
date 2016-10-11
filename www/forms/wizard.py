e664d61c1af192f07148f06d897f2262582f9f34
孙丽川 <sunlc@goodrain.com>
2016-10-11 19:05:39 +0800

    引导页面修改

--- www/forms/wizard.py
+++ www/forms/wizard.py
@@ -131,7 +131,7 @@ class AdminForm(forms.Form):
                 Field('password', css_class="form-control", placeholder=u"请输入至少8位数密码"),
                 Field('password_repeat', css_class="form-control", placeholder=u"请再输入一次密码"),
                 HTML("""<div class="form-group" style="text-align:left;"><a href="http://doc.goodrain.com/cloudbang-agreement/201656" target="_blank">《云帮社区版用户服务协议》</a></div>"""),
-                FormActions(Submit('register', u'同意协议，申请授权', css_class='btn btn-lg btn-success btn-block')),
+                FormActions(Submit('register', u'同意协议，创建账号', css_class='btn btn-lg btn-success btn-block')),
                 HTML("""<div class="form-group"><a href="http://www.goodrain.com/" class="linkgood text-center">goodrain.com</a></div>"""),
                 css_class="login-wrap"
             )
