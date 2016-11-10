5ba7a914459cd2d6519fc8c0f75530d29627980c
wuwen <wuwen@goodrain.com>
2016-11-10 15:00:43 +0800

    children() add

--- www/static/www/js/gr/multi_port.js
+++ www/static/www/js/gr/multi_port.js
@@ -8,7 +8,7 @@
         var port_switch = $(this);
           port = $(this).closest('tr').attr('port');
           port_type = $(this).attr('name'); //inner outer
-          var alias = $(this).parent().parent().children().eq(2).children("a").html();
+          var alias = $(this).parent().parent().children().children("a").html();
              alert(alias);
           if (state) {
             action = "open_" + port_type;
