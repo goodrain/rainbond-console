b8efe40a17bd6bbfc4cbcf8ae9faae22e8f534fd
zhangbin <elviszhang2002@gmail.com>
2016-11-23 16:33:51 +0800

    u

--- www/urls/ajax.py
+++ www/urls/ajax.py
@@ -39,5 +39,5 @@ urlpatterns = patterns(
     url(r'^(?P<tenantName>[\w\-]+)/(?P<serviceAlias>[\w\-]+)/service-mnt-share-type', ajax.MntShareTypeView.as_view()),
     url(r'^(?P<tenantName>[\w\-]+)/(?P<serviceAlias>[\w\-]+)/container-stats', ajax.ContainerStatsView.as_view()),
     
-    url(r'^(?P<tenantName>[\w\-]+)/(?P<serviceAlias>[\w\-]+)/sources/monitor/', ajax.ServiceMonitorQueryMem.as_view()),
+    url(r'^(?P<tenantName>[\w\-]+)/(?P<serviceAlias>[\w\-]+)/sources/monitor/', ajax.ServiceMonitorQuery.as_view()),
 )
