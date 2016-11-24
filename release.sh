9909c50a289264c4a8a787d87365767dede53569
qisy <messi@goodrain.com>
2016-11-24 13:12:37 +0800

    release脚本

--- release.sh
+++ release.sh
@@ -9,8 +9,19 @@ PROGRAM="console"
 WORKDIR=$PWD
 
 sed "$ a ENV RELEASE_TAG $RELEASE_TAG" Dockerfile_release > Dockerfile.release
-docker build $FORCE -t hub.goodrain.com/goodrain/$PROGRAM:$RELEASE_TAG -f Dockerfile.release .
-docker tag hub.goodrain.com/goodrain/$PROGRAM:$RELEASE_TAG hub.goodrain.com/goodrain/$PROGRAM
-docker push hub.goodrain.com/goodrain/$PROGRAM
+docker build $FORCE -t hub.goodrain.com/dc-deploy/$PROGRAM:$RELEASE_TAG -f Dockerfile.release .
+docker tag hub.goodrain.com/dc-deploy/$PROGRAM:$RELEASE_TAG hub.goodrain.com/dc-deploy/$PROGRAM
+docker push hub.goodrain.com/dc-deploy/$PROGRAM
+
+# rename tag
+#docker tag `docker images  | grep console | grep latest | awk '{print $3}'` hub.goodrain.com/dc-deploy/console:community
+docker tag `docker images  | grep console | grep latest | awk '{print $3}'` hub.goodrain.com/dc-deploy/console:pre-release
+docker images | grep console | grep community
+
+# remove old images
+#docker images  | grep console | grep -v `docker images| grep console|grep community |awk '{print $3}'` | awk '{print $3}' | xargs docker rmi
+
+# push community
+docker push hub.goodrain.com/dc-deploy/console:pre-release
 
 rm -fv Dockerfile.release
