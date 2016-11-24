b19b10d258481893638ad87be090e131494a22d3
zhouyq <zhouyq@goodrain.com>
2016-11-24 17:11:00 +0800

    更改推送顺序

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ WORKDIR=$PWD
 sed "$ a ENV RELEASE_TAG $RELEASE_TAG" Dockerfile_release > Dockerfile.release
 docker build $FORCE -t hub.goodrain.com/dc-deploy/$PROGRAM:$RELEASE_TAG -f Dockerfile.release .
 docker tag hub.goodrain.com/dc-deploy/$PROGRAM:$RELEASE_TAG hub.goodrain.com/dc-deploy/$PROGRAM
-docker push hub.goodrain.com/dc-deploy/$PROGRAM
+#docker push hub.goodrain.com/dc-deploy/$PROGRAM
 
 # rename tag
 #docker tag `docker images  | grep console | grep latest | awk '{print $3}'` hub.goodrain.com/dc-deploy/console:community
