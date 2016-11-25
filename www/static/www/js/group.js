a6f2a6ae035508544eb4696260f914f4a416a900
wuwen <wuwen@goodrain.com>
2016-11-25 14:19:04 +0800

    app group

--- www/static/www/js/group.js
+++ www/static/www/js/group.js
@@ -363,20 +363,183 @@ $(function(){
 
     //切换
     $("#imgbtn").click(function(){
-        $("#tab-box").hide();
-        $("#img-box").show()
+        $("#tabBox").hide();
+        $("#imgBox").show()
         $(this).addClass("sed");
         $("#tabbtn").removeClass("sed");
     });
     $("#tabbtn").click(function(){
-        $("#tab-box").show();
-        $("#img-box").hide()
+        $("#tabBox").show();
+        $("#imgBox").hide()
         $(this).addClass("sed");
         $("#imgbtn").removeClass("sed");
     });
     
-    //图
+    //////图
+    var json_svg = {
+        "service_dep_cname1" :["service_dep_cname2"],
+        "service_dep_cname2" :["service_dep_cname3"],
+        "service_dep_cname3" :[],
+        "service_dep_cname4" :[],
+        "service_dep_cname5" :["service_dep_cname8","service_dep_cname3"],
+        "service_dep_cname6" :["service_dep_cname9"],
+        "service_dep_cname7" :["service_dep_cname3"], 
+        "service_dep_cname8" :["service_dep_cname4"],
+        "service_dep_cname9" :["service_dep_cname3"],
+    }
+    var json_data = {
+        "service_cname1" : {"service_id" : "serone","service_alias" : "aliasone"},
+        "service_cname2" : {"service_id" : "sertwo","service_alias" : "aliastwo"},
+        "service_cname3" : {"service_id" : "serthree","service_alias" : "aliasthree"},
+        "service_cname4" : {"service_id" : "serfour","service_alias" : "aliasfour"},
+        "service_cname5" : {"service_id" : "serfive","service_alias" : "aliasfive"},
+        "service_cname6" : {"service_id" : "sersix","service_alias" : "aliassix"},
+        "service_cname7" : {"service_id" : "serseven","service_alias" : "aliasseven"},
+        "service_cname8" : {"service_id" : "sereight","service_alias" : "aliaseight"},
+        "service_cname9" : {"service_id" : "sernine","service_alias" : "aliasnine"},
+       
+    }
+    var svgNS = 'http://www.w3.org/2000/svg';
+    var arrDepApp =[];  //全部依赖别的
+    var arrApp =[];  //组合数组  右边所有依赖合并
+    var AppBot =[];
+    var AppTop = [];
+    var AppMid = []; // 中部，即依赖别的
+    for(var key in json_svg){
+        if(json_svg[key].length == 0){
+            AppBot.push(key);
+        }else{
+            arrDepApp.push(key);
+            arrApp = arrApp.concat(json_svg[key]);
+        }
+    }
+    //console.log(AppBot);
+    //console.log(arrDepApp);
+    //console.log(arrApp);
+
+    var AppMB = []; 
+    //数组去重
+    for(i=0;i<arrApp.length;i++){
+        if(AppMB.indexOf(arrApp[i])<0){
+            AppMB.push(arrApp[i])
+        }
+    }
+
+    //console.log(AppMB);
+    var strMB =  AppMB.join("&");
+    strMB = strMB + "&";
+    //console.log(strMB);
     
+    var strTM = arrDepApp.join("&");
+    strTM = strTM  + "&";
+
+    for(var i=0; i<AppBot.length;i++){
+        var str = AppBot[i] + "&";
+        var rex = new RegExp(str, 'g');
+        strMB = strMB.replace(rex, "");       
+    }
+    //console.log(strMB);
+    var strM = strMB.substring(0,(strMB.length-1));
+    //console.log(strM);
+    
+    AppMid = strM.split("&");
+    
+
+    for(var i=0; i<AppMid.length;i++){
+        var str = AppMid[i] + "&";
+        var rex = new RegExp(str, 'g');
+        strTM  = strTM.replace(rex, "");       
+    }
+    var strT = strTM.substring(0,(strTM.length-1));
+    AppTop =  strT.split("&");
+    //console.log(AppTop);
+    //console.log(AppMid);
+
+    // svg 绘图
+    var svgNS = 'http://www.w3.org/2000/svg';
+    var oSvgDiv = document.getElementById("svg-box");
+    var axisXY  = {};
+    function createTag(tag,objAttr){
+        var oTag = document.createElementNS(svgNS , tag);
+        for(var attr in objAttr){
+            oTag.setAttribute(attr,objAttr[attr]);
+        }
+        return oTag;
+    }
+    var oSvg = createTag('svg',{'xmlns':svgNS,'width':'100%','height':'600'});
+    
+    var divWidth = oSvgDiv.offsetWidth;
+    //console.log(divWidth);
+    
+    for(var i=0; i<AppTop.length;i++){
+        var top_width = divWidth/AppTop.length;
+        var top_w = top_width - 20;
+        //FnSvgIcon(top_width,50,i,AppTop[i],top_w,""); 
+        axisXY[AppTop[i]] = [(top_width*i+top_width/2),80];
+    }
+    for(var i=0; i<AppMid.length;i++){
+        var mid_width = divWidth/AppMid.length;
+        var mid_w = mid_width - 20;
+        //FnSvgIcon(mid_width,250,i,AppMid[i],mid_w,"");
+        axisXY[AppMid[i]] = [(mid_width*i+mid_width/2),280];
+    }
+    for(var i=0; i<AppBot.length;i++){
+        var bot_width = divWidth/AppBot.length;
+        var bot_w = bot_width - 20;
+        //FnSvgIcon(bot_width,450,i,AppBot[i],bot_w,"");
+        axisXY[AppBot[i]] = [(bot_width*i+bot_width/2),480];
+    }
+    //console.log(axisXY);
+    for(var key in json_svg){
+        if(json_svg[key].length != 0){
+            //console.log(key);
+            //console.log(axisXY[key]);
+            //console.log(json_svg[key]);
+            for(var i=0; i<json_svg[key].length; i++){
+                //console.log(axisXY[json_svg[key][i]]);
+                var startX = axisXY[key][0];
+                //console.log(startX);
+                var startY = axisXY[key][1];
+                //console.log(startY);
+                var endX = axisXY[json_svg[key][i]][0];
+                //console.log(endX);
+                var endY = axisXY[json_svg[key][i]][1];
+                //console.log(endY);
+                var oLine = createTag('line',{'x1':startX,'y1':startY,'x2':endX,'y2':endY,'stroke':'#ccc'});
+                //console.log(oLine);
+                oSvg.appendChild(oLine);
+            }
+        }
+    }
+    //
+    function FnSvgIcon(wid,hei,num,txt,txtWid,url){
+        var oImg = createTag('image',{'width':'60px','height':'60px','x':(wid*num+wid/2-30),'y':hei,'href':'images/app1.png'});
+        var oText = createTag('text',{'x':(wid*num+wid/2),'y':hei+70,'font-size':'12','text-anchor':'middle','fill':'#999','lengthAdjust':'spacing'});
+        oText.innerHTML = txt;
+        var oA= createTag('a',{'href':url});
+        var oG = createTag('g',{'style':'cursor:pointer'});
+        oA.appendChild(oText);
+        oA.appendChild(oImg);
+        oG.appendChild(oA);
+        oSvg.appendChild(oG);
+    }
+   for(var i=0; i<AppTop.length;i++){
+        var top_width = divWidth/AppTop.length;
+        var top_w = top_width - 20;
+        FnSvgIcon(top_width,50,i,AppTop[i],top_w,"");  
+    }
+    for(var i=0; i<AppMid.length;i++){
+        var mid_width = divWidth/AppMid.length;
+        var mid_w = mid_width - 20;
+        FnSvgIcon(mid_width,250,i,AppMid[i],mid_w,"");  
+    }
+    for(var i=0; i<AppBot.length;i++){
+        var bot_width = divWidth/AppBot.length;
+        var bot_w = bot_width - 20;
+        FnSvgIcon(bot_width,450,i,AppBot[i],bot_w,""); 
+    }
+    oSvgDiv.appendChild(oSvg);
+    //////图
 });
 
 
