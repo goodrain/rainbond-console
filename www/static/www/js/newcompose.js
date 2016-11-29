16f5266375980e35d8321defebcb7c640f1d0f10
wuwen <wuwen@goodrain.com>
2016-11-29 14:33:17 +0800

    tips

--- www/static/www/js/newcompose.js
+++ www/static/www/js/newcompose.js
@@ -318,20 +318,20 @@ $(function(){
 	});
 
 	// 名称 compose 
-	var oldname = $("#com-name").val();
-	$("#com-name").focus(function(){
+	//var oldname = $("#com-name").val();
+	/*$("#com-name").focus(function(){
 	  	$(this).attr("value","");
-	});
-	$("#com-name").blur(function(){
-		if($(this).val()== ""){
-			$(this).attr("value",oldname);
-		}
 	});
 	$("#com-name").change(function(){
 		if($(this).val()== ""){
 			$(this).attr("value",oldname);
 		}
 	});
+	$("#com-name").blur(function(){
+		if($(this).val()== ""){
+			$(this).attr("value",oldname);
+		}
+	});*/
 	// 图
 	var json_svg_ = $("#compose_relations").attr("value");
 	var json_svg = JSON.parse(json_svg_);
@@ -627,7 +627,7 @@ $(function(){
     	console.log(secdate);
     	//
 		var tenantName = $("#tenantNameValue").val();
-		var compose_group_name = $("#com-name").val() == "" ? oldname : $("#com-name").val();
+		var compose_group_name = $("#com-name").val();
 		
     	///
     	$.ajax({
