6811189d446dd9e027d4c308858bb4bbde5074e5
孙丽川 <sunlc@goodrain.com>
2016-12-02 16:02:56 +0800

    url support multi port

--- openapi/views/cloudservices.py
+++ openapi/views/cloudservices.py
@@ -358,14 +358,16 @@ class CloudServiceInstallView(BaseAPIView):
         http_port_str = ":" + http_port_str
         # 只有http服务返回url
         access_url = ""
-        if TenantServicesPort.objects.filter(service_id=newTenantService.service_id,
-                                             is_outer_service=True,
-                                             protocol='http').exists():
-            access_url = "http://{0}.{1}{2}{3}".format(
-                newTenantService.service_alias,
-                tenant_name,
-                wild_domain,
-                http_port_str)
+        service_port_list = TenantServicesPort.objects.filter(service_id=newTenantService.service_id,
+                                                              is_outer_service=True,
+                                                              protocol='http')
+        if len(service_port_list) > 0:
+            port_value = service_port_list[0].container_port
+            access_url = "http://{0}.{1}.{2}{3}{4}".format(port_value,
+                                                           newTenantService.service_alias,
+                                                           tenant_name,
+                                                           wild_domain,
+                                                           http_port_str)
         logger.debug("openapi.cloudservice", "service access url {0}".format(access_url))
         json_data = {}
         json_data["cloud_assistant"] = sn.instance.cloud_assistant
