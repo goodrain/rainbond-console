c76389f751cd746ca12fbfe478b06ecfe04a46c3
wuwen <wuwen@goodrain.com>
2016-12-15 14:05:59 +0800

    测试 四

--- www/static/www/js/newappstep.js
+++ www/static/www/js/newappstep.js
@@ -28,7 +28,7 @@ $(function(){
     
     FnRange("OneMemory","OneMemoryText","OneMemoryWid",256,2048);
     FnRange("NodeNum","NodeText","NodeWid",1,10);
-    FnRange("TimeLong","TimeLongText","TimeLongWid",9,24);
+    FnRange("TimeLong","TimeLongText","TimeLongWid",1,24);
 
     // 滑动框 结束
 
@@ -133,8 +133,6 @@ $(function(){
                     
                     if(grbranch == "newobj"){
                         $("#gr_branchbox").hide();
-                    }else{
-                        $("#gr_branchbox").show();
                     }
                     var sedoption = $(listWrap).children("option:selected");
                     var service_code_id=$(sedoption).attr("data");
@@ -147,11 +145,7 @@ $(function(){
                         var service_code_id=$(sedoption).attr("data");
                         var clone_url = $('#repos_'+service_code_id).val();
                         Fnbranch(service_code_from,service_code_id,clone_url); 
-                        if(grbranch == "newobj"){
-                            $("#gr_branchbox").hide();
-                        }else{
-                            $("#gr_branchbox").show();
-                        } 
+                       
                     });
                 }else{
                     $('#waiting').html("无可用仓库");
