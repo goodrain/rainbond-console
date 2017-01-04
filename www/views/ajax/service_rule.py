38d9a38b72b91ea315b76de83ba294944d887b34
barnett <zengqg@goodrain.com>
2017-01-04 13:22:23 +0800

    修改代码格式

--- www/views/ajax/service_rule.py
+++ www/views/ajax/service_rule.py
@@ -8,62 +8,63 @@ import logging
 
 logger = logging.getLogger('default')
 
+
 class ServiceRuleManage(AuthedView):
-
     def post(self, request, *args, **kwargs):
-	    """
-	    增加规则
-	    """	
+        """
+        增加规则
+        """
         result = {}
         try:
-        	rule = ServiceRule(
-	        	tenant_id=self.service.tenant_id,
-	            service_id=self.service.service_id
-	        	)
+            rule = ServiceRule(tenant_id=self.service.tenant_id, service_id=self.service.service_id)
             result["status"] = "success"
             result["rule"] = rule
+        
         except Exception, e:
             logger.exception(e)
-            result["status"] = "failure"
+        result["status"] = "failure"
+        
         return JsonResponse(result)
-
+    
     def get(self, request, *args, **kwargs):
-	    """
-	    获取规则
-	    """	
+        
+        """
+        获取规则
+        """
+        
         result = {}
         try:
-            rule = ServiceRule(
-	        	tenant_id=self.service.tenant_id,
-	            service_id=self.service.service_id
-	        	)
+            rule = ServiceRule(tenant_id=self.service.tenant_id, service_id=self.service.service_id)
             result["status"] = "success"
             result["rule"] = rule
         except Exception, e:
             logger.exception(e)
             result["status"] = "failure"
         return JsonResponse(result)
-
+    
     def put(self, request, *args, **kwargs):
-	    """
-	    修改规则
-	    """	
+        
+        """
+        修改规则
+        """
         result = {}
         try:
             result["status"] = "success"
         except Exception, e:
             logger.exception(e)
             result["status"] = "failure"
-        return JsonResponse(result)  
-
+        return JsonResponse(result)
+    
     def delete(self, request, *args, **kwargs):
-	    """
-	    删除规则
-	    """	
+        
+        """
+        删除规则
+        """
+        
         result = {}
         try:
             result["status"] = "success"
         except Exception, e:
             logger.exception(e)
             result["status"] = "failure"
-        return JsonResponse(result)          
+        return JsonResponse(result)
