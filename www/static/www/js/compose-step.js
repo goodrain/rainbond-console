c6df98ed0ae12099107ff96e0863531e0a47fa61
wuwen <wuwen@goodrain.com>
2017-01-05 08:38:21 +0800

    layer css  range

--- www/static/www/js/compose-step.js
+++ www/static/www/js/compose-step.js
@@ -280,7 +280,7 @@ $(function(){
     oSvgDiv.appendChild(oSvg);
     /// svg
     //图 end 
-    
+
     /////切换 start
     $(".tablink a").click(function(){
         var num = $(this).index();
@@ -308,6 +308,205 @@ $(function(){
     });
     // 切换 end
 
+   // 01 输入用户名
+     $(".fn-circle").each(function(){
+        var this_id= $(this).attr("id");
+        $("#" + this_id + "_create_app_name").blur(function(){
+            var appName = $(this).val();
+            //var checkReg = /^[a-z][a-z0-9-]*[a-z0-9]$/;
+            //var result = true;
+            if(appName == ""){
+                $("#" + this_id + "create_appname_notice").slideDown();
+                return;
+            }else{
+                $("#" + this_id + "create_appname_notice").slideUp();
+            }
+        });
+    });
+    // 01 end 
+
+    //弹出层
+    function FnLayer(textTit,myid){
+        var oDiv = '<div class="layerbg"><div class="layermain"></div></div>';
+        var oCloseBtn = '<a href="javascript:;" class="closebtn fn-close">X</a>';
+        var oTit = '<p class="layer-tit">'+ textTit +'</p>';
+        var oInput ='<p class="input-css"><input name="" type="text" value="" /></p>';
+        var oLink = '<p class="layerlink"><a href="javascript:;" class="fn-sure">确定</a><a href="javascript:;" class="fn-close">取消</a></p>';
+        $("body").append(oDiv);
+        $("div.layermain").append(oCloseBtn,oTit);
+        $("div.layermain").append(oInput);
+        $("div.layermain").append(oLink);
+        $(".fn-close").click(function(){
+             $("div.layerbg").remove();
+             $(".input-css input").prop("value","");
+             $("#group-name").find("option[value='-1']").prop("selected",true);
+        });
+        $(".fn-sure").click(function(){
+            if(inputText == ""){
+                swal("您还没有输入组名！")
+                return false;
+            }else{
+                var inputText = $(".input-css input").val();
+                var tenant_name = $("#currentTeantName").val();
+                ///ajax start
+
+                $.ajax({
+                    type : "post",
+                    url : "/ajax/" + tenant_name  + "/group/add",
+                    data : {
+                        group_name : inputText
+                    },
+                    cache : false,
+                    beforeSend : function(xhr, settings) {
+                        var csrftoken = $.cookie('csrftoken');
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    },
+                    success : function(msg) {
+                        if(msg.ok){
+                            var  group_id = msg.group_id;
+                            var  group_name = msg.group_name;
+                            var  Option = "<option value=" +  group_id + ">" + group_name + "</option>";
+                            $("div.layerbg").remove();
+                            $(".input-css input").prop("value","");
+                            $(myid).find("option").eq(0).after(Option);
+                            $(myid).find("option").each(function(){
+                                var oVal = $(this).prop("value");
+                                if(oVal == group_id){
+                                    $(this).prop("selected",true);
+                                }
+                            });
+                        }else{
+                            swal(msg.info);
+                        }
+                    },
+                    error : function() {
+                        swal("系统异常,请重试");
+                    }
+                });
+                
+                ///ajax end
+            }
+              
+        });   
+    }
+    //  弹出层
+    $(".fn-circle").each(function(){
+        var this_id= $(this).attr("id");
+        var group_name = this_id + "_group-name";
+        $(group_name).change(function(){
+            var gName = $(group_name).find("option:selected").val();
+            if (gName == -2) {
+                FnLayer("请输入新增组名",group_name);  
+            }
+        });
+    });
+    //// 选择 groupname end 
+   
+   
+    // 滑块 开始 
+    function FnRange(inputid,textid,widid,num){
+        var range= document.getElementById(inputid);
+        var result = document.getElementById(textid);
+        var wid = document.getElementById(widid);
+        var maxnum = range.getAttribute("max");
+        cachedRangeValue = /*localStorage.rangeValue ? localStorage.rangeValue :*/  num; 
+        // 检测浏览器
+        var o = document.createElement('input');
+        o.type = 'range';
+        if ( o.type === 'text' ) alert('不好意思，你的浏览器还不够酷，试试最新的浏览器吧。');
+        range.value = cachedRangeValue;
+        wid.style.width = (range.value-num)/(maxnum-num)*100 + "%";
+        range.addEventListener("mouseup", function() {
+            if(inputid == "OneMemory"){
+                if(range.value >= 128 && range.value < 256){
+                    result.innerHTML = "128M";
+                }else if(range.value >= 256 && range.value < 512){
+                    result.innerHTML = "256M";
+                }else if(range.value >= 512 && range.value < 1024){
+                    result.innerHTML = "512M";
+                }else if(range.value >= 1024 && range.value < 2048){
+                    result.innerHTML = "1G";
+                }else if(range.value >= 2048 && range.value < 3072){
+                    result.innerHTML = "2G";
+                }else if(range.value >= 3072 && range.value < 4096){
+                    result.innerHTML = "3G";
+                }else if(range.value >= 4096 && range.value < 5120){
+                    result.innerHTML = "4G";
+                }else if(range.value >= 5120 && range.value < 6144){
+                    result.innerHTML = "5G";
+                }else if(range.value >= 6144 && range.value < 7168){
+                    result.innerHTML = "6G";
+                }else if(range.value >= 7168 && range.value < 8100){
+                   result.innerHTML = "7G";
+                }else{
+                   result.innerHTML = "8G";
+                }
+            }else{
+               result.innerHTML = range.value; 
+            }
+            wid.style.width = (range.value-num)/(maxnum-num)*100 + "%";
+            //alert("你选择的值是：" + range.value + ". 我现在正在用本地存储保存此值。在现代浏览器上刷新并检测。");
+            //localStorage ? (localStorage.rangeValue = range.value) : alert("数据保存到了数据库或是其他什么地方。");
+            //result.innerHTML = range.value;
+            FnPrice();
+        }, false);
+        // 滑动时显示选择的值
+        range.addEventListener("input", function() {
+            if(inputid == "OneMemory"){
+                if(range.value >= 128 && range.value < 256){
+                    result.innerHTML = "128M";
+                }else if(range.value >= 256 && range.value < 512){
+                    result.innerHTML = "256M";
+                }else if(range.value >= 512 && range.value < 1024){
+                    result.innerHTML = "512M";
+                }else if(range.value >= 1024 && range.value < 2048){
+                    result.innerHTML = "1G";
+                }else if(range.value >= 2048 && range.value < 3072){
+                    result.innerHTML = "2G";
+                }else if(range.value >= 3072 && range.value < 4096){
+                    result.innerHTML = "3G";
+                }else if(range.value >= 4096 && range.value < 5120){
+                    result.innerHTML = "4G";
+                }else if(range.value >= 5120 && range.value < 6144){
+                    result.innerHTML = "5G";
+                }else if(range.value >= 6144 && range.value < 7168){
+                    result.innerHTML = "6G";
+                }else if(range.value >= 7168 && range.value < 8100){
+                   result.innerHTML = "7G";
+                }else{
+                   result.innerHTML = "8G";
+                }
+            }else{
+               result.innerHTML = range.value; 
+            }
+            wid.style.width = (range.value-num)/(maxnum-num)*100 + "%";
+
+        }, false);
+    }
+    
+    
+    
+   $(".fn-circle").each(function(){
+        var this_id= $(this).attr("id");
+        var OneMemory =  this_id + "_OneMemory";
+        var OneMemoryText = this_id + "_OneMemoryText";
+        var OneMemoryWid = this_id + "_OneMemoryWid";
+        var NodeNum = this_id + "_NodeNum";
+        var NodeText = this_id + "_NodeText";
+        var NodeWid = this_id + "_NodeWid";
+        var Disk = this_id + "_Disk";
+        var DiskText = this_id + "_DiskText";
+        var DiskWid = this_id + "_DiskWid";
+        var TimeLong = this_id + "_TimeLong";
+        var TimeLongText = this_id + "_TimeLongText";
+        var TimeLongWid = this_id + "_TimeLongWid";
+        FnRange("OneMemory","OneMemoryText","OneMemoryWid",128);
+        FnRange("NodeNum","NodeText","NodeWid",1);
+        FnRange("Disk","DiskText","DiskWid",1);
+        FnRange("TimeLong","TimeLongText","TimeLongWid",1);
+    });
+    
+    // 滑动框 结束
 
     $(".fn-circle").each(function(){
         var this_id= $(this).attr("id");
