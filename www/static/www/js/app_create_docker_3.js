730857a481a019eb8022d57a120225484de3d858
jiahui li <15176371375@163.com>
2017-01-10 10:06:11 +0800

    Move

--- www/static/www/js/app_create_docker_3.js
+++ www/static/www/js/app_create_docker_3.js
@@ -190,7 +190,7 @@ $(function () {
                 var onOff = true;
                 for( var j = 0; j<appNameLen; j++ )
                 {
-                    if( $("a.appName")[j].innerHTML == $(".depend input")[i].getAttribute("data-id") )
+                    if( $("a.appName")[j].getAttribute("data-serviceid") == $(".depend input")[i].getAttribute("data-id") )
                     {
                         onOff = false;
                         break;
