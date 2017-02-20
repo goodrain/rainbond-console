62c5cd36dc81f06d5b567088e3fa7520ecbc6649
jiahui li <15176371375@163.com>
2017-02-20 16:38:33 +0800

    Domain

--- www/static/www/js/cdn.js
+++ www/static/www/js/cdn.js
@@ -1,5 +1,25 @@
 $(function(){
     $("button.add_domain").click(function(){
         $("tfoot.input_domain").show();
-    })
+    });
+    $("button.add_sure").click(function(){
+        swal("系统异常");
+        var str = "<tr><td>"+"123"+"</td>";
+        str += "<td>"+456+"</td>";
+        str += "<td>"+789+"</td>";
+        str += "<td><a class='del_domain'>删除</a></td></tr>";
+        str.appendTo("tbody.domain-box");
+        del_domain();
+    });
+    $("button.add_cancel").click(function(){
+        $("tfoot.input_domain").hide();
+        $("input.domain_name").val("");
+    });
+    del_domain();
+    function del_domain(){
+        $("a.del_domain").off('click');
+        $("a.del_domain").on('click',function(){
+            $(this).parents("tr").remove();
+        });
+    }
 })
