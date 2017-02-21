b6f6bb7e96f62548acec004f32977f9515aa2a5f
jiahui li <15176371375@163.com>
2017-02-21 10:44:45 +0800

    Operator

--- www/static/www/js/cdn.js
+++ www/static/www/js/cdn.js
@@ -3,15 +3,20 @@ $(function(){
         $("p.input_domain").show();
     });
     $("button.add_sure").click(function(){
-        swal("系统异常");
-        var str = "<tr><td>"+"123"+"</td>";
-        str += "<td>"+456+"</td>";
-        str += "<td>"+789+"</td>";
-        str += "<td><a class='del_domain'>删除</a></td></tr>";
-        $(str).appendTo("tbody.domain-box");
-        $("p.input_domain").hide();
-        $("input.domain_name").val("");
-        del_domain();
+        if( $("input.domain_name").val() )
+        {
+            var str = "<tr><td>"+"123"+"</td>";
+            str += "<td>"+456+"</td>";
+            str += "<td>"+789+"</td>";
+            str += "<td><a class='del_domain'>删除</a></td></tr>";
+            $(str).appendTo("tbody.domain_box");
+            $("p.input_domain").hide();
+            $("input.domain_name").val("");
+            del_domain();
+        }
+        else{
+            swal("请输入域名");
+        }
     });
     $("button.add_cancel").click(function(){
         $("p.input_domain").hide();
@@ -28,6 +33,37 @@ $(function(){
     $("button.add_operator").click(function(){
         $("p.input_operator").show();
     });
+    $("button.operator_sure").click(function(){
+        if( $(".input_operator.operator_name") && $(".input_operator.operator_realName").val() && $(".input_operator.operator_password").val() )
+        {
+            var str = "<tr><td>"+$(".input_operator.operator_name").val()+"</td>";
+            str += "<td>"+$(".input_operator.operator_realName").val()+"</td>";
+            str += "<td>"+$(".input_operator.operator_password").val()+"</td>";
+            str += "<td><a class='authorize_cancel'>取消权限</a></td></tr>";
+            $(str).appendTo("tbody.operator_box");
+            $("p.input_operator").hide();
+            $("input.operator_name").val("");
+            $("input.operator_realName").val("");
+            $("input.operator_password").val("");
+            del_operator();
+        }
+        else{
+            swal("请输入信息");
+        }
+    });
+    $("button.operator_cancel").click(function(){
+        $("p.input_operator").hide();
+        $("input.operator_name").val("");
+        $("input.operator_realName").val("");
+        $("input.operator_password").val("");
+    });
+    del_operator();
+    function del_operator(){
+        $("a.del_operator").off('click');
+        $("a.del_operator").on('click',function(){
+            $(this).parents("tr").remove();
+        });
+    }
 
 
     editPort();
