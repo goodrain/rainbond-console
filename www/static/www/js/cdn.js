74c1d4cb15cb0b94b6b3e6b4efdefd229d7f10ad
jiahui li <15176371375@163.com>
2017-02-21 11:46:56 +0800

    add_domain

--- www/static/www/js/cdn.js
+++ www/static/www/js/cdn.js
@@ -5,14 +5,32 @@ $(function(){
     $("button.add_sure").click(function(){
         if( $("input.domain_name").val() )
         {
-            var str = "<tr><td>"+"123"+"</td>";
-            str += "<td>"+456+"</td>";
-            str += "<td>"+789+"</td>";
-            str += "<td><a class='del_domain'>删除</a></td></tr>";
-            $(str).appendTo("tbody.domain_box");
-            $("p.input_domain").hide();
-            $("input.domain_name").val("");
-            del_domain();
+            var tenantName = $("#tenantName").val();
+            var app_id = $("#app_id").val();
+            $.ajax({
+                type : "POST",
+                url : "/ajax/"+tenantName+"/"+app_id+"/domain/add",
+                data : {
+                    doamin : $("input.domain_name").val()
+                },
+                cache: false,
+                success : function(data){
+                    if(data["status"] == "success")
+                    {
+                        var str = "<tr><td>"+$("input.domain_name").val()+"</td>";
+                        str += "<td>未审核</td>";
+                        str += "<td>2017-02-19</td>";
+                        str += "<td><a class='del_domain'>删除</a></td></tr>";
+                        $(str).appendTo("tbody.domain_box");
+                        $("p.input_domain").hide();
+                        $("input.domain_name").val("");
+                        del_domain();
+                    }
+                },
+                error : function(){
+                    swal("系统异常");
+                }
+            });
         }
         else{
             swal("请输入域名");
@@ -26,7 +44,18 @@ $(function(){
     function del_domain(){
         $("a.del_domain").off('click');
         $("a.del_domain").on('click',function(){
-            $(this).parents("tr").remove();
+            $.ajax({
+                type : "POST",
+                url : "",
+                data : 123,
+                cache: false,
+                success : function(data){
+                    $(this).parents("tr").remove();
+                },
+                error : function(){
+                    swal("系统异常");
+                }
+            });
         });
     }
 
@@ -36,18 +65,29 @@ $(function(){
     $("button.operator_sure").click(function(){
         if( $("input.operator_name").val() && $("input.operator_realName").val() && $("input.operator_password").val() )
         {
-            var str = "<tr><td>"+$("input.operator_name").val()+"</td>";
-            str += "<td>"+$("input.operator_realName").val()+"</td>";
-            str += "<td class='operator_auth clearfix'>"+'<span class="check"></span><span class="text_auth">读取</span><span class="check"></span><span class="text_auth">写入</span><span class="check"></span><span class="text_auth">删除</span>'+"</td>";
-            str += '<td style="color:#28cb75;">正常</td>';
-            str += '<td>2017-02-17 12:00</td>';
-            str += "<td><a class='authorize_cancel'>取消权限</a></td></tr>";
-            $(str).appendTo("tbody.operator_box");
-            $("p.input_operator").hide();
-            $("input.operator_name").val("");
-            $("input.operator_realName").val("");
-            $("input.operator_password").val("");
-            del_operator();
+            $.ajax({
+                type : "POST",
+                url : "",
+                data : 123,
+                cache: false,
+                success : function(data){
+                    var str = "<tr><td>"+$("input.operator_name").val()+"</td>";
+                    str += "<td>"+$("input.operator_realName").val()+"</td>";
+                    str += "<td class='operator_auth clearfix'>"+'<span class="check"></span><span class="text_auth">读取</span><span class="check"></span><span class="text_auth">写入</span><span class="check"></span><span class="text_auth">删除</span>'+"</td>";
+                    str += '<td style="color:#28cb75;">正常</td>';
+                    str += '<td>2017-02-17 12:00</td>';
+                    str += "<td><a class='authorize_cancel'>取消权限</a></td></tr>";
+                    $(str).appendTo("tbody.operator_box");
+                    $("p.input_operator").hide();
+                    $("input.operator_name").val("");
+                    $("input.operator_realName").val("");
+                    $("input.operator_password").val("");
+                    del_operator();
+                },
+                error : function(){
+                    swal("系统异常");
+                }
+            });
         }
         else{
             swal("请输入信息");
@@ -63,7 +103,18 @@ $(function(){
     function del_operator(){
         $("a.del_operator").off('click');
         $("a.del_operator").on('click',function(){
-            $(this).parents("tr").remove();
+            $.ajax({
+                type : "POST",
+                url : "",
+                data : 123,
+                cache: false,
+                success : function(data){
+                    $(this).parents("tr").remove();
+                },
+                error : function(){
+                    swal("系统异常");
+                }
+            });
         });
     }
 
