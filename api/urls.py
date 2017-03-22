4b247deee9208680a4afbd58e55a8adf3222d3e5
lichao <lic@goodrain.com>
2017-03-22 18:47:35 +0800

    update op api

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken import views
 from api.views.services import SelectedServiceView, PublishServiceView, \
     ReceiveServiceView, QueryServiceView, QueryTenantView
 from api.views.tenants.services import TenantServiceStaticsView, TenantHibernateView, TenantView, AllTenantView, GitCheckCodeView, \
-    UpdateTenantResourceView
+     UpdateServiceExpireTime
 from api.views.tenants import move
 from api.views.rules import *
 
@@ -26,7 +26,7 @@ urlpatterns = patterns(
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth', views.obtain_auth_token),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^tenants/services/update_resource', UpdateTenantResourceView.as_view()),
+    url(r'^tenants/services/update_resource', UpdateServiceExpireTime.as_view()),
     url(r'^rules/(?P<service_region>[\w\-]+)$', RulesController.as_view()),
     url(r'^rules/(?P<rule_id>[\w\-]+)/instance$', InstanceManager.as_view()),
     url(r'^services/(?P<service_id>[\w\-]+)/info', ServiceInfo.as_view()),
