53cb843213c0c07472e743d84d0ae25b5fbf9f24
lichao <lic@goodrain.com>
2017-03-28 17:44:43 +0800

    update add license expired page

--- www/utils/sn.py
+++ www/utils/sn.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 from crypt import AuthCode
 import json
-
+import datetime
 
 class SNUtil:
     def __init__(self):
@@ -32,6 +32,20 @@ class SNUtil:
                 self.pricing = json_dict.get("pricing", "community")
                 self.client_id = json_dict.get("client_id", "")
                 self.client_secret = json_dict.get("client_secret", "")
+                self.expire_day = json_dict.get("end_time","")
+            if version == "02":
+                key_str = self.sn[:-2]
+                str_key = "goodrain_private_cloud_assistant_sn_for_individual"
+                json_str = AuthCode.decode(key_str, str_key)
+                json_dict = json.loads(json_str)
+                self.cloud_assistant = json_dict.get("enterprise", self.cloud_assistant)
+                self.username = json_dict.get("username", self.username)
+                self.password = json_dict.get("password", "")
+                self.pricing = json_dict.get("pricing", "community")
+                self.client_id = json_dict.get("client_id", "")
+                self.client_secret = json_dict.get("client_secret", "")
+                self.expire_day = json_dict.get("end_time","")
+
 
     @property
     def cloud_assistant(self):
@@ -57,8 +71,23 @@ class SNUtil:
     def client_secret(self):
         return self.client_secret
 
+    @property
+    def expired_day(self):
+        return self.expire_day
+
     def is_private(self):
         return self.pricing == "community"
+
+    def is_expired(self):
+        if not self.expire_day:
+            return False
+        else:
+            lisecen_expire_day = datetime.datetime.strptime(self.expire_day, "%Y-%m-%d")
+            now = datetime.datetime.now()
+            if now > lisecen_expire_day:
+                return True
+            else:
+                return False
 
 instance = SNUtil()
 
