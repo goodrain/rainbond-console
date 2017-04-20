3e86c2d50d5a55b2a38b68145ad8be364a1cbf9f
lichao <lic@goodrain.com>
2017-04-20 10:11:26 +0800

    添加发送的字典

--- api/views/services/basic.py
+++ api/views/services/basic.py
@@ -402,7 +402,9 @@ class PublishServiceView(APIView):
                             # 将服务组信息发送到云市
                             tenant_id = data["tenant_id"]
                             param_data = {"group_name": app_service_group.group_share_alias,
-                                          "tenant_id": tenant_id,"group_version": app_service_group.group_version}
+                                          "tenant_id": tenant_id, "group_version": app_service_group.group_version,
+                                          "publish_type": app_service_group.publish_type,
+                                          "installable": app_service_group.installable}
                             tmp_ids = app_service_group.service_ids
                             service_id_list = json.loads(tmp_ids)
                             if len(service_id_list) > 0:
