ea2b23a222222ca56f21f1e1af40f4158ba96f26
qisy <qisy@goodrain.com>
2017-04-24 12:18:47 +0800

    更新编译脚本

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $1 = "bash" ];then
-    exec /bin/bash
-elif [ $1 = "version" ];then
-    echo $RELEASE_TAG
-else
-    gunicorn goodrain_web.wsgi --max-requests=5000 --reload --debug --workers=4 --log-file - --access-logfile - --error-logfile -
-fi
--- release.sh
+++ release.sh
@@ -1,21 +1,31 @@
+set -e
 BASE_DIR=$PWD
 git archive community --format tgz -o hack/source.tgz
 
 cd hack/
 
-db_container=$(docker run -d hub.goodrain.com/dc-deploy/mysql)
+db_container=console-mysql
+docker run -d --name console-mysql hub.goodrain.com/dc-deploy/mysql
 sleep 15
 docker exec $db_container ps -ef
 docker exec $db_container mysql -e "create database console;"
 docker exec $db_container mysql -e "grant all on console.* to build@'%' identified by 'build';"
 docker exec $db_container mysql -e "flush privileges;"
 db_ip=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' $db_container)
-sed -e "/^WORKDIR/i ENV MYSQL_HOST $db_ip" Dockerfile_build_template > Dockerfile_build
+
+RELEASE_TAG=$(git describe)
+RELEASE_VERSION=${RELEASE_TAG%%-*}
+
+sed -e "/^WORKDIR/i ENV MYSQL_HOST $db_ip" \
+    -e "/^WORKDIR/i ENV RELEASE_TAG $RELEASE_TAG" \
+  Dockerfile_build_template > Dockerfile_build
 
 docker build -t console-build -f Dockerfile_build .
 
 rm -rf $PWD/app
-docker run -v $PWD/app:/app -w /app-build/dist console-build rsync -a console_app /app/
+docker run -v $PWD/app:/app -w /app-build/dist console-build rsync -a console_app/ /app
 docker run -v $PWD/app:/app -w /app-build/dist console-build cp -v console_manage/console_manage /app/
 
 docker build -t console-release -f Dockerfile_release .
+docker kill $db_container
+docker rm $db_container
