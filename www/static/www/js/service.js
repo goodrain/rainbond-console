7a00035f29b701ba4e895c159f3aa3a64c30682d
jiahui li <15176371375@163.com>
2017-05-10 14:18:31 +0800

    Show

--- www/static/www/js/service.js
+++ www/static/www/js/service.js
@@ -281,7 +281,10 @@ function connectSocket(event_id,action) {
         tmpLog = "<p>" + time2+" "+ m.message + "</p>";
         //$("#keylog").children("div:first-child").before(tmpLog)
         $("#keylog .panel-heading").eq(0).css({"padding-bottom":"5px"});
-        $("#keylog .log").eq(0).css({"height":"30px"});
+        $("#keylog .ajax_log").eq(0).css({"height":"30px"});
+        $("#keylog .log").eq(0).hide();
+        $("#keylog .hide_log").eq(0).show();
+        $("#keylog .log_type").eq(0).hide();
         $(tmpLog).prependTo($("#keylog .log_content").eq(0));
         if( m.step == "callback" || m.step == "last" )
         {
