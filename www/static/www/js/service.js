02886c8f320a4873020c610bf718b675ad883b85
wuwen <wuwen@goodrain.com>
2017-05-24 18:28:41 +0800

    js

--- www/static/www/js/service.js
+++ www/static/www/js/service.js
@@ -169,7 +169,7 @@ function createEvents(name, service, action) {
                 }
 
                 var str_log = '<li><time class="tl-time"><h4>'+time+'</h4></time><i class="fa bg-grey tl-icon"></i><div class="tl-content"><div class="panel panel-primary"><div class="panel-heading"><span>'+type_json[event["event_type"]]+'中@'+event["user_name"]+'</span><div class="user"><p>';
-                str_log += '</p><p class="ajax_log" data-log="'+event["event_id"]+'" style="display: none;">查看详情</p><p class="hide_log" style="display: block;">查看详情</p></div></div><div class="panel-body"><div class="log"><p class="log_type" style="display: none;"><label class="active" data-log="info">Info日志</label><label data-log="debug">Debug日志</label><label data-log="error">Error日志</label></p><div class="log_content log_height2 log_'+event["event_id"]+'"></div></div></div></div></div></li>'
+                str_log += '</p><p class="ajax_log" data-log="'+event["event_id"]+'" style="display: none;">查看详情</p><p class="hide_log" style="display: block;">收起</p></div></div><div class="panel-body"><div class="log"><p class="log_type" style="display: none;"><label class="active" data-log="info">Info日志</label><label data-log="debug">Debug日志</label><label data-log="error">Error日志</label></p><div class="log_content log_height2 log_'+event["event_id"]+'"></div></div></div></div></div></li>'
 
                 if( event["event_type"] == "deploy" && event["old_deploy_version"] )
                 {
