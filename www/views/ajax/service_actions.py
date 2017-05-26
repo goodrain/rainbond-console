21cf99b61eedd29d80e9f762f891c42526b0e4c2
崔斌 <cuiminmm@gmail.com>
2017-05-26 11:10:54 +0800

    composer导入修改端口

--- www/views/ajax/service_actions.py
+++ www/views/ajax/service_actions.py
@@ -629,7 +629,6 @@ class UseMidRain(AuthedView):
 
     def addIsHttpEnv(self, service):
         # add domposer-compose
-        logger.debug("addIsHttpEnv")
         is_compose = TenantServiceInfo.objects.filter(service_id=self.service.service_id, language="docker-compose").count()
         if is_compose > 0:
             logger.debug("is_composer %s, IS_HTTP" % service.service_id)
@@ -1303,7 +1302,11 @@ class ServicePort(AuthedView):
             logger.info(deal_port.mapping_port)
             baseService = BaseTenantService()
             if deal_port.mapping_port <= 1:
-                mapping_port = baseService.prepare_mapping_port(self.service, deal_port.container_port)
+                is_compose = TenantServiceInfo.objects.filter(service_id=self.service.service_id, language="docker-compose").count()
+                if is_compose > 0:
+                    mapping_port = deal_port.container_port
+                else:
+                    mapping_port = baseService.prepare_mapping_port(self.service, deal_port.container_port)
                 deal_port.mapping_port = mapping_port
                 deal_port.save(update_fields=['mapping_port'])
                 TenantServiceEnvVar.objects.filter(service_id=deal_port.service_id,
