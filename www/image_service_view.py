88bbf4bd6247ef53a26a1abe7f4cfc72df0e0810
崔斌 <cuiminmm@gmail.com>
2017-06-12 16:56:15 +0800

    bug

--- www/image_service_view.py
+++ www/image_service_view.py
@@ -192,10 +192,8 @@ class ImageServiceDeploy(LeftSideBarMixin, AuthedView):
                         if args:
                             args = "{0}^_^{1}={2}".format(args, mm[0], mm[1])
                         else:
-                            args = "{0}={1}".format(args, mm[0], mm[1])
-                    logger.debug(list_args[:-1])
+                            args = "{0}={1}".format(mm[0], mm[1])
                     logger.debug(args)
-                    logger.debug(base64.b64decode(args))
                     result["params"] = base64.b64encode(args)
             else:
                 result["status"] = "no_image_url"
