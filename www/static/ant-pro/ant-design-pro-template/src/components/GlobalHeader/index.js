87135463bbcac6c9f55fd8bdf4865bfff3b16bd4
yangyq <yangyq@goodrain.com>
2018-03-12 18:44:56 +0800

    [remove] 去掉测试数据

--- www/static/ant-pro/ant-design-pro-template/src/components/GlobalHeader/index.js
+++ www/static/ant-pro/ant-design-pro-template/src/components/GlobalHeader/index.js
@@ -1,6 +1,4 @@
-import React, {
-    PureComponent
-} from 'react';
+import React, {PureComponent} from 'react';
 import {
     Layout,
     Menu,
@@ -15,17 +13,13 @@ import Ellipsis from '../Ellipsis';
 import moment from 'moment';
 import groupBy from 'lodash/groupBy';
 import Debounce from 'lodash-decorators/debounce';
-import {
-    Link
-} from 'dva/router';
+import {Link} from 'dva/router';
 import NoticeIcon from '../NoticeIcon';
 import HeaderSearch from '../HeaderSearch';
 import styles from './index.less';
 import cookie from '../../utils/cookie';
 
-const {
-    Header
-} = Layout;
+const {Header} = Layout;
 
 export default class GlobalHeader extends PureComponent {
     componentWillUnmount() {
@@ -37,119 +31,106 @@ export default class GlobalHeader extends PureComponent {
         const {
             notices = []
         } = this.props;
-        if ( notices.length === 0 ) {
+        if (notices.length === 0) {
             return {};
         }
-        const newNotices = notices.map( ( notice ) => {
+        const newNotices = notices.map((notice) => {
             const newNotice = {
                 ...notice
             };
-            if ( newNotice.datetime ) {
-                newNotice.datetime = moment( notice.datetime ).fromNow();
+            if (newNotice.datetime) {
+                newNotice.datetime = moment(notice.datetime).fromNow();
             }
             // transform id to item key
-            if ( newNotice.id ) {
+            if (newNotice.id) {
                 newNotice.key = newNotice.id;
             }
-            if ( newNotice.extra && newNotice.status ) {
-                const color = ( {
-                    todo: '',
-                    processing: 'blue',
-                    urgent: 'red',
-                    doing: 'gold'
-                } )[ newNotice.status ];
-                newNotice.extra = <Tag color={color} style={{
-          marginRight: 0
-        }}>{newNotice.extra}</Tag>;
+            if (newNotice.extra && newNotice.status) {
+                const color = ({todo: '', processing: 'blue', urgent: 'red', doing: 'gold'})[newNotice.status];
+                newNotice.extra = <Tag
+                    color={color}
+                    style={{
+                    marginRight: 0
+                }}>{newNotice.extra}</Tag>;
             }
             return newNotice;
-        } );
-        return groupBy( newNotices, 'type' );
+        });
+        return groupBy(newNotices, 'type');
     }
     toggle = () => {
-        const {
-            collapsed,
-            onCollapse
-        } = this.props;
-        onCollapse( !collapsed );
+        const {collapsed, onCollapse} = this.props;
+        onCollapse(!collapsed);
         this.triggerResizeEvent();
     }
-    @Debounce( 600 )
+    @Debounce(600)
     triggerResizeEvent() { // eslint-disable-line
-        const event = document.createEvent( 'HTMLEvents' );
-        event.initEvent( 'resize', true, false );
-        window.dispatchEvent( event );
+        const event = document.createEvent('HTMLEvents');
+        event.initEvent('resize', true, false);
+        window.dispatchEvent(event);
     }
     renderTeams = () => {
         const onTeamClick = this.props.onTeamClick;
-        const {
-            currTeam
-        } = this.props;
+        const {currTeam} = this.props;
         const currentUser = this.props.currentUser;
         const teams = currentUser.teams || [];
-        const team = teams.filter( ( item ) => {
+        const team = teams.filter((item) => {
             item.team_name === currTeam;
-        } )
+        })
 
         return <Menu className={styles.menu} selectedKeys={[]} onClick={onTeamClick}>
-      <Menu.Item key={'tre'}>
-        <Ellipsis tooltip>这是很长的名字名字名字啊</Ellipsis>
-      </Menu.Item>
-      {teams.map((item) => {
-        return (
-          <Menu.Item key={item.team_name}>
-            <Ellipsis tooltip>{item.team_alias}</Ellipsis>
-          </Menu.Item>
-        )
-      })
+            {teams.map((item) => {
+                return (
+                    <Menu.Item key={item.team_name}>
+                        <Ellipsis tooltip>{item.team_alias}</Ellipsis>
+                    </Menu.Item>
+                )
+            })
 }
-      <Menu.Divider/>
-      <Menu.Item key={'createTeam'}><Icon type="plus"/>新建团队</Menu.Item>
-    </Menu>
+            <Menu.Divider/>
+            <Menu.Item key={'createTeam'}><Icon type="plus"/>新建团队</Menu.Item>
+        </Menu>
     }
     getCurrTeam = () => {
         const currTeam = this.props.currTeam;
         const currentUser = this.props.currentUser;
         const teams = currentUser.teams || [];
-        return teams.filter( ( item ) => {
+        return teams.filter((item) => {
             return item.team_name === currTeam;
-        } )[ 0 ]
+        })[0]
     }
     renderRegions = () => {
         const onRegionClick = this.props.onRegionClick;
         const team = this.getCurrTeam();
-        if ( team ) {
+        if (team) {
             return <Menu className={styles.menu} selectedKeys={[]} onClick={onRegionClick}>
-        {(team.region || []).map((item) => {
-          return (
-            <Menu.Item key={item.team_region_name}>{item.team_region_alias}</Menu.Item>
-          )
-        })
+                {(team.region || []).map((item) => {
+                    return (
+                        <Menu.Item key={item.team_region_name}>{item.team_region_alias}</Menu.Item>
+                    )
+                })
 }
-        <Menu.Divider/>
-        <Menu.Item key={'openRegion'}><Icon type="plus"/>开通数据中心</Menu.Item>
-      </Menu>
+                <Menu.Divider/>
+                <Menu.Item key={'openRegion'}><Icon type="plus"/>开通数据中心</Menu.Item>
+            </Menu>
         }
         return <Menu/>;
     }
     getCurrTeamTit() {
         var team = this.getCurrTeam();
-        if ( team ) {
+        if (team) {
             return team.team_alias;
         }
         return ''
     }
     getCurrRegionTit() {
-        const {
-            currRegion
-        } = this.props;
+        const {currRegion} = this.props;
         var team = this.getCurrTeam();
-        if ( team ) {
+        if (team) {
             var regions = team.region;
-            var selectedRegion = regions.filter( ( item ) => {
+            var selectedRegion = regions.filter((item) => {
                 return item.team_region_name === currRegion;
-            } )[ 0 ]
-            if ( selectedRegion ) {
+            })[0]
+            if (selectedRegion) {
                 return selectedRegion.team_region_alias;
             }
         }
@@ -172,59 +153,59 @@ export default class GlobalHeader extends PureComponent {
             currTeam
         } = this.props;
 
-        if ( !currentUser ) {
+        if (!currentUser) {
             return null
         }
 
         const menu = (
             <Menu selectedKeys={[]} onClick={onMenuClick}>
-        {/*<Menu.Item disabled><Icon type="user" />个人中心</Menu.Item>
+                {/*<Menu.Item disabled><Icon type="user" />个人中心</Menu.Item>
         <Menu.Item disabled><Icon type="setting" />设置</Menu.Item>
         <Menu.Item key="triggerError"><Icon type="close-circle" />触发报错</Menu.Item>
         <Menu.Divider />*/}
-        <Menu.Item key="logout"><Icon type="logout"/>退出登录</Menu.Item>
-      </Menu>
+                <Menu.Item key="logout"><Icon type="logout"/>退出登录</Menu.Item>
+            </Menu>
         );
 
         const noticeData = this.getNoticeData();
         return (
             <Header className={styles.header}>
-        {isMobile && ([
-          (
-            <Link to="/" className={styles.logo} key="logo">
-              <img src={logo} alt="logo" width="32"/>
-            </Link>
-          ), < Divider type = "vertical" key = "line" />
-        ])}
-        <Icon
-          className={styles.trigger}
-          type={collapsed
-          ? 'menu-unfold'
-          : 'menu-fold'}
-          onClick={this.toggle}/>
+                {isMobile && ([
+                    (
+                        <Link to="/" className={styles.logo} key="logo">
+                            <img src={logo} alt="logo" width="32"/>
+                        </Link>
+                    ), < Divider type = "vertical" key = "line" />
+                ])}
+                <Icon
+                    className={styles.trigger}
+                    type={collapsed
+                    ? 'menu-unfold'
+                    : 'menu-fold'}
+                    onClick={this.toggle}/>
 
-        <div className={styles.teamregion}>
-          <span className={styles.tit}>团队:</span>
-          <Dropdown overlay={this.renderTeams()}>
-            <span className={styles.dropdown}>
-              {this.getCurrTeamTit()
+                <div className={styles.teamregion}>
+                    <span className={styles.tit}>团队:</span>
+                    <Dropdown overlay={this.renderTeams()}>
+                        <span className={styles.dropdown}>
+                            {this.getCurrTeamTit()
 }
-              <Icon type="down"/>
-            </span>
-          </Dropdown>
+                            <Icon type="down"/>
+                        </span>
+                    </Dropdown>
 
-          <span className={styles.tit}>数据中心:</span>
-          <Dropdown overlay={this.renderRegions()}>
-            <span className={styles.dropdown}>
-              {this.getCurrRegionTit()
+                    <span className={styles.tit}>数据中心:</span>
+                    <Dropdown overlay={this.renderRegions()}>
+                        <span className={styles.dropdown}>
+                            {this.getCurrRegionTit()
 }
-              <Icon type="down"/>
-            </span>
-          </Dropdown>
-        </div>
+                            <Icon type="down"/>
+                        </span>
+                    </Dropdown>
+                </div>
 
-        <div className={styles.right}>
-          {/*
+                <div className={styles.right}>
+                    {/*
           <HeaderSearch
             className={`${styles.action} ${styles.search}`}
             placeholder="站内搜索"
@@ -267,20 +248,22 @@ export default class GlobalHeader extends PureComponent {
             />
           </NoticeIcon>
         */}
-          {currentUser
-            ? (
-              <Dropdown overlay={menu}>
-                <span className={`${styles.action} ${styles.account}`}>
-                  <Avatar size="small" className={styles.avatar} src={currentUser.avatar}/>
-                  <span className={styles.name}>{currentUser.user_name}</span>
-                </span>
-              </Dropdown>
-            )
-            : <Spin size="small" style={{
-              marginLeft: 8
-            }}/>}
-        </div>
-      </Header>
+                    {currentUser
+                        ? (
+                            <Dropdown overlay={menu}>
+                                <span className={`${styles.action} ${styles.account}`}>
+                                    <Avatar size="small" className={styles.avatar} src={currentUser.avatar}/>
+                                    <span className={styles.name}>{currentUser.user_name}</span>
+                                </span>
+                            </Dropdown>
+                        )
+                        : <Spin
+                            size="small"
+                            style={{
+                            marginLeft: 8
+                        }}/>}
+                </div>
+            </Header>
         );
     }
 }
