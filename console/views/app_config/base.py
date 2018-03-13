6c681cb5c5801b88696809488d21642d6b8b16fb
lichao <lic@goodrain.com>
2018-03-13 16:48:30 +0800

    [FIX] fix service not found bug

--- console/views/app_config/base.py
+++ console/views/app_config/base.py
@@ -23,7 +23,7 @@ class AppBaseView(RegionTenantHeaderView):
         service_alias = kwargs.get("serviceAlias", None)
         if not service_alias:
             raise ImportError("You url not contains args - serviceAlias -")
-        services = TenantServiceInfo.objects.filter(service_alias=service_alias)
+        services = TenantServiceInfo.objects.filter(service_alias=service_alias,tenant_id=self.tenant.tenant_id)
         if services:
             self.service = services[0]
             if self.service.tenant_id != self.tenant.tenant_id:
