9cbfc795f212af1dd5d7e69c8d530680fd52c486
yangyq <yangyq@goodrain.com>
2018-03-20 18:06:03 +0800

    add Test log

--- www/static/ant-pro/ant-design-pro-template/src/components/CodeCustomForm/index.js
+++ www/static/ant-pro/ant-design-pro-template/src/components/CodeCustomForm/index.js
@@ -121,7 +121,7 @@ export default class Index extends PureComponent {
 		const data = this.props.data || {};
 		const showSubmitBtn = this.props.showSubmitBtn === void 0 ? true : this.props.showSubmitBtn;
 		const showCreateGroup = this.props.showCreateGroup === void 0 ? true : this.props.showCreateGroup;
-		console.log('this is newwwwww')
+		console.log(data);
 		return (
 				<Fragment>
 			        <Form onSubmit={this.handleSubmit} layout="horizontal" hideRequiredMark>
