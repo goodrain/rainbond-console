f2e6c766ae60a89f88779834f5dceb0505af46f8
ysicing <root@ysicing.net>
2018-03-21 20:25:27 +0800

    [REV] update dockerfile

--- release.sh
+++ release.sh
@@ -31,7 +31,7 @@ function release(){
 
   sed "s/__RELEASE_DESC__/${release_desc}/" Dockerfile.release > Dockerfile.build
   docker build -t grthon:2.7.9 -f Dockerfile.base .
-  docker build --no-cache -t rainbond/${image_name}:${VERSION} -f Dockerfile.build .
+  docker build -t rainbond/${image_name}:${VERSION} -f Dockerfile.build .
   rm -r ./Dockerfile.build
 }
 
