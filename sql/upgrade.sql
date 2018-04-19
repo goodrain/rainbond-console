c6a028df41bb1f92e7af906121e670771e322722
lichao <lic@goodrain.com>
2018-04-19 13:09:14 +0800

    [FIX] fix plugin default value in db is too short bug

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -269,3 +269,5 @@ CREATE TABLE enterprise_user_perm
 -- 2018.04.19
 ALTER TABLE plugin_config_items MODIFY attr_default_value VARCHAR(128);
 ALTER TABLE tenant_service_plugin_attr MODIFY attr_default_value VARCHAR(128);
+ALTER TABLE tenant_service_plugin_attr MODIFY attr_value VARCHAR(128);
+ALTER TABLE tenant_service_plugin_attr MODIFY attr_alt_value VARCHAR(128);
