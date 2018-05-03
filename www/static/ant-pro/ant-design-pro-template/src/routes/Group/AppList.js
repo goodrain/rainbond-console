c76c21980430f166351cd7c07554dfe3b16aa491
wuwen <wuwen@172.16.0.131>
2018-05-03 10:35:17 +0800

    no message

--- www/static/ant-pro/ant-design-pro-template/src/routes/Group/AppList.js
+++ www/static/ant-pro/ant-design-pro-template/src/routes/Group/AppList.js
@@ -19,259 +19,284 @@ import ScrollerX from '../../components/ScrollerX';
 
 @connect(({groupControl}) => ({}), null, null, {pure: false})
 export default class AppList extends PureComponent {
-		constructor(props) {
-				super(props);
-				this.state = {
-						selectedRowKeys: [],
-						apps: [],
-						teamAction: {}
-				}
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedRowKeys: [],
+			apps: [],
+			teamAction: {},
+			current: 1,
+			total: 0,
+			pageSize: 10
 		}
-		componentDidMount() {
-				this.loadApps();
-				this.timer = setInterval(() => {
+	}
+	componentDidMount() {
+		this.loadApps();
+		this.timer = setInterval(() => {
+			this.loadApps();
+		}, 5000)
+	}
+	componentWillUnmount() {
+		clearInterval(this.timer);
+		this
+			.props
+			.dispatch({type: 'groupControl/clearApps'})
+	}
+	loadApps = () => {
+
+		const {dispatch, form, index} = this.props;
+		const team_name = globalUtil.getCurrTeamName();
+		const region_name = globalUtil.getCurrRegionName();
+
+		dispatch({
+			type: 'groupControl/fetchApps',
+			payload: {
+				team_name: team_name,
+				region_name: region_name,
+				group_id: this.props.groupId,
+				page:this.state.current,
+				page_size:10,
+
+			},
+			callback: ((data) => {
+				this.setState({
+					apps: data.list || [],
+					teamAction: data.bean || {},
+					total: data.total
+				})
+			})
+		})
+	}
+	onSelectChange = (selectedRowKeys, selectedRow) => {
+		this.setState({selectedRowKeys: selectedRowKeys});
+	}
+	handleReStart = (data) => {
+		restart({
+			team_name: globalUtil.getCurrTeamName(),
+			app_alias: data.service_alias
+		}).then((data) => {
+			if (data) {
+				notification.success({message: `操作成功，重启中`});
+			}
+		})
+	}
+	handleStart = (data) => {
+		start({
+			team_name: globalUtil.getCurrTeamName(),
+			app_alias: data.service_alias
+		}).then((data) => {
+			if (data) {
+				notification.success({message: `操作成功，启动中`});
+			}
+		})
+	}
+	handleStop = (data) => {
+		stop({
+			team_name: globalUtil.getCurrTeamName(),
+			app_alias: data.service_alias
+		}).then((data) => {
+			if (data) {
+				notification.success({message: `操作成功，关闭中`});
+			}
+		})
+	}
+	getSelected() {
+		var key = this.state.selectedRowKeys;
+		var res = key.map((item, index) => {
+			console.log(index)
+			console.log(key)
+			return this.state.apps[key[index]];
+		})
+		return res;
+		console.log(res)
+	}
+	getSelectedKeys() {
+		var selected = this.getSelected();
+		console.log(selected)
+		return selected.map((item) => {
+			return item.service_id;
+		})
+	}
+	handleBatchRestart = () => {
+		const ids = this.getSelectedKeys();
+		batchReStart({
+			team_name: globalUtil.getCurrTeamName(),
+			serviceIds: ids.join(',')
+		}).then((data) => {
+			if (data) {
+				notification.success({message: `批量重启中`});
+			}
+		})
+	}
+	handleBatchStart = () => {
+		const ids = this.getSelectedKeys();
+		batchStart({
+			team_name: globalUtil.getCurrTeamName(),
+			serviceIds: ids.join(',')
+		}).then((data) => {
+			if (data) {
+				notification.success({message: `批量启动中`});
+			}
+		})
+	}
+	handleBatchStop = () => {
+		const ids = this.getSelectedKeys();
+		batchStop({
+			team_name: globalUtil.getCurrTeamName(),
+			serviceIds: ids.join(',')
+		}).then((data) => {
+			if (data) {
+				notification.success({message: `批量关闭中`});
+			}
+		})
+	}
+	//是否可以批量重启
+	canBatchRestart = () => {
+		const selectedRowKeys = this.getSelected();
+		const hasSelected = selectedRowKeys.length > 0;
+		const canotRestart = selectedRowKeys.filter((item) => {
+			return !appStatusUtil.canRestart(item)
+		})
+		return hasSelected;
+	}
+	//是否可以批量启动
+	canBatchStart = () => {
+		const selectedRowKeys = this.getSelected();
+		const hasSelected = selectedRowKeys.length > 0;
+		const canotStart = selectedRowKeys.filter((item) => {
+			return !appStatusUtil.canStart(item)
+		})
+		return hasSelected;
+	}
+	//是否可以批量关闭
+	canBatchStop = () => {
+		const selectedRowKeys = this.getSelected();
+		const hasSelected = selectedRowKeys.length > 0;
+		const canotStop = selectedRowKeys.filter((item) => {
+			return !appStatusUtil.canStop(item);
+		})
+		return hasSelected;
+	}
+	render() {
+		   
+			const {apps, teamAction} = this.state;
+			const {selectedRowKeys} = this.state;
+			const rowSelection = {
+					selectedRowKeys: selectedRowKeys,
+					onChange: this.onSelectChange
+			};
+			const hasSelected = selectedRowKeys.length > 0;
+			const pagination = {
+				current: this.state.current,
+				total: this.state.total,
+				onChange: (page) => {
+					this.setState({current: page,selectedRowKeys:[]}, () => {
 						this.loadApps();
-				}, 5000)
-		}
-		componentWillUnmount() {
-				clearInterval(this.timer);
-				this
-						.props
-						.dispatch({type: 'groupControl/clearApps'})
-		}
-		loadApps = () => {
-
-				const {dispatch, form, index} = this.props;
-				const team_name = globalUtil.getCurrTeamName();
-				const region_name = globalUtil.getCurrRegionName();
-
-				dispatch({
-						type: 'groupControl/fetchApps',
-						payload: {
-								team_name: team_name,
-								region_name: region_name,
-								group_id: this.props.groupId,
-								page:1,
-								page_size:1000
-						},
-						callback: ((data) => {
-								this.setState({
-										apps: data.list || [],
-										teamAction: data.bean || {}
-								})
-						})
-				})
-		}
-		onSelectChange = (selectedRowKeys, selectedRow) => {
-				this.setState({selectedRowKeys: selectedRow});
-		}
-		handleReStart = (data) => {
-				restart({
-						team_name: globalUtil.getCurrTeamName(),
-						app_alias: data.service_alias
-				}).then((data) => {
-						if (data) {
-								notification.success({message: `操作成功，重启中`});
+					})
+				 }
+			}
+			const columns = [
+				{
+						title: '应用名称',
+						dataIndex: 'service_cname',
+						render: (val, data) => {
+								return <Link to={`/team/${globalUtil.getCurrTeamName()}/region/${globalUtil.getCurrRegionName()}/app/${data.service_alias}/overview`}>{val}</Link>
 						}
-				})
-		}
-		handleStart = (data) => {
-				start({
-						team_name: globalUtil.getCurrTeamName(),
-						app_alias: data.service_alias
-				}).then((data) => {
-						if (data) {
-								notification.success({message: `操作成功，启动中`});
+				}, {
+						title: '应用类型',
+						dataIndex: 'service_type'
+				}, {
+						title: '内存',
+						dataIndex: 'min_memory',
+						render: (val, data) => {
+								return val + 'MB'
 						}
-				})
-		}
-		handleStop = (data) => {
-				stop({
-						team_name: globalUtil.getCurrTeamName(),
-						app_alias: data.service_alias
-				}).then((data) => {
-						if (data) {
-								notification.success({message: `操作成功，关闭中`});
+				}, {
+						title: '状态',
+						dataIndex: 'status_cn',
+						render: (val, data) => {
+								return <Badge status={appUtil.appStatusToBadgeStatus(data.status)} text={val}/>
 						}
-				})
-		}
-		getSelected() {
-				var res = this.state.selectedRowKeys;
-				res = res.map((item) => {
-								return item.service_id;
-						})
-				return res;
-		}
-		handleBatchRestart = () => {
-				const ids = this.getSelected();
-				batchReStart({
-						team_name: globalUtil.getCurrTeamName(),
-						serviceIds: ids.join(',')
-				}).then((data) => {
-						if (data) {
-								notification.success({message: `批量重启中`});
+				}, {
+						title: '更新时间',
+						dataIndex: 'update_time',
+						render: (val) => {
+								return moment(val).format("YYYY-MM-DD HH:mm:ss")
 						}
-				})
-		}
-		handleBatchStart = () => {
-				const ids = this.getSelected();
-				batchStart({
-						team_name: globalUtil.getCurrTeamName(),
-						serviceIds: ids.join(',')
-				}).then((data) => {
-						if (data) {
-								notification.success({message: `批量启动中`});
-						}
-				})
-		}
-		handleBatchStop = () => {
-				const ids = this.getSelected();
-				batchStop({
-						team_name: globalUtil.getCurrTeamName(),
-						serviceIds: ids.join(',')
-				}).then((data) => {
-						if (data) {
-								notification.success({message: `批量关闭中`});
-						}
-				})
-		}
-		//是否可以批量重启
-		canBatchRestart = () => {
-				const {selectedRowKeys} = this.state;
-				const hasSelected = selectedRowKeys.length > 0;
-				const canotRestart = selectedRowKeys.filter((item) => {
-						return !appStatusUtil.canRestart(item)
-				})
-				return hasSelected;
-		}
-		//是否可以批量启动
-		canBatchStart = () => {
-				const {selectedRowKeys} = this.state;
-				const hasSelected = selectedRowKeys.length > 0;
-				const canotStart = selectedRowKeys.filter((item) => {
-						return !appStatusUtil.canStart(item)
-				})
-				return hasSelected;
-		}
-		//是否可以批量关闭
-		canBatchStop = () => {
-				const {selectedRowKeys} = this.state;
-				const hasSelected = selectedRowKeys.length > 0;
-				const canotStop = selectedRowKeys.filter((item) => {
-						return !appStatusUtil.canStop(item);
-				})
-				return hasSelected;
-		}
-		render() {
-				const {apps, teamAction} = this.state;
-				const {selectedRowKeys} = this.state;
-				const rowSelection = {
-						selectedRow: selectedRowKeys,
-						onChange: this.onSelectChange
-				};
-				const hasSelected = selectedRowKeys.length > 0;
-				const columns = [
-						{
-								title: '应用名称',
-								dataIndex: 'service_cname',
-								render: (val, data) => {
-										return <Link to={`/team/${globalUtil.getCurrTeamName()}/region/${globalUtil.getCurrRegionName()}/app/${data.service_alias}/overview`}>{val}</Link>
-								}
-						}, {
-								title: '应用类型',
-								dataIndex: 'service_type'
-						}, {
-								title: '内存',
-								dataIndex: 'min_memory',
-								render: (val, data) => {
-										return val + 'MB'
-								}
-						}, {
-								title: '状态',
-								dataIndex: 'status_cn',
-								render: (val, data) => {
-										return <Badge status={appUtil.appStatusToBadgeStatus(data.status)} text={val}/>
-								}
-						}, {
-								title: '更新时间',
-								dataIndex: 'update_time',
-								render: (val) => {
-										return moment(val).format("YYYY-MM-DD HH:mm:ss")
-								}
-						}, {
-								title: '操作',
-								dataIndex: 'action',
-								render: (val, data) => {
-										if (!appUtil.canManageApp(teamAction)) {
-												return null;
-										}
-										return (
-												<Fragment>
-														{appStatusUtil.canRestart(data)
-																? <a
-																				onClick={() => {
-																				this.handleReStart(data)
-																		}}
-																				href="javascript:;"
-																				style={{
-																				marginRight: 10
-																		}}>重启</a>
-																: null
+				}, {
+						title: '操作',
+						dataIndex: 'action',
+						render: (val, data) => {
+							if (!appUtil.canManageApp(teamAction)) {
+								return null;
+							}
+							return (
+								<Fragment>
+									{appStatusUtil.canRestart(data)
+										? <a
+													onClick={() => {
+													this.handleReStart(data)
+											}}
+													href="javascript:;"
+													style={{
+													marginRight: 10
+											}}>重启</a>
+										: null
 }
-														{appStatusUtil.canStart(data)
-																? <a
-																				onClick={() => {
-																				this.handleStart(data)
-																		}}
-																				href="javascript:;"
-																				style={{
-																				marginRight: 10
-																		}}>启动</a>
-																: null
+									{appStatusUtil.canStart(data)
+											? <a
+															onClick={() => {
+															this.handleStart(data)
+													}}
+															href="javascript:;"
+															style={{
+															marginRight: 10
+													}}>启动</a>
+											: null
 }
-														{appStatusUtil.canStop(data)
-																? <a
-																				onClick={() => {
-																				this.handleStop(data)
-																		}}
-																				href="javascript:;">关闭</a>
-																: null
+									{appStatusUtil.canStop(data)
+											? <a
+															onClick={() => {
+															this.handleStop(data)
+													}}
+															href="javascript:;">关闭</a>
+											: null
 }
 
-												</Fragment>
-										)
-								}
+								</Fragment>
+							)
 						}
-				];
+				}
+			];
 
-				return (
-						<Card style={{
-								minHeight: 400
-						}} bordered={false}>
-							
-								<div
-										style={{
-										display: appUtil.canManageApp(teamAction)
-												? 'block'
-												: 'none'
-								}}
-										className={styles.tableList}>
-										<div className={styles.tableListOperator}>
-												<Button disabled={!this.canBatchRestart()} onClick={this.handleBatchRestart}>
-														批量重启
-												</Button>
-												<Button disabled={!this.canBatchStop()} onClick={this.handleBatchStop}>
-														批量关闭
-												</Button>
-												<Button disabled={!this.canBatchStart()} onClick={this.handleBatchStart}>
-														批量启动
-												</Button>
-										</div>
-								</div>
-							<ScrollerX sm={750}>
-								<Table rowSelection={rowSelection} columns={columns} dataSource={apps || []}/>
-							</ScrollerX>
-						</Card>
-				)
-		}
+			return (
+					<Card style={{
+							minHeight: 400
+					}} bordered={false}>
+						
+							<div
+									style={{
+									display: appUtil.canManageApp(teamAction)
+											? 'block'
+											: 'none'
+							}}
+									className={styles.tableList}>
+									<div className={styles.tableListOperator}>
+										<Button disabled={!this.canBatchRestart()} onClick={this.handleBatchRestart}>
+												批量重启
+										</Button>
+										<Button disabled={!this.canBatchStop()} onClick={this.handleBatchStop}>
+												批量关闭
+										</Button>
+										<Button disabled={!this.canBatchStart()} onClick={this.handleBatchStart}>
+												批量启动
+										</Button>
+									</div>
+							</div>
+						<ScrollerX sm={750}>
+							<Table pagination={pagination}  rowSelection={rowSelection} columns={columns} dataSource={apps || []}/>
+						</ScrollerX>
+					</Card>
+			)
+	}
 }
