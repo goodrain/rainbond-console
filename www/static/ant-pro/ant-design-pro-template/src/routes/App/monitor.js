ccc3a067b8b29bc8a125db864f27c0524c7ea85a
yangyq <yangyq@goodrain.com>
2018-05-08 15:03:26 +0800

    update monitor

--- www/static/ant-pro/ant-design-pro-template/src/routes/App/monitor.js
+++ www/static/ant-pro/ant-design-pro-template/src/routes/App/monitor.js
@@ -66,7 +66,7 @@ class MonitorHistory extends PureComponent {
   }
   getStep() {
     var houer = this.state.houer;
-    return 60*60*houer;
+    return 60*60*houer/20 + 's';
   }
   componentDidMount() {
     this.mounted = true;
