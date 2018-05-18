8a052be5a80a33b49636f72627f416634cecb66a
lichao <lic@goodrain.com>
2018-05-18 16:01:01 +0800

    [FIX] update plugin delete manage

--- console/syncservice/plugin_manage.py
+++ console/syncservice/plugin_manage.py
@@ -20,21 +20,17 @@ class PluginManage(object):
         print tps.count()
         for tp in tps:
             tsprs = TenantServicePluginRelation.objects.filter(plugin_id=tp.plugin_id)
+            tenant = Tenants.objects.get(tenant_id=tp.tenant_id)
             for tspr in tsprs:
                 tss = TenantServiceInfo.objects.filter(service_id=tspr.service_id)
                 if tss:
                     service = tss[0]
+                    print "process {0}".format(service.service_cname)
                     try:
-                        tenant = Tenants.objects.get(tenant_id=service.tenant_id)
+
                         try:
                             region_api.uninstall_service_plugin(service.service_region, tenant.tenant_name, tp.plugin_id,
                                                                 service.service_alias)
-                        except region_api.CallApiError as e:
-                            print e
-                            if e.status != 404:
-                                continue
-                        try:
-                            region_api.delete_plugin(service.service_region, tenant.tenant_name, tp.plugin_id)
                         except region_api.CallApiError as e:
                             print e
                             if e.status != 404:
@@ -47,14 +43,24 @@ class PluginManage(object):
                                                               plugin_id=tp.plugin_id).delete()
                         TenantServicePluginAttr.objects.filter(service_id=service.service_id,
                                                                plugin_id=tp.plugin_id).delete()
-                        PluginConfigGroup.objects.filter(plugin_id=tp.plugin_id).delete()
-                        PluginConfigItems.objects.filter(plugin_id=tp.plugin_id).delete()
-                        PluginBuildVersion.objects.filter(plugin_id=tp.plugin_id).delete()
-                        tp.delete()
+
                     except Exception as e:
                         print e
                         logger.exception(e)
                         continue
+            try:
+                print "process plugin {0}".format(tp.plugin_name)
+                try:
+                    region_api.delete_plugin(tp.region, tenant.tenant_name, tp.plugin_id)
+                except region_api.CallApiError as e:
+                    print e
+                PluginConfigGroup.objects.filter(plugin_id=tp.plugin_id).delete()
+                PluginConfigItems.objects.filter(plugin_id=tp.plugin_id).delete()
+                PluginBuildVersion.objects.filter(plugin_id=tp.plugin_id).delete()
+                tp.delete()
+            except Exception as e:
+                print e
+
 
         print "finished !"
         logger.debug("finished !")
