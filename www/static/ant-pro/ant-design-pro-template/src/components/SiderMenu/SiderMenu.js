6cc4f17a0da8ffbeb92827cd29ecbd40ee3881c7
yangyq <yangyq@goodrain.com>
2018-05-24 15:02:52 +0800

    [FIX] fixed no finance bug

--- www/static/ant-pro/ant-design-pro-template/src/components/SiderMenu/SiderMenu.js
+++ www/static/ant-pro/ant-design-pro-template/src/components/SiderMenu/SiderMenu.js
@@ -5,6 +5,7 @@ import {Link} from 'dva/router';
 import styles from './index.less';
 import globalUtil from '../../utils/global';
 import userUtil from '../../utils/user';
+import teamUtil from '../../utils/team';
 
 const {Sider} = Layout;
 const {SubMenu} = Menu;
@@ -258,10 +259,10 @@ export default class SiderMenu extends PureComponent {
        var region_name = globalUtil.getCurrRegionName();
        var region  = userUtil.hasTeamAndRegion(user, team_name, region_name);
        if(region){
-         //当前是公有数据中心
-         if(region.region_scope === 'public' && (team.identity === 'owner' || team.identity === 'admin')){
-            return ItemDom;
-         }
+          //当前是公有数据中心
+          if(region.region_scope === 'public' && (teamUtil.canViewFinance(team))){
+             return ItemDom;
+          }
        }
        return null;
     } else {
