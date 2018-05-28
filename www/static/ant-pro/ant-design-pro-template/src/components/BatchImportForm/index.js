404e85757c838480ede478b243638ea8c959c4ac
wuwen <wuwen@172.16.0.110>
2018-05-28 09:59:57 +0800

    [add] add import  test

--- www/static/ant-pro/ant-design-pro-template/src/components/BatchImportForm/index.js
+++ www/static/ant-pro/ant-design-pro-template/src/components/BatchImportForm/index.js
@@ -13,7 +13,8 @@ import {
 		notification,
 		Select,
 		Input,
-		Modal
+		Modal,
+		message
 } from 'antd';
 import globalUtil from '../../utils/global';
 
@@ -25,23 +26,66 @@ export default class Index extends PureComponent {
 		constructor(props) {
 			super(props);
 			this.state = {
-					
+				fileList:[]
 			}
 		}
 		handleSubmit = ()=>{
-			
+			const event_id = this.props.event_id;
+			const source_dir = this.props.source_dir;
+			this
+			.props
+			.dispatch({
+				type: 'createApp/importApp',
+				payload: {
+					team_name: globalUtil.getCurrTeamName(),
+					scope: 'enterprise',
+					event_id: event_id,
+					file_name: this.state.file_name
+				},
+				callback: ((data) => {
+					message.success('操作成功，正在导入',2);
+					// this.props.onOk && this.props.onOk(data);
+				})
+        	})
+		}
+
+		handleQueryImportDir = ()=>{
+			const event_id = this.props.event_id;
+			const source_dir = this.props.source_dir;
+			this
+			.props
+			.dispatch({
+				type: 'createApp/queryImportDirApp',
+				payload: {
+					team_name: globalUtil.getCurrTeamName(),
+					event_id:event_id
+				},
+				callback: ((data) => {
+					this.setState({fileList:data.list},()=>{
+						 if(this.state.fileList.length >0){
+							 this.props.onOk && this.props.onOk(this.state.fileList)
+						 }else{
+							message.info('您还没有放入文件，请先放入文件！',2);
+						 }
+					});
+
+				})
+			})
 		}
 		render() {
-			 	const {onCancel} = this.props;
-				return (
-					<Modal
-						visible={true}
-						onCancel={onCancel}
-						onOk={this.handleSubmit}
-						title="批量导入应用"
-						okText="导入"
-						>
-					</Modal>
-				)
+			const event_id = this.props.event_id;
+			const source_dir = this.props.source_dir;
+			return (
+				<Modal
+					visible={true}
+					onCancel={this.props.onCancel}
+					onOk={this.handleQueryImportDir}
+					title="批量导入应用"
+					>
+					<div>
+						<p>请将要上传的文件，放入<strong>{source_dir}</strong>目录，然后点击“确认”按钮</p>
+					</div>
+				</Modal>
+			)
 		}
 }
