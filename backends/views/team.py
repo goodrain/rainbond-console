59ca95553187ffaf6f0afe5b4914d5d92d15ba4d
zhoujunhao <18853925545@163.com>
2018-05-30 18:38:18 +0800

    [FIX] fix backends manage

--- backends/views/team.py
+++ backends/views/team.py
@@ -6,6 +6,7 @@ from backends.services.exceptions import *
 from backends.services.resultservice import *
 from backends.services.tenantservice import tenant_service
 from backends.services.userservice import user_service
+from backends.services.regionservice import region_service
 from console.services.team_services import team_services as console_team_service
 from base import BaseAPIView
 from goodrain_web.tools import JuncheePaginator
@@ -15,7 +16,6 @@ from console.services.user_services import user_services as console_user_service
 from console.services.perm_services import perm_services as console_perm_service
 from console.services.region_services import region_services as console_region_service
 from django.db import transaction
-from backends.services.regionservice import region_service
 
 logger = logging.getLogger("default")
 
@@ -23,7 +23,7 @@ logger = logging.getLogger("default")
 class AllTeamView(BaseAPIView):
     def get(self, request, *args, **kwargs):
         """
-        获取所有租户信息
+        获取团队信息
         ---
         parameters:
             - name: page_num
@@ -36,14 +36,53 @@ class AllTeamView(BaseAPIView):
               required: false
               type: string
               paramType: query
+            - name: enterprise_alias
+              description: 企业别名
+              required: false
+              type: string
+              paramType: query
+            - name: tenant_alias
+              description: 团队别名
+              required: false
+              type: string
+              paramType: query
+            - name: tenant_name
+              description: 团队名称
+              required: false
+              type: string
+              paramType: query
 
         """
         try:
             page = request.GET.get("page_num", 1)
             page_size = request.GET.get("page_size", 20)
-            tenant_list = tenant_service.get_all_tenants()
+            enterprise_alias = request.GET.get("enterprise_alias", None)
+            tenant_alias = request.GET.get("tenant_alias", None)
+            tenant_name = request.GET.get("tenant_name", None)
+            enterprise_id = None
+            if enterprise_alias:
+                enter = enterprise_services.get_enterprise_by_enterprise_alias(enterprise_alias)
+                if not enter:
+                    return Response(
+                        generate_result("0404", "enterprise is not found", "企业{0}不存在".format(enterprise_alias)))
+                enterprise_id = enter.enterprise_id
+
+            if tenant_alias:
+                team = console_team_service.get_team_by_team_alias(tenant_alias)
+                if not team:
+                    return Response(
+                        generate_result("0404", "team is not found", "团队别名{0}不存在".format(tenant_alias)))
+
+            if tenant_name:
+                team = console_team_service.get_tenant_by_tenant_name(tenant_name)
+                if not team:
+                    return Response(
+                        generate_result("0404", "team is not found", "团队名称{0}不存在".format(tenant_name)))
+
+            tenant_list = tenant_service.get_team_by_name_or_alias_or_enter(tenant_name, tenant_alias, enterprise_id)
             tenant_paginator = JuncheePaginator(tenant_list, int(page_size))
             tenants = tenant_paginator.page(int(page))
+
             tenants_num = Tenants.objects.count()
             # 需要license控制，现在没有，默认为一百万
             allow_num = 1000000
@@ -52,9 +91,9 @@ class AllTeamView(BaseAPIView):
 
             for tenant in tenants:
                 tenant_dict = {}
-                user_list = tenant_service.get_tenant_users(tenant["tenant_name"])
+                user_list = tenant_service.get_tenant_users(tenant.tenant_name)
                 tenant_dict["user_num"] = len(user_list)
-                tenant_dict.update(tenant)
+                tenant_dict.update(tenant.to_dict())
                 list.append(tenant_dict)
             bean = {"total_tenant_num": allow_num, "cur_tenant_num": tenants_num}
 
@@ -169,7 +208,7 @@ class TeamView(BaseAPIView):
             tenant = tenant_service.get_tenant(tenant_name)
             user_list = tenant_service.get_users_by_tenantID(tenant.ID)
             user_num = len(user_list)
-            rt_list = [{"tenant_id": tenant.tenant_id, "tenant_name": tenant.tenant_name, "user_num": user_num}]
+            rt_list = [{"tenant_id": tenant.tenant_id, "tenant_name": tenant.tenant_name, "user_num": user_num,"tenant_alias":tenant.tenant_alias}]
             result = generate_result("0000", "success", "查询成功", list=rt_list)
         except Tenants.DoesNotExist as e:
             logger.exception(e)
@@ -270,6 +309,7 @@ class AddTeamUserView(BaseAPIView):
             result = generate_result("9999", "system error", "系统异常")
         return Response(result)
 
+
 class TeamUsableRegionView(BaseAPIView):
 
     def get(self, request, tenant_name, *args, **kwargs):
@@ -301,4 +341,3 @@ class TeamUsableRegionView(BaseAPIView):
             logger.exception(e)
             result = generate_result("9999", "system error", "系统异常")
         return Response(result)
-
