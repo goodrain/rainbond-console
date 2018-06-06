08dffefd6f0e5bcdb27d2c3a50007c6152f5acb2
yangyq <yangyq@goodrain.com>
2018-06-06 14:08:29 +0800

    Update my plugins

--- www/static/ant-pro/ant-design-pro-template/src/routes/Plugin/index.js
+++ www/static/ant-pro/ant-design-pro-template/src/routes/Plugin/index.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React, {PureComponent, Fragment} from 'react';
 import {connect} from 'dva';
 import {Card, Button, Icon, List} from 'antd';
 import {Link, routerRedux} from 'dva/router';
@@ -12,119 +12,97 @@ import Ellipsis from '../../components/Ellipsis';
 import Manage from './manage';
 import ConfirmModal from '../../components/ConfirmModal';
 import NoPermTip from '../../components/NoPermTip';
-const ButtonGroup = Button.Group;
 
 
-class MartketPlugin extends PureComponent {
+class MarketPlugin extends PureComponent {
   constructor(props){
-      super(props);
-      this.state = {
-          pageSize:10,
-          total:0,
-          page:1,
-          loading: false,
-          plugins: []
-      }
+     super(props);
+     this.state = {
+        list: null
+     }
   }
-  componentDidMount = () => {
-      this.loadPlugins();
+  componentDidMount(){
+      this.fetchPlugins();
   }
-  handleClose = () => {
-      this.props.onClose && this.props.onClose();
-  }
-  handleSearch = (app_name) => {
-      this.setState({
-          app_name: app_name,
-          page: 1
-      }, () => {
-          this.loadPlugins();
-      })
-  }
-  loadPlugins = () => {
-      this.setState({
-          loading: true
-      }, () => {
-          this
-              .props
-              .dispatch({
-                  type: 'plugin/getUnInstalledPlugin',
-                  payload: {
-                      plugin_name: this.state.app_name,
-                      page: this.state.page,
-                      pageSize: this.state.pageSize
-                  },
-                  callback: (data) => {
-                      this.setState({
-                          plugins: data.list || [],
-                          loading: false,
-                          total: data.total
-                      })
-                  }
-              })
-      })
-  }
-  handleInstall= (data) => {
-      this
-          .props
-          .dispatch({
-              type: 'plugin/installMarketPlugin',
-              payload: {
-                  plugin_id: data.id
-              },
-              callback: (data) => {
-                  notification.success({message: '操作成功'});
-                  this.loadPlugins();
-                  this.props.onInstallSuccess && this.props.onInstallSuccess();
-              }
+  fetchPlugins = () => {
+    this
+      .props
+      .dispatch({
+        type: 'plugin/getUnInstalledPlugin',
+        payload: {
+            page: 1,
+            limit: 1000
+        },
+        callback: ((data) => {
+          this.setState({
+            list: data.list || []
           })
+        })
+      });
   }
-  handlePageChange = (page) => {
-      this.state.page = page;
-      this.loadPlugins();
+  handleInstall = (data) => {
+     this.props.dispatch({
+        type: 'plugin/installMarketPlugin',
+        payload: {
+           plugin_id: data.id
+        },
+        callback: (data) => {
+             Notification.success({
+               message: '安装成功'
+             });
+             this.fetchPlugins();
+             this.props.onInstallSuccess && this.props.onInstallSuccess()
+        }
+     })
+  }
+  renderTmp = () => {
+       var list = this.state.list;
+       if(!list) {
+           return <p style={{}}><Icon type="loading" /></p>
+       }
+
+       list = list.filter((item) => {
+           return !item.is_installed && item.is_complete
+       })
+
+       return <List
+       rowKey="id"
+       grid={{
+       gutter: 24,
+       lg: 3,
+       md: 2,
+       sm: 1,
+       xs: 1
+     }}
+       dataSource={list}
+       renderItem={item => (<List.Item
+           key={item.id}
+           >
+           <Card
+             className={styles.card}
+             actions={[<span onClick={() => {this.handleInstall(item)}}>安装</span>]}>
+             <Card.Meta
+               style={{height: 99, overflow: 'hidden'}}
+               avatar={< Icon style = {{fontSize: 50, color:'rgba(0, 0, 0, 0.2)'}}type = "api" />}
+               title={item.plugin_name}
+               description={(
+               <Ellipsis className={styles.item} lines={3}>< p style={{ display: 'block',color:'rgb(220, 220, 220)', marginBottom:8}} > {
+                 pluginUtil.getCategoryCN(item.category)
+               } < /p>{item.desc}</Ellipsis>
+             )}/>
+           </Card>
+         </List.Item>
+       )}/>
   }
   render(){
-      const list = this.state.plugins || [];
-      const paginationProps = {
-          pageSize: this.state.pageSize,
-          total: this.state.total,
-          current: this.state.page,
-          onChange: (pageSize) => {
-              this.handlePageChange(pageSize)
-          }
-      };
-      return <List
-                rowKey="id"
-                grid={{
-                gutter: 24,
-                lg: 2,
-                md: 2,
-                sm: 1,
-                xs: 1
-              }}
-                dataSource={list}
-                renderItem={item => ((
-                  <List.Item
-                    key={item.id}
-                    >
-                    <Card
-          
-                      className={styles.card}
-                      actions={[item.is_installed ? <span>已安装</span> : <span onClick={()=>{this.handleInstall(item)}}>安装</span>]}>
-                      <Card.Meta
-                        style={{height: 99, overflow: 'hidden'}}
-                        avatar={< Icon style = {{fontSize: 50, color:'rgba(0, 0, 0, 0.2)'}}type = "api" />}
-                        title={item.plugin_name}
-                        description={(
-                        <Ellipsis className={styles.item} lines={3}>< p style={{ display: 'block',color:'rgb(220, 220, 220)', marginBottom:8}} > {
-                          pluginUtil.getCategoryCN(item.category)
-                        } < /p>{item.desc}</Ellipsis>
-                      )}/>
-                    </Card>
-                  </List.Item>
-                ))}/>
+    const list = this.state.list || [];
+    return (
+      <div className={styles.cardList}>
+         {this.renderTmp()}
+      </div>
+    )
   }
 }
-
 
 @connect(({list, loading}) => ({}))
 class PluginList extends PureComponent {
@@ -146,8 +124,7 @@ class PluginList extends PureComponent {
         desc: '实时分析应用的吞吐率、响应时间、在线人数等指标',
         plugin_alias: '服务实时性能分析',
         hasInstall: false
-      },
-      showMarketPlugin: false
+      }
     }
     this.timer = null;
   }
@@ -257,81 +234,66 @@ class PluginList extends PureComponent {
         </p>
       </div>
     );
+
     const extraContent = (
       <div className={styles.extraImg}></div>
     );
+
     return (
       <PageHeaderLayout title="我的插件" content={content} extraContent={extraContent}>
-          <div style={{marginBottom:'10px',textAlign:'right'}}>
-            <ButtonGroup>
-              <Button onClick={this.handleCreate}><Icon type="plus" />新建插件</Button>
-              {!this.state.showMarketPlugin && <Button onClick={() => {this.setState({showMarketPlugin: true})}}><Icon type="arrow-down" />从内部市场安装</Button>}
-              {this.state.showMarketPlugin && <Button onClick={() => {this.setState({showMarketPlugin: false})}}><Icon type="close" />隐藏内部市场</Button>}
-            </ButtonGroup>
-          </div>
-        <div  style={{display: this.state.showMarketPlugin ? 'flex' : 'block',position:'relative', overflow: 'hidden'}}>
-            <div className={styles.cardList}
-                style={{
-                  marginTop: 14,
-                  transition: 'all .8s',
-                  width: this.state.showMarketPlugin ? '50%' : '100%',
-                  display: 'inline-block'
-              }}
-            >
-              <List
-                rowKey="id"
-                grid={this.state.showMarketPlugin ? {
-                  gutter: 24,
-                  lg: 2,
-                  md: 2,
-                  sm: 1,
-                  xs: 1
-                } : {
-                gutter: 24,
-                lg: 3,
-                md: 2,
-                sm: 1,
-                xs: 1
-              }}
-                dataSource={list}
-                renderItem={item => ((
-                  <List.Item
-                    key={item.id}
-                    >
-                    <Card
-          
-                      className={styles.card}
-                      actions={this.getAction(item)}>
-                      <Card.Meta
-                        style={{height: 99, overflow: 'hidden'}}
-                        avatar={< Icon style = {{fontSize: 50, color:'rgba(0, 0, 0, 0.2)'}}type = "api" />}
-                        title={this.getItemTitle(item)}
-                        description={(
-                        <Ellipsis className={styles.item} lines={3}>< p style={{ display: 'block',color:'rgb(220, 220, 220)', marginBottom:8}} > {
-                          pluginUtil.getCategoryCN(item.category)
-                        } < /p>{item.desc}</Ellipsis>
-                      )}/>
-                    </Card>
-                  </List.Item>
-                ))}/>
-            </div>
-
-            <div className={styles.cardList} style={{
-                    transition: 'all .8s',
-                    transform: this.state.showMarketPlugin ? 'translate3d(0, 0, 0)' : 'translate3d(100%, 0, 0)',
-                    marginLeft:8,
-                    width: '49%'
-                }}>
-                {this.state.showMarketPlugin && <MartketPlugin dispatch={this.props.dispatch} />}
-            </div>
-        </div>
-        
+        <div className={styles.cardList}>
+          <List
+            rowKey="id"
+            grid={{
+            gutter: 24,
+            lg: 3,
+            md: 2,
+            sm: 1,
+            xs: 1
+          }}
+            dataSource={[
+            '', ...list
+          ]}
+            renderItem={item => (item
+            ? (
+              <List.Item
+                key={item.id}
+                >
+                <Card
+       
+                  className={styles.card}
+                  actions={this.getAction(item)}>
+                  <Card.Meta
+                    style={{height: 100, overflow: 'hidden'}}
+                    avatar={< Icon style = {{fontSize: 50, color:'rgba(0, 0, 0, 0.2)'}}type = "api" />}
+                    title={this.getItemTitle(item)}
+                    description={(
+                    <Ellipsis className={styles.item} lines={3}>< p style={{ display: 'block',color:'rgb(220, 220, 220)', marginBottom:8}} > {
+                      pluginUtil.getCategoryCN(item.category)
+                    } < /p>{item.desc}</Ellipsis>
+                  )}/>
+                </Card>
+              </List.Item>
+            )
+            : (
+              <List.Item>
+                <Button type="dashed" onClick={this.handleCreate} className={styles.newButton}>
+                  <Icon type="plus"/>
+                  新建插件
+                </Button>
+              </List.Item>
+            ))}/>
             {this.state.deletePlugin && <ConfirmModal
           onOk={this.hanldeDeletePlugin}
           onCancel={this.cancelDeletePlugin}
           title="删除插件"
           desc="确定要删除此插件吗？"/>}
-        
+        </div>
+        <dl style={{paddingTop: 32, marginTop: 32}}>
+            <dt style={{marginBottom: '16px', fontSize: '18px'}}>从内部市场安装</dt>
+            <dd><MarketPlugin dispatch={this.props.dispatch} onInstallSuccess={this.fetchPlugins} /></dd>
+            
+        </dl>
         
       </PageHeaderLayout>
     );
@@ -355,4 +317,5 @@ class Index extends PureComponent {
     }
   }
 }
+
 export default Index;
