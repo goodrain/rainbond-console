cbafe1a8fe82e04530ec424be5c93ba9a238b129
wuwen <wuwen@172.16.0.110>
2018-06-19 09:31:17 +0800

    [add] add port

--- www/static/ant-pro/ant-design-pro-template/src/routes/Finance/invoice-info.js
+++ www/static/ant-pro/ant-design-pro-template/src/routes/Finance/invoice-info.js
@@ -19,169 +19,104 @@ export default class BasicList extends PureComponent {
   constructor(props){
       super(props);
       this.state = {
-          date: moment(new Date().getTime()).format('YYYY-MM-DD'),
-          companyInfo: {},
-          disk:{},
-          memory:{},
-          list:[],
+         selectedRowKeys: []
           
       }
   }
   componentDidMount() {
-      this.getCompanyInfo();
-      this.getRegionResource();
-      this.getRegionOneDayMoney();
+     
   }
-  getRegionResource(){
-    this.props.dispatch({
-      type: 'global/getRegionSource',
-      payload:{
-         team_name: globalUtil.getCurrTeamName(),
-         enterprise_id: this.props.user.enterprise_id,
-         region: globalUtil.getCurrRegionName()
-      },
-      callback: (data) => {
-         this.setState({memory:data.bean.memory || {}, disk: data.bean.disk})
-      }
-    })
-  }
-  getCompanyInfo = () => {
-     this.props.dispatch({
-       type: 'global/getCompanyInfo',
-       payload:{
-          team_name: globalUtil.getCurrTeamName(),
-          enterprise_id: this.props.user.enterprise_id
-       },
-       callback: (data) => {
-          this.setState({companyInfo: data.bean})
-          console.log(data)
-       }
-     })
-  }
-  getRegionOneDayMoney = () => {
-    this.props.dispatch({
-      type: 'global/getRegionOneDayMoney',
-      payload:{
-         team_name: globalUtil.getCurrTeamName(),
-         enterprise_id: this.props.user.enterprise_id,
-         date: this.state.date,
-         region: globalUtil.getCurrRegionName()
-      },
-      callback: (data) => {
-         this.setState({list: data.list || []})
-      }
-    })
-  }
-  handleDateChange = (date, str) => {
-    this.setState({date: str}, () => {
-       this.getRegionOneDayMoney();
-    })
-  }
+ 
   render() {
     const { loading } = this.props;
-    const list = this.state.list || [];
-    console.log(list)
-    const Info = ({ title, value, bordered }) => (
-      <div className={styles.headerInfo}>
-        <span>{title}</span>
-        <p>{value}</p>
-        {bordered && <em />}
-      </div>
-    );
-
+    const { selectedRowKeys } = this.state;
     const extraContent = (
       <div className={styles.extraContent}>
-        <DatePicker onChange={this.handleDateChange} allowClear={false} defaultValue={moment(this.state.date, "YYYY-MM-DD")} />
+          
       </div>
     );
-
     const columns = [{
-        title: '时间',
-        dataIndex: 'time',
-        key: 'time',
-      },{
-        title: '内存费用',
-        dataIndex: 'memory_fee',
-        key: 'memory_fee',
-        render: (v,data) => {
-           return ( 
-                  data.memory_limit  === '0'?
-                  <Tooltip placement="topLeft" title={'已使用内存' +  data.memory_usage + 'GB，已超出内存' + data.memory_over + '(GB)'}>
-                    {v + '元'}
-                  </Tooltip>
-                  :
-                  <Tooltip placement="topLeft" title={'包月内存'+ data.memory_limit  +'(GB)，已使用内存' + data.memory_usage +'GB，已超出内存' + data.memory_over + '(GB)'}>
-                   {v + '元'}
-                  </Tooltip>
-              )
-        }
-      }, {
-        title: '磁盘费用',
-        dataIndex: 'disk_fee',
-        key: 'disk_fee',
-        render: (v,data) => {
-           return ( 
-              data.disk_limit  === '0'?
-              <Tooltip placement="topLeft" title={'已使用磁盘' + data.disk_usage +'GB，已超出磁盘' + data.disk_over + '(GB)'}>
-               { v + '元'}
-              </Tooltip>
-              :
-              <Tooltip placement="topLeft" title={'包月磁盘'+ data.disk_limit +'(GB)，已使用磁盘' + data.disk_usage +'GB，已超出磁盘' + data.disk_over + '(GB)'}>
-               {v + '元'}
-              </Tooltip>
-          )
-        }
-      }, {
-        title: '费用',
-        dataIndex: 'net_fee',
-        key: 'net_fee',
-        render: (v,data) => {
-          return ( 
-              <Tooltip placement="topLeft" title={'已使用流量' + data.net_usage +'(GB)'}>
-                { v + '元'}
-              </Tooltip>
-             )
-        }
-      }, {
-        title: '总费用',
-        dataIndex: 'total_fee',
-        key: 'total_fee',
-        render: (v,data) => {
-           return v + '元'
-        }
-      }];
+      title: '申请时间',
+      dataIndex: 'time',
+    }, {
+      title: '发票类型',
+      dataIndex: 'type',
+    }, {
+      title: '抬头',
+      dataIndex: 'company',
+    },
+    {
+      title: '发票金额',
+      dataIndex: 'money',
+    },{
+      title: '发票内容',
+      dataIndex: 'content',
+    },{
+      title: '状态',
+      dataIndex: 'zt',
+    },{
+      title: '快递编号',
+      dataIndex: 'bh',
+    },{
+      title: '操作',
+     	dataIndex: 'action',
+			render: (val, data) => {
+							return (
+                <a href="javascript:;">查看</a>
+							)
+						}
+    }];
+    const data = [];
+    for (let i = 0; i < 20; i++) {
+      data.push({
+        key: i,
+        time: `${i}`,
+        type: '10000',
+        company: `alipay`,
+        money:i,
+        content: `${i}`,
+        money: '10000',
+        zt: `alipay`,
+        bh:i,
+        cz:i
+      });
+    }
+    const pageHeaderContent = (
+        <Button style={{float:'right'}}><a target="_blank" href="https://www.goodrain.com/spa/#/personalCenter/my/recharge">发票申请</a></Button>
+    );
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
 
-    var money = `${this.state.companyInfo.balance || 0} 元`;
-    var entId = this.state.companyInfo.ent_id;
-    console.log(this.state.companyInfo)
-    if(this.state.companyInfo.owed_amt > 0){
-       money = `欠费 ${this.state.companyInfo.owed_amt} 元`;
-    }
-    var regionName = globalUtil.getCurrRegionName();
-    let regionId = '';
-    if(regionName == 'ali-hz') {
-       regionId = 2;
-    }
-
-    if(regionName == 'ali-sh'){
-       regionId = 1;
-    }
-    
     return (
-      <PageHeaderLayout>
+      <PageHeaderLayout
+        breadcrumbList={[{
+          title: "首页",
+          href: `/`
+        }, {
+          title: "财务中心",
+          href: ``
+        }, {
+          title: "发票查询",
+          href: ``
+        }]}
+        title={"发票查询"}
+        content={pageHeaderContent}
+      >
         <div className={styles.standardList}>
 
           <Card
             className={styles.listCard}
             bordered={false}
-            title="当前数据中心每小时资源费用"
+            title="发票查询"
             style={{ marginTop: 24 }}
             bodyStyle={{ padding: '0 32px 40px 32px' }}
             extra={extraContent}
           >
-          
-            
+            <Table  dataSource={data} columns={columns} />
           </Card>
+          
         </div>
       </PageHeaderLayout>
     );
--- www/static/ant-pro/ant-design-pro-template/src/routes/Finance/invoice.js
+++ www/static/ant-pro/ant-design-pro-template/src/routes/Finance/invoice.js
@@ -5,6 +5,7 @@ import { Table, Card, Row, Col, Radio, Input, Button, Icon, DatePicker, Tooltip,
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import styles from '../List/BasicList.less';
 import globalUtil from '../../utils/global';
+import InvoiceForm from '../../components/InvoiceForm';
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
@@ -19,200 +20,86 @@ export default class BasicList extends PureComponent {
   constructor(props){
       super(props);
       this.state = {
-          date: moment(new Date().getTime()).format('YYYY-MM-DD'),
-          companyInfo: {},
-          disk:{},
-          memory:{},
-          list:[],
+         selectedRowKeys: []
           
       }
   }
   componentDidMount() {
-      this.getCompanyInfo();
-      this.getRegionResource();
-      this.getRegionOneDayMoney();
+     
   }
-  getRegionResource(){
-    this.props.dispatch({
-      type: 'global/getRegionSource',
-      payload:{
-         team_name: globalUtil.getCurrTeamName(),
-         enterprise_id: this.props.user.enterprise_id,
-         region: globalUtil.getCurrRegionName()
-      },
-      callback: (data) => {
-         this.setState({memory:data.bean.memory || {}, disk: data.bean.disk})
-      }
-    })
-  }
-  getCompanyInfo = () => {
-     this.props.dispatch({
-       type: 'global/getCompanyInfo',
-       payload:{
-          team_name: globalUtil.getCurrTeamName(),
-          enterprise_id: this.props.user.enterprise_id
-       },
-       callback: (data) => {
-          this.setState({companyInfo: data.bean})
-          console.log(data)
-       }
-     })
-  }
-  getRegionOneDayMoney = () => {
-    this.props.dispatch({
-      type: 'global/getRegionOneDayMoney',
-      payload:{
-         team_name: globalUtil.getCurrTeamName(),
-         enterprise_id: this.props.user.enterprise_id,
-         date: this.state.date,
-         region: globalUtil.getCurrRegionName()
-      },
-      callback: (data) => {
-         this.setState({list: data.list || []})
-      }
-    })
-  }
-  handleDateChange = (date, str) => {
-    this.setState({date: str}, () => {
-       this.getRegionOneDayMoney();
-    })
+  onSelectChange = (selectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys });
   }
   render() {
     const { loading } = this.props;
-    const list = this.state.list || [];
-    console.log(list)
-    const Info = ({ title, value, bordered }) => (
-      <div className={styles.headerInfo}>
-        <span>{title}</span>
-        <p>{value}</p>
-        {bordered && <em />}
-      </div>
-    );
-
+    const { selectedRowKeys } = this.state;
     const extraContent = (
       <div className={styles.extraContent}>
         <DatePicker onChange={this.handleDateChange} allowClear={false} defaultValue={moment(this.state.date, "YYYY-MM-DD")} />
       </div>
     );
-
     const columns = [{
-        title: '时间',
-        dataIndex: 'time',
-        key: 'time',
-      },{
-        title: '内存费用',
-        dataIndex: 'memory_fee',
-        key: 'memory_fee',
-        render: (v,data) => {
-           return ( 
-                  data.memory_limit  === '0'?
-                  <Tooltip placement="topLeft" title={'已使用内存' +  data.memory_usage + 'GB，已超出内存' + data.memory_over + '(GB)'}>
-                    {v + '元'}
-                  </Tooltip>
-                  :
-                  <Tooltip placement="topLeft" title={'包月内存'+ data.memory_limit  +'(GB)，已使用内存' + data.memory_usage +'GB，已超出内存' + data.memory_over + '(GB)'}>
-                   {v + '元'}
-                  </Tooltip>
-              )
-        }
-      }, {
-        title: '磁盘费用',
-        dataIndex: 'disk_fee',
-        key: 'disk_fee',
-        render: (v,data) => {
-           return ( 
-              data.disk_limit  === '0'?
-              <Tooltip placement="topLeft" title={'已使用磁盘' + data.disk_usage +'GB，已超出磁盘' + data.disk_over + '(GB)'}>
-               { v + '元'}
-              </Tooltip>
-              :
-              <Tooltip placement="topLeft" title={'包月磁盘'+ data.disk_limit +'(GB)，已使用磁盘' + data.disk_usage +'GB，已超出磁盘' + data.disk_over + '(GB)'}>
-               {v + '元'}
-              </Tooltip>
-          )
-        }
-      }, {
-        title: '流量费用',
-        dataIndex: 'net_fee',
-        key: 'net_fee',
-        render: (v,data) => {
-          return ( 
-              <Tooltip placement="topLeft" title={'已使用流量' + data.net_usage +'(GB)'}>
-                { v + '元'}
-              </Tooltip>
-             )
-        }
-      }, {
-        title: '总费用',
-        dataIndex: 'total_fee',
-        key: 'total_fee',
-        render: (v,data) => {
-           return v + '元'
-        }
-      }];
+      title: '订单号',
+      dataIndex: 'order',
+    }, {
+      title: '储值金额',
+      dataIndex: 'money',
+    }, {
+      title: '储值方式',
+      dataIndex: 'type',
+    },
+    {
+      title: '订单时间',
+      dataIndex: 'time',
+    }];
+    const data = [];
+    for (let i = 0; i < 20; i++) {
+      data.push({
+        key: i,
+        order: `${i}`,
+        money: '10000',
+        type: `alipay`,
+        time:i
+      });
+    }
+    const pageHeaderContent = (
+        <Button style={{float:'right'}}><a target="_blank" href="https://www.goodrain.com/spa/#/personalCenter/my/recharge">发票查询</a></Button>
+    );
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
 
-    var money = `${this.state.companyInfo.balance || 0} 元`;
-    var entId = this.state.companyInfo.ent_id;
-    console.log(this.state.companyInfo)
-    if(this.state.companyInfo.owed_amt > 0){
-       money = `欠费 ${this.state.companyInfo.owed_amt} 元`;
-    }
-    var regionName = globalUtil.getCurrRegionName();
-    let regionId = '';
-    if(regionName == 'ali-hz') {
-       regionId = 2;
-    }
-
-    if(regionName == 'ali-sh'){
-       regionId = 1;
-    }
-    
     return (
-      <PageHeaderLayout>
+      <PageHeaderLayout
+        breadcrumbList={[{
+          title: "首页",
+          href: `/`
+        }, {
+          title: "财务中心",
+          href: ``
+        }, {
+          title: "发票申请",
+          href: ``
+        }]}
+        title={"发票申请"}
+        content={pageHeaderContent}
+      >
         <div className={styles.standardList}>
-          <Card bordered={false}>
-            <Row>
-              <Col sm={8} xs={24}>
-                    <Info title="企业账户余额" value={money} bordered />
-              </Col>
-              <Tooltip title={`过期时间：${this.state.memory.expire_date || '-'}`}>
-              <Col sm={8} xs={24}>
-                    <Info title="数据中心内存 (已使用/总资源)" value={`${this.state.memory.used || 0}/${this.state.memory.limit || 0} M`} bordered />
-              </Col>
-              </Tooltip>
-              <Tooltip title={`过期时间：${this.state.disk.expire_date || '-'}`}>
-              <Col sm={8} xs={24}>
-                    <Info title="数据中心磁盘 (已使用/总资源)" value={`${this.state.disk.used || 0}/${this.state.disk.limit || 0} G`} />
-              </Col>
-              </Tooltip>
-            </Row>
-          </Card>
-
-          <div style={{textAlign: 'right', paddingTop: 24}}>
-
-            <Button.Group>
-            {(regionId && !entId) && <Button type="primary"><a target="_blank" href={`https://www.goodrain.com/spa/#/resBuy/${regionId}`}>购买资源</a></Button>}
-            {(entId && regionId)&& <Button type="primary"><a target="_blank" href={`https://www.goodrain.com/spa/#/resBuy/${regionId}/${entId}`}>购买资源</a></Button>}
-            <Button><a target="_blank" href="https://www.goodrain.com/spa/#/personalCenter/my/recharge">账户充值</a></Button>
-            <Dropdown overlay={<Menu>
-                      <Menu.Item>
-                        <a target="_blank" href="https://www.goodrain.com/spa/#/personalCenter/my/mydc-history">查看消费记录</a>
-                      </Menu.Item>
-          </Menu>}><Button>更多...</Button></Dropdown>
-            </Button.Group>
-          </div>
 
           <Card
             className={styles.listCard}
             bordered={false}
-            title="当前数据中心每小时资源费用"
+            title="选择开票订单"
             style={{ marginTop: 24 }}
             bodyStyle={{ padding: '0 32px 40px 32px' }}
             extra={extraContent}
           >
-            <Table dataSource={list} columns={columns} />
-            
+            <Table rowSelection={rowSelection} dataSource={data} columns={columns} />
+            <div style={{textAlign:'center'}}><Button type="primary">申请开票</Button></div>
           </Card>
+          <InvoiceForm/>
         </div>
       </PageHeaderLayout>
     );
