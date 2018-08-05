3baa728e86ab84458b4ba48b153748c1ce78b158
朱峰 <zhuf_py@163.com>
2018-08-05 13:37:51 +0800

    [FIX] fix None_type error@deploy

--- console/repositories/deploy_repo.py
+++ console/repositories/deploy_repo.py
@@ -9,7 +9,7 @@ from console.models import DeployRelation
 
 class DeployRepo(object):
     def get_deploy_relation_by_service_id(self, service_id):
-        deploy = DeployRelation.objects.filter(service_id=service_id)
+        deploy = DeployRelation.objects.get(service_id=service_id)
         if not deploy:
             secretkey = ''.join(random.sample(string.ascii_letters + string.digits, 8))
             secret_key = base64.b64encode(pickle.dumps({"secret_key": secretkey}))
