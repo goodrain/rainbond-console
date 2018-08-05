aa48ad3d0aeafca3634c2b7760ddc22656e4ac96
朱峰 <zhuf_py@163.com>
2018-08-05 14:12:42 +0800

    [FIX] fix @deploy

--- console/repositories/deploy_repo.py
+++ console/repositories/deploy_repo.py
@@ -9,12 +9,13 @@ from console.models import DeployRelation
 
 class DeployRepo(object):
     def get_deploy_relation_by_service_id(self, service_id):
-        deploy = DeployRelation.objects.get(service_id=service_id)
-        if not deploy:
+        secret_key = DeployRelation.objects.get(service_id=service_id).secret_key
+        if not secret_key:
             secretkey = ''.join(random.sample(string.ascii_letters + string.digits, 8))
             secret_key = base64.b64encode(pickle.dumps({"secret_key": secretkey}))
             deploy = DeployRelation.objects.create(service_id=service_id, secret_key=secret_key)
-            return deploy
+            secret_key = deploy.secret_key
+            return secret_key
 
 
     def create_deploy_relation_by_service_id(self, service_id):
