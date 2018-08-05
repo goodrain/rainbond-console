ee05f95d8db61ef878733d10587f13c8a072327b
朱峰 <zhuf_py@163.com>
2018-08-05 13:47:45 +0800

    [FIX] fix no attribute bug@deploy

--- console/views/webhook.py
+++ console/views/webhook.py
@@ -318,7 +318,7 @@ class GetWebHooksUrl(AppBaseView):
             service_id = service_obj.service_id
             custom_url = "https://" + host + "/custom/deploy/" + service_obj.service_id
             deploy = deploy_repo.get_deploy_relation_by_service_id(service_id=service_id)
-            secret_key = pickle.loads(base64.b64decode(deploy.secret_key)).get("secret_key")
+            secret_key = pickle.loads(base64.b64decode(deploy)).get("secret_key")
             status = self.service.open_webhooks
             result = general_message(200, "success", "获取URl及开启状态成功", bean={"url": url, "custom_url":custom_url, "secret_key":secret_key, "status": status, "display":True})
 
