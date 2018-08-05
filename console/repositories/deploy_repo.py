501f1eeb792a14ca7349b51fcfea52a83462c84e
朱峰 <zhuf_py@163.com>
2018-08-05 13:08:50 +0800

    [FIX] 500 error bug@deploy

--- console/repositories/deploy_repo.py
+++ console/repositories/deploy_repo.py
@@ -10,15 +10,22 @@ from console.models import DeployRelation
 class DeployRepo(object):
     def get_deploy_get_deploy_relation_by_service_id(self, service_id):
         deploy = DeployRelation.objects.filter(service_id=service_id)
-        return deploy[0]
+        if deploy:
+            return deploy[0]
+        else:
+            secretkey = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+            secret_key = base64.b64encode(pickle.dumps({"secret_key": secretkey}))
+            DeployRelation.objects.create(service_id=service_id, secret_key=secret_key)
+            deploy = DeployRelation.objects.filter(service_id=service_id)
+            return deploy[0]
 
     def create_deploy_relation_by_service_id(self, service_id):
         deploy_relation = DeployRelation.objects.filter(service_id=service_id)
         if not deploy_relation:
             secretkey = ''.join(random.sample(string.ascii_letters + string.digits, 8))
             secret_key = base64.b64encode(pickle.dumps({"secret_key": secretkey}))
-            deploy_relation = DeployRelation.objects.create(service_id=service_id, secret_key=secret_key)
-        return deploy_relation[0]
+            DeployRelation.objects.create(service_id=service_id, secret_key=secret_key)
+        # return deploy_relation[0]
 
 
 
