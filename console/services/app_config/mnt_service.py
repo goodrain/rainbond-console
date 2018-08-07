751d59ec348c02ccf6534132279345537832bf3e
朱峰 <zhuf_py@163.com>
2018-08-07 15:50:57 +0800

    [FIX] fix path name1 bug@deploy

--- console/services/app_config/mnt_service.py
+++ console/services/app_config/mnt_service.py
@@ -51,6 +51,15 @@ class AppMntService(object):
                     })
         return mounted_dependencies, total
 
+    def get_local_vol_path(self, tenant, service):
+        all_mnt_relations = mnt_repo.get_service_mnts(tenant.tenant_id, service.service_id)
+        local_vol_path = []
+        for mount in all_mnt_relations:
+            local_vol_path.append(mount.mnt_dir)
+        return local_vol_path
+
+
+
     def get_service_unmnt_details(self, tenant, service, service_ids, page, page_size):
 
         services = service_repo.get_services_by_service_ids(*service_ids)
