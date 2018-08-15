15c82c66fecbeaa36ded009e4cc111d22b33dbda
barnett <zengqg@goodrain.com>
2018-08-15 10:24:58 +0800

    [ADD] add font file

--- www/static/www-new/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-export default function reducers(state = {}, action={}) {
-	switch(action.type){
-		case 'SET_APP_INFO':
-			return Object.assign({}, state, {appInfo: action.appInfo});
-	}
-	return state;
-}
