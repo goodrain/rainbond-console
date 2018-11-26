4335ea4acc706531026cef1e827b60c07a828adf
王东 <wangd@goodrain.com>
2018-11-26 14:10:37 +0800

    [FIX] test outer port

--- console/services/app_config/port_service.py
+++ console/services/app_config/port_service.py
@@ -257,7 +257,7 @@ class AppPortService(object):
                 service_name = service.service_alias
                 group_name = group_obj.group_name
                 container_port = deal_port.container_port
-                domain_name = "http://" + container_port + "." + service_name + "." + group_name
+                domain_name = str(container_port) + "." + str(service_name) + "." + str(tenant.tenant_name) + "." + str(region.region_name) + "." + str(region.httpdomain)
                 create_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 protocol = "http"
                 http_rule_id = make_uuid(domain_name)
