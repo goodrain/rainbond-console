1e0d08ff132ef399f0ff6f8815f2d830b512898c
王东 <wangd@goodrain.com>
2018-11-26 16:22:52 +0800

    [FIX] fix sql

--- sql/3.7.2-5.0.sql
+++ sql/3.7.2-5.0.sql
@@ -26,31 +26,5 @@ INSERT INTO `tenant_user_role_permission` (`role_id`, `per_id`) VALUES (13, 30);
 INSERT INTO `tenant_user_role_permission` (`role_id`, `per_id`) VALUES (13, 31);
 
 
--- service_domain表中增加字段
-
-ALTER TABLE service_domain ADD COLUMN `is_senior` bool DEFAULT false NOT NULL;
-ALTER TABLE service_domain ADD COLUMN `service_alias` varchar(32) DEFAULT '';
-ALTER TABLE service_domain ADD COLUMN `group_name` varchar(32) DEFAULT '';
-ALTER TABLE service_domain ADD COLUMN `domain_path` varchar(256) DEFAULT '' NULL;
-ALTER TABLE service_domain ADD COLUMN `domain_cookie` varchar(256) DEFAULT '' NULL;
-ALTER TABLE service_domain ADD COLUMN `domain_heander` varchar(256) DEFAULT '' NULL;
-
-
--- 创建表service_tcp_domain
-
-CREATE TABLE `service_tcp_domain` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `service_id` varchar(32),
-  `service_name` varchar(32),
-  `end_point` varchar(256),
-  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
-  `container_port` int(11) DEFAULT 0,
-  `domain_type` varchar(20) DEFAULT 'www',
-  `service_alias` varchar(32) DEFAULT '',
-  `group_name` varchar(32) DEFAULT '',
-  `protocol` varchar(15) DEFAULT '',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
-
 
 
