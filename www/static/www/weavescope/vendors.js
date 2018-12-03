ecf30c6c9ac54d69058d018ed62ccaefe72fa1e9
张士龙 <zsl1549@163.com>
2018-12-03 17:06:08 +0800

    [REV] Topology refresh problem

--- www/static/www/weavescope/vendors.js
+++ www/static/www/weavescope/vendors.js
@@ -90,7 +90,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + {"0":"c0aa562e8a26602b68bc","1":"e66f41eb5aad7ef2d85b","2":"15a5398e8e5488c8cfe5"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + {"0":"614eb5a668d8e238a870","1":"af63de53cdfbc6cb7e4e","2":"15a5398e8e5488c8cfe5"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
