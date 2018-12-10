344b848b0b813dfe02a3a41831a09c934d9710a0
王东 <wangd@goodrain.com>
2018-12-10 11:52:46 +0800

    [FIX] fix logger

--- goodrain_web/settings.py
+++ goodrain_web/settings.py
@@ -127,13 +127,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 LOGGING = {
+    # 设置版本
     'version': 1,
+    # 是否允许其他日志功能，Flase表示不禁用，禁用导致其他记录日志的功能将被阻止
     'disable_existing_loggers': False,
+    # 过滤器
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
+    # 格式化，日志输出格式
     'formatters': {
         'standard': {
             'format':
@@ -148,13 +152,13 @@ LOGGING = {
                 "%Y-%m-%d %H:%M:%S"
         },
     },
+    # 日志的处理方式
     'handlers': {
         'file_handler': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': '/tmp/goodrain.log',
-            'maxBytes': 1024 * 1024 * 5,
-            'backupCount': 5,
+            'stream': sys.stdout,
+            # 格式化      标准
             'formatter': 'standard',
         },
         'request_api': {
@@ -172,6 +176,7 @@ LOGGING = {
             'root_topic': 'goodrain_web',
             'formatter': 'zmq_formatter',
         },
+        # 输出日志的控制台
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
