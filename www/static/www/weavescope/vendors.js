ba1940f0943352f6fd53cfb165d8884b74e0b05e
张士龙 <zsl1549@163.com>
2018-12-18 15:56:28 +0800

    [REV] Modify instance name

--- www/static/www/weavescope/vendors.js
+++ www/static/www/weavescope/vendors.js
@@ -90,7 +90,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + {"0":"ef8bf63422990f0ea4c0","1":"b70786875428944878cb","2":"15a5398e8e5488c8cfe5"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + {"0":"0f089e78d2fbf8a12ae4","1":"b70786875428944878cb","2":"15a5398e8e5488c8cfe5"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
