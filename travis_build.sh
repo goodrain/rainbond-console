a838931c5352fed3a26f372fd6550dca462478f5
barnett <576501057@qq.com>
2018-12-29 14:34:02 +0800

    [REV] update build shell

--- travis_build.sh
+++ travis_build.sh
@@ -5,20 +5,22 @@ set -xe
 image_name="rbd-app-ui"
 
 VERSION=${TRAVIS_BRANCH}
-if [ "$VERSION"!="master" ];then
+if [ "$VERSION" != "master" ]; then
   VERSION=${TRAVIS_BRANCH:1:3}
 fi
 buildTime=$(date +%F-%H)
 
 function release(){
+  sed -i "s/VERSION/${VERSION}/g" Dockerfile.release
   git_commit=$(git log -n 1 --pretty --format=%h)
   release_desc=${VERSION}-${git_commit}-${buildTime}
   sed "s/__RELEASE_DESC__/${release_desc}/" Dockerfile.release > Dockerfile.build
+
   docker build -t rainbond/${image_name}:${VERSION} -f Dockerfile.build .
   rm -r ./Dockerfile.build
   if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
      docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-     docker push rainbond/rbd-app-ui:5.0
+     docker push rainbond/rbd-app-ui:${VERSION}
   fi
 }
 
