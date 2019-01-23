1ba543549613312911aea2c98008144812865171
ysicing zheng <i@ysicing.me>
2019-01-23 16:37:46 +0800

    [ADD] add travis ci

---
+++ travis_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -xe
+
+image_name="rbd-app-ui"
+
+VERSION=${TRAVIS_BRANCH}
+if [ "$VERSION" != "master" ]; then
+  VERSION=${TRAVIS_BRANCH:1:3}
+  if [ "$VERSION" == "3.7" ];then
+	VERSION="3.7.2"
+  fi
+fi
+buildTime=$(date +%F-%H)
+
+function release(){
+  sed -i "s/VERSION/${VERSION}/g" Dockerfile.release
+  git_commit=$(git log -n 1 --pretty --format=%h)
+  release_desc=${VERSION}-${git_commit}-${buildTime}
+  sed "s/__RELEASE_DESC__/${release_desc}/" Dockerfile.release > Dockerfile.build
+
+  docker build -t rainbond/${image_name}:${VERSION} -f Dockerfile.build .
+  rm -r ./Dockerfile.build
+  if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+     docker push rainbond/rbd-app-ui:${VERSION}
+  fi
+}
+
+case $1 in
+    *)
+    release
+    ;;
+esac
