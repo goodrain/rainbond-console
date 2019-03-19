d5ceb380cf5c0733aba8af1efa9a69efb9d99405
18611728358 <wang1861172@163.com>
2019-03-19 17:25:17 +0800

    [FIX] fix delete source class services may not use port limits less than 1024

--- console/services/app_config/port_service.py
+++ console/services/app_config/port_service.py
@@ -35,10 +35,6 @@ class AppPortService(object):
             return 400, u"端口{0}已存在".format(container_port)
         if not (1 <= container_port <= 65535):
             return 412, u"端口必须为1到65535的整数"
-        if service.service_source == AppConstants.SOURCE_CODE:
-            if service.language not in ("dockerfile", "docker"):
-                if container_port <= 1024:
-                    return 400, u"源码应用非Dockerfile构建的应用端口不能小于1024"
         return 200, "success"
 
     def check_port_alias(self, port_alias):
