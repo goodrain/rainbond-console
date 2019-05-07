db2eea38222f46bf06602feb8f2e56480b24ee10
ysicing <i@ysicing.me>
2019-05-07 10:40:52 +0800

    [REV] update travis ci

--- travis_build.sh
+++ travis_build.sh
@@ -18,7 +18,7 @@ function release(){
   release_desc=${VERSION}-${git_commit}-${buildTime}
   sed "s/__RELEASE_DESC__/${release_desc}/" Dockerfile.release > Dockerfile.build
 
-  docker build -t rainbond/${image_name}:${VERSION} -f Dockerfile.build .
+  docker build --network=host -t rainbond/${image_name}:${VERSION} -f Dockerfile.build .
   rm -r ./Dockerfile.build
   if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
      docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
