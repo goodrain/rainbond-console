e810903b885f65fc53129492c8be7ba3ad364eab
crowser <di94sh@hotmail.com>
2019-05-13 16:59:47 +0800

    refactor backup_service

--- console/services/backup_service.py
+++ console/services/backup_service.py
@@ -141,13 +141,13 @@ class GroupAppBackupService(object):
             backup_record.save()
         return 200, "success", backup_record
 
-    def delete_group_backup_by_backup_id(self, tenant, region, backup_id):
+    def delete_group_backup_by_backup_id(self, tenant, region, backup_id, group_id):
         backup_record = backup_record_repo.get_record_by_backup_id(tenant.tenant_id, backup_id)
         if not backup_record:
             return 404, "不存在该备份记录"
         if backup_record.status == "starting":
             return 409, "该备份正在进行中"
-        if backup_record.status == "success":
+        if backup_record.status == "success" and group_repo.get_group_by_id(group_id):
             return 409, "该备份不可删除"
         region_api.delete_backup_by_backup_id(region, tenant.tenant_name, backup_id)
         backup_record_repo.delete_record_by_backup_id(tenant.tenant_id, backup_id)
