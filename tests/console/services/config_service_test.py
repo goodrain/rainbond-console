d067243ecf6b5946cbf4c39d44c907fd55a4c2f2
GLYASAI <chaoschunqfeijg@163.com>
2019-08-08 11:22:23 +0800

    [REV] change the length of logo to 2097152 bytes

--- tests/console/services/config_service_test.py
+++ tests/console/services/config_service_test.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
 
 
 def test_image_to_base64():
     from console.services.config_service import config_service
-    base64 = config_service.image_to_base64("/data/media/21940d2566e94ef3a2d7569f09b25ba1.png")
-    print base64
+    base64 = config_service.image_to_base64("/data/media/bg.jpg")
+    print len(base64)
+    print sys.getsizeof(base64)
+    assert 0
