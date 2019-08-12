ac2d9ef706655a808e5d449bacff38f421976a77
GLYASAI <chaoschunqfeijg@163.com>
2019-08-12 18:39:49 +0800

    [FIX] wrong regoin num for tenant

--- console/repositories/tenant_region_repo.py
+++ console/repositories/tenant_region_repo.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
+from www.db.base import BaseConnection
 from www.models.main import TenantRegionInfo
 
 
 class TenantRegionRepo(object):
 
     def count_by_tenant_id(self, tenant_id):
-        return TenantRegionInfo.objects.filter(tenant_id=tenant_id).count()
+        sql = """
+        SELECT
+            count( * ) as total
+        FROM
+            region_info a
+            LEFT JOIN tenant_region b ON a.region_name = b.region_name
+        WHERE
+            b.tenant_id = "{tenant_id}"
+        """.format(tenant_id=tenant_id)
+        conn = BaseConnection()
+        result = conn.query(sql)
+        return result[0]["total"]
 
     def get_by_tenant_id_and_region_name(self, tenant_id, region_name):
         return TenantRegionInfo.objects.get(tenant_id=tenant_id, region_name=region_name, is_active=1, is_init=1)
