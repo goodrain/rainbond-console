18d6d1f33fcee046c112e68b2c09e1a93ce7e1d8
GLYASAI <chaoschunqfeijg@163.com>
2019-09-05 01:55:51 +0800

    [FIX] wrong relations

--- console/services/rbd_center_app_service.py
+++ console/services/rbd_center_app_service.py
@@ -7,6 +7,20 @@ from console.repositories.market_app_repo import rainbond_app_repo
 
 
 class RbdCenterAppService(object):
+    def get_version_apps(self, eid, version, service_source):
+        """
+        get rainbond center(market) application
+        raise: RecordNotFound
+        raise: RbdAppNotFound
+        """
+        rain_app = rainbond_app_repo.get_enterpirse_app_by_key_and_version(eid, service_source.group_key, version)
+        if rain_app is None:
+            raise RecordNotFound("Enterprice id: {0}; Group key: {1}; version: {2}; \
+                RainbondCenterApp not found.".format(eid, service_source.group_key, version))
+
+        apps_template = json.loads(rain_app.app_template)
+        return apps_template.get("apps")
+
     def get_version_app(self, eid, version, service_source):
         """
         Get the specified version of the rainbond center(market) application
