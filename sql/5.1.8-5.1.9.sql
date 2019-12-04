c7b116d96eecc1dc7df2e2d5e17088480ba18b74
barnettZQG <zengqg@goodrain.com>
2019-12-04 12:38:04 +0800

    Update 5.1.8-5.1.9.sql

--- sql/5.1.8-5.1.9.sql
+++ sql/5.1.8-5.1.9.sql
@@ -1,3 +1,5 @@
+-- 更新版本
+update console.console_sys_config set `value`="5.1.9" where `key`="RAINBOND_VERSION";
 ------------------------------## module console ##--------------------------------------
 
 -- console_sys_config
