fe4a942054b929cb2dda08c76aebc37e4b5d0b13
liushuai <linux2573@163.com>
2019-12-31 15:38:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ elif [ "$1" = "init" ];then
     python manage.py makemigrations console
     python manage.py migrate
 else
-    mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} -e "select * from console.region_info" || ./entrypoint.sh init && echo "Database initialization completed"
+    mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} -e "select * from console.region_info" || ./entrypoint.sh init && echo -e "\033[32;1mDatabase initialization completed\033[0m"
     python upgrade.py
     exec gunicorn goodrain_web.wsgi -b 0.0.0.0:${PORT:-7070} --max-requests=5000 -k gevent --reload --debug --workers=4 --log-file - --access-logfile - --error-logfile -
 fi
