cf656102d733c5b2ba036339ae1494e447938cfb
panda-zxs <1954431966@qq.com>
2020-03-06 15:22:24 +0800

    # FIX 修改应用模板发布记录状态控制逻辑

--- console/utils/restful_client.py
+++ console/utils/restful_client.py
@@ -6,7 +6,7 @@ from market_client.configuration import Configuration
 
 def get_market_client(enterpriseID, enterpriseToken, host=None):
     configuration = Configuration()
-    configuration.host = host if host else os.environ.get('APP_CLOUD_API', 'http://9000.gr054bca.23ehgni5.0196bd.grapps.cn')
+    configuration.host = host if host else os.environ.get('APP_CLOUD_API', 'http://api.goodrain.com:80')
     configuration.api_key['X_ENTERPRISE_TOKEN'] = enterpriseToken
     configuration.api_key['X_ENTERPRISE_ID'] = enterpriseID
     # create an instance of the API class
@@ -15,6 +15,6 @@ def get_market_client(enterpriseID, enterpriseToken, host=None):
 
 def get_default_market_client():
     configuration = Configuration()
-    configuration.host = os.environ.get('APP_CLOUD_API', 'http://9000.gr054bca.23ehgni5.0196bd.grapps.cn')
+    configuration.host = os.environ.get('APP_CLOUD_API', 'http://api.goodrain.com:80')
     # create an instance of the API class
     return market_client.AppsApi(market_client.ApiClient(configuration))
