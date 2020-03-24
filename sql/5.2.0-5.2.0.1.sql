8cfbe8f5b9483ff911c0331cfcf499175e12b173
panda-zxs <1954431966@qq.com>
2020-03-24 23:50:38 +0800

    add 补充升级sql

--- sql/5.2.0-5.2.0.1.sql
+++ sql/5.2.0-5.2.0.1.sql
@@ -5,3 +5,5 @@ alter table user_info modify nick_name varchar(64) DEFAULT NULL;
 alter table region_info add region_type varchar(32) DEFAULT '[]';
 alter table region_info add enterprise_id varchar(32) DEFAULT NULL;
 
+alter table console_sys_config add enterprise_id varchar(32) DEFAULT NULL;
+alter table tenant_enterprise add logo varchar(128) DEFAULT NULL;
