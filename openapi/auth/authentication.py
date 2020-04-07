d17e5953dc072ff6d25f5a8cde135dec01ec2b89
panda-zxs <1954431966@qq.com>
2020-04-07 19:13:55 +0800

    # FIX 去除无效代码

--- openapi/auth/authentication.py
+++ openapi/auth/authentication.py
@@ -2,9 +2,7 @@
 # creater by: barnett
 
 import logging
-import os
 from openapi.services.api_user_service import apiUserService
-from www.models.main import Users
 from rest_framework import authentication
 from rest_framework import exceptions
 logger = logging.getLogger("default")
@@ -19,24 +17,6 @@ class OpenAPIAuthentication(authentication.TokenAuthentication):
             user = apiUserService.get_user_by_token(token)
             if not user:
                 raise exceptions.AuthenticationFailed('No such user or user is not admin')
-        except Exception as e:
-            logger.exception(e)
-            raise exceptions.AuthenticationFailed('No such user')
-        return (user, None)
-
-
-class EnterOpenAPIAuthentication(authentication.TokenAuthentication):
-    def authenticate(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        user = None
-        if not token:
-            raise exceptions.AuthenticationFailed('No Token')
-        try:
-            if token == os.getenv("CONSOLE_API_TOKEN", "yBZ7LcveQNyjXuLjU3c3JmBARdNQnRUY7UqUKYpPn8g"):
-                user = Users(user_id=-1)
-            print user
-            if not user:
-                raise exceptions.AuthenticationFailed('No such user')
         except Exception as e:
             logger.exception(e)
             raise exceptions.AuthenticationFailed('No such user')
