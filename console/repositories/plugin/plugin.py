d7dba31f6b0e52d548186bd13765c5f23a377318
fanyangyang <a985850124@qq.com>
2020-05-14 17:10:36 +0800

    tolerate duplicate plugin_id

--- console/repositories/plugin/plugin.py
+++ console/repositories/plugin/plugin.py
@@ -31,7 +31,10 @@ class TenantPluginRepository(object):
             return None
 
     def get_by_plugin_id(self, plugin_id):
-        return TenantPlugin.objects.get(plugin_id=plugin_id)
+        plugins = TenantPlugin.objects.filter(plugin_id=plugin_id)
+        if not plugins:
+            return None
+        return plugins[0]
 
     def get_plugin_by_plugin_ids(self, plugin_ids):
         return TenantPlugin.objects.filter(plugin_id__in=plugin_ids)
