c2ed158e9df160e929a2702fb0502ce0e1b7f7ef
panda-zxs <1954431966@qq.com>
2020-05-26 17:33:57 +0800

    FIX 修改继承类

--- console/management/commands/init_roles.py
+++ console/management/commands/init_roles.py
@@ -28,7 +28,9 @@ class Command(BaseCommand):
             if users:
                 for user in users:
                     if user.user_id == team.creater:
-                        user_kind_role_service.update_user_roles(kind="team", kind_id=team.tenant_id, user=user, role_ids=[admin.ID])
+                        user_kind_role_service.update_user_roles(
+                            kind="team", kind_id=team.tenant_id, user=user, role_ids=[admin.ID])
                     else:
-                        user_kind_role_service.update_user_roles(kind="team", kind_id=team.tenant_id, user=user, role_ids=[developer.ID])
-        print ("初始化平台默认权限分配成功")
+                        user_kind_role_service.update_user_roles(
+                            kind="team", kind_id=team.tenant_id, user=user, role_ids=[developer.ID])
+        print("初始化平台默认权限分配成功")
