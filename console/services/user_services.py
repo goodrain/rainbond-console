744f8edcdbe533d60fe2d3dd0e598e30029934e1
panda-zxs <1954431966@qq.com>
2020-06-12 10:48:12 +0800

    fix check

--- console/services/user_services.py
+++ console/services/user_services.py
@@ -491,29 +491,35 @@ class UserService(object):
         return user_repo.get_by_tenant_id(tenant_id, user_id)
 
     def check_params(self, user_name, email, password, re_password, eid=None):
+        is_pass, msg = self.__check_user_name(user_name)
+        if not is_pass:
+            return is_pass, msg
+        is_pass, msg = self.__check_email(email)
+        if not is_pass:
+            return is_pass, msg
         if password != re_password:
             return False, "两次输入的密码不一致"
         return True, "success"
 
-    def __check_user_name(self, user_name, eid):
+    def __check_user_name(self, user_name):
         if not user_name:
             return False, "用户名不能为空"
-        if self.is_user_exist(user_name, eid):
+        if user_repo.get_user_by_user_name(user_name):
             return False, "用户{0}已存在".format(user_name)
         r = re.compile(u'^[a-zA-Z0-9_\\-\u4e00-\u9fa5]+$')
         if not r.match(user_name.decode("utf-8")):
             return False, "用户名称只支持中英文下划线和中划线"
         return True, "success"
 
-    def __check_email(self, email, eid):
+    def __check_email(self, email):
         if not email:
             return False, "邮箱不能为空"
-        if self.get_user_by_phone(email, eid):
+        if user_repo.get_user_by_email(email=email):
             return False, "邮箱{0}已存在".format(email)
         r = re.compile(r'^[\w\-\.]+@[\w\-]+(\.[\w\-]+)+$')
         if not r.match(email):
             return False, "邮箱地址不合法"
-        if self.get_user_by_email(email):
+        if user_repo.get_user_by_phone(email):
             return False, "邮箱已存在"
         return True, "success"
 
