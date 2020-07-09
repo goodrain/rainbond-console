6223dd2aa2e1834690463a989d63cd53d3cb163f
panda-zxs <1954431966@qq.com>
2020-07-09 10:33:45 +0800

    Fix 优化插件创建逻辑

--- console/repositories/plugin/plugin.py
+++ console/repositories/plugin/plugin.py
@@ -87,7 +87,8 @@ class TenantPluginRepository(object):
 
     def create_if_not_exist(self, **plugin):
         try:
-            return TenantPlugin.objects.get(tenant_id=plugin["tenant_id"], plugin_id=plugin["plugin_id"])
+            return TenantPlugin.objects.get(
+                tenant_id=plugin["tenant_id"], plugin_id=plugin["plugin_id"], region=plugin["region"])
         except TenantPlugin.DoesNotExist:
             return TenantPlugin.objects.create(**plugin)
 
