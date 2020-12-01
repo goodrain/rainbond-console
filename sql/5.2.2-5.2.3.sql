3b204fefc64c33555ef0475267ca7a3cfbbfbfed
yangk <yangk@xuhaoyudeMacBook-Pro.local>
2020-12-01 17:31:18 +0800

    modified tag unique index

--- sql/5.2.2-5.2.3.sql
+++ sql/5.2.2-5.2.3.sql
@@ -1,1 +1,3 @@
 alter table user_oauth_service modify column access_token varchar(2047);
+alter table `rainbond_center_app_tag` drop index `name`;
+alter table `rainbond_center_app_tag` ADD unique(`name`,`enterprise_id`);
