2dab3a130b2889c9da5461d30db233a39d640c6d
barnettZQG <zengqg@goodrain.com>
2020-12-18 14:06:11 +0800

    fix user search

--- console/services/user_services.py
+++ console/services/user_services.py
@@ -349,7 +349,7 @@ class UserService(object):
     def get_user_by_eid(self, eid, name, page, page_size):
         users = user_repo.get_enterprise_users(eid)
         if name:
-            users = users.filter(nick_name__contains=name)
+            users = users.filter(Q(nick_name__contains=name) | Q(real_name__contains=name))
         total = users.count()
         return users[(page - 1) * page_size:page * page_size], total
 
