d75ec129b4290e6e6e98b75e64d866fec6ac6f64
barnettZQG <zengqg@goodrain.com>
2021-02-20 16:51:44 +0800

    remove memorycache

--- goodrain_web/memcachedclient.py
+++ goodrain_web/memcachedclient.py
@@ -1,35 +1,15 @@
 # -*- coding: utf8 -*-
-import os
 import logging
-import pylibmc
 
 logger = logging.getLogger('default')
 
 
 class MemcachedCli(object):
     def __init__(self):
-        try:
-            if os.environ.get('MEMCACHED_HOST') and os.environ.get('MEMCACHED_PORT'):
-                self.mc = pylibmc.Client([os.environ.get('MEMCACHED_HOST') + ":" + os.environ.get('MEMCACHED_PORT')],
-                                         binary=True,
-                                         behaviors={
-                                             "tcp_nodelay": True,
-                                             "ketama": True
-                                         })
-        except Exception as e:
-            logger.exception(e)
+        pass
 
     def getKey(self, key):
-        try:
-            if self.mc:
-                return self.mc.get(key)
-        except Exception:
-            pass
-        return None
+        pass
 
     def setKey(self, key, value):
-        try:
-            if self.mc:
-                self.mc.set(key, value)
-        except Exception:
-            pass
+        pass
