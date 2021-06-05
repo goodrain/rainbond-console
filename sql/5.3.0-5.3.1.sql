20c409a50bb77d4cadf3087eb5c4108e29ce8f45
yangk <yangk@goodrain.com>
2021-06-05 11:13:24 +0800

    delete sql

--- sql/5.3.0-5.3.1.sql
+++ sql/5.3.0-5.3.1.sql
@@ -2,5 +2,4 @@
 ALTER TABLE tenant_service_env_var MODIFY attr_value text;
 ALTER TABLE tenant_service_monitor MODIFY `path` varchar(255);
 ALTER TABLE tenant_info CHANGE `region` `region` varchar(64) Default '';
-ALTER TABLE tenant_info DROP COLUMN `region`;
 ALTER TABLE service_share_record ADD COLUMN `share_app_version_info` VARCHAR(255) DEFAULT '';
