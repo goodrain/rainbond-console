4e73a5f1c7c2779bbdbc173a4996a2482fb24476
barnettZQG <zengqg@goodrain.com>
2021-07-01 19:19:10 +0800

    change some bug

--- console/services/market_app_service.py
+++ console/services/market_app_service.py
@@ -7,36 +7,45 @@ import json
 import logging
 import time
 
-# market app
-from console.services.market_app.component_group import ComponentGroup
-from console.services.market_app.app_upgrade import AppUpgrade
 # enum
 from console.constants import AppConstants
 from console.enum.app import GovernanceModeEnum
 from console.enum.component_enum import ComponentType
 # exception
-from console.exception.bcode import (ErrAppConfigGroupExists, ErrK8sServiceNameExists)
-from console.exception.main import (AbortRequest, ErrVolumePath, MarketAppLost, RbdAppNotFound, ServiceHandleException)
-from console.models.main import RainbondCenterApp, RainbondCenterAppVersion
-from console.repositories.app import (app_market_repo, app_tag_repo, service_source_repo)
-from console.repositories.app_config import (env_var_repo, extend_repo, port_repo, volume_repo)
+from console.exception.bcode import (ErrAppConfigGroupExists,
+                                     ErrK8sServiceNameExists)
+from console.exception.main import (AbortRequest, ErrVolumePath, MarketAppLost,
+                                    RbdAppNotFound, ServiceHandleException)
+from console.models.main import (AppMarket, AppUpgradeRecord,
+                                 RainbondCenterApp, RainbondCenterAppVersion)
+from console.repositories.app import (app_market_repo, app_tag_repo,
+                                      service_source_repo)
+from console.repositories.app_config import (env_var_repo, extend_repo,
+                                             port_repo, volume_repo)
 from console.repositories.base import BaseConnection
 from console.repositories.group import group_repo, tenant_service_group_repo
-from console.repositories.market_app_repo import (app_import_record_repo, rainbond_app_repo)
+from console.repositories.market_app_repo import (app_import_record_repo,
+                                                  rainbond_app_repo)
 from console.repositories.plugin import plugin_repo
 from console.repositories.service_repo import service_repo
 from console.repositories.share_repo import share_repo
 from console.repositories.team_repo import team_repo
 from console.services.app import app_market_service, app_service
 from console.services.app_actions import app_manage_service
-from console.services.app_config import (AppMntService, domain_service, port_service, probe_service, volume_service)
+from console.services.app_config import (AppMntService, domain_service,
+                                         port_service, probe_service,
+                                         volume_service)
 from console.services.app_config.app_relation_service import \
     AppServiceRelationService
 from console.services.app_config.component_graph import component_graph_service
 from console.services.app_config.service_monitor import service_monitor_repo
 from console.services.app_config_group import app_config_group_service
 from console.services.group_service import group_service
-from console.services.plugin import (app_plugin_service, plugin_config_service, plugin_service, plugin_version_service)
+from console.services.market_app.app_upgrade import AppUpgrade
+# market app
+from console.services.market_app.component_group import ComponentGroup
+from console.services.plugin import (app_plugin_service, plugin_config_service,
+                                     plugin_service, plugin_version_service)
 from console.services.region_services import region_services
 from console.services.upgrade_services import upgrade_service
 from console.services.user_services import user_services
@@ -44,14 +53,13 @@ from console.utils.version import compare_version, sorted_versions
 from django.core.paginator import Paginator
 from django.db import transaction
 from django.db.models import Q
-# model
-from www.models.main import (TenantEnterprise, TenantEnterpriseToken, TenantServiceEnvVar, TenantServiceInfo,
-                             TenantServicesPort, Users)
-from www.models.plugin import ServicePluginConfigVar
-from console.models.main import AppUpgradeRecord
-from console.models.main import AppMarket
 # www
 from www.apiclient.regionapi import RegionInvokeApi
+# model
+from www.models.main import (TenantEnterprise, TenantEnterpriseToken,
+                             TenantServiceEnvVar, TenantServiceInfo,
+                             TenantServicesPort, Users)
+from www.models.plugin import ServicePluginConfigVar
 from www.tenantservice.baseservice import BaseTenantService
 from www.utils.crypt import make_uuid
 
@@ -1512,7 +1520,7 @@ class MarketAppService(object):
         if not component_sources:
             return []
         component_ids = [cs.service_id for cs in component_sources]
-        components = service_repo.get_service_by_service_ids(component_ids)
+        components = service_repo.list_by_component_ids(component_ids)
 
         versions = self.list_app_versions(enterprise_id, component_sources[0])
 
--- sql/5.3.0-5.3.1.sql
+++ sql/5.3.0-5.3.1.sql
@@ -1,10 +1,9 @@
 -- only support mysql
-ALTER TABLE `rbdconsole`.`service_group`
-ADD COLUMN `app_store_name` varchar(255) NULL AFTER `app_type`,
-ADD COLUMN `app_store_url` varchar(2047) NULL AFTER `app_store_name`,
-ADD COLUMN `app_template_name` varchar(255) NULL AFTER `app_store_url`,
-ADD COLUMN `version` varchar(255) NULL AFTER `app_template_name`;
-
+ALTER TABLE `service_group` ADD COLUMN `app_store_name` varchar(255) NULL;
+ALTER TABLE `service_group` ADD COLUMN `app_store_url` varchar(2047) NULL;
+ALTER TABLE `service_group` ADD COLUMN `app_template_name` varchar(255) NULL;
+ALTER TABLE `service_group` ADD COLUMN `version` varchar(255) NULL;
+ALTER TABLE `service_group` ADD COLUMN `app_type` varchar(255) DEFAULT 'rainbond';
 ALTER TABLE tenant_service_env_var MODIFY attr_value text;
 ALTER TABLE tenant_service_monitor MODIFY `path` varchar(255);
 ALTER TABLE tenant_info CHANGE `region` `region` varchar(64) Default '';
