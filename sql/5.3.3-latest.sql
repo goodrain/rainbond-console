891f5cb134bfc72db2c25aa09db02ce9b8c84da1
pescox <pescoding@outlook.com>
2022-01-06 16:02:34 +0800

    add path-rewrite upgrade sql

--- sql/5.3.3-latest.sql
+++ sql/5.3.3-latest.sql
@@ -8,3 +8,7 @@ ALTER TABLE service_group ADD COLUMN `k8s_app` VARCHAR(64);
 ALTER TABLE tenant_service ADD COLUMN `k8s_component_name` VARCHAR(100);
 update tenant_service set k8s_component_name=service_alias;
 ALTER TABLE tenant_service_delete ADD COLUMN `k8s_component_name` VARCHAR(100);
+
+-- 5.5.0 - 5.5.1 sql
+ALTER TABLE `service_domain` ADD COLUMN `path_rewrite` bool DEFAULT false NOT NULL;
+ALTER TABLE `service_domain` ADD COLUMN `rewrites` longtext NOT NULL;
