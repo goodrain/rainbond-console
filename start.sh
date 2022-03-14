6211f5914c9cf048e83781a55fdbd980e991f9e2
ZhangSetSail <zqh15131121078@126.com>
2022-03-14 13:40:31 +0800

    接口优化以及脚本添加服务器检测

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script determines whether the user opens the k3s cluster
 
+
+FREE=`free -m |awk '/Mem/{print $4}'`
+CPUS=`cat /proc/cpuinfo | grep "processor" | wc -l`
+DISK=`df -m / |awk '/sda1/{print $4}'`
 # Move the dockerd k3s configuration to let the supervisor manage the process
 # Unzip the image package and delete the compressed package
 function k3s_start() {
@@ -28,6 +32,24 @@ if [ "${ENABLE_CLUSTER}" == 'true' ];then
     # explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
     find /run /var/run -iname 'docker*.pid' -delete || :
     k3s_start
+fi
+
+# memory detection（4G）
+if [ $FREE -lt 4096 ]; then
+     echo "Problem: free=$FREE,Insufficient memory, at least 4G memory is required"
+     exit 1
+fi
+
+# cpu core detection
+if [ $CPUS  -lt 2 ];then
+    echo "Problem: cpus=$CPUS,Insufficient number of cores, at least 2 cores required"
+    exit 2
+fi
+
+# disk detection
+if [ $DISK -lt 51200 ];then
+    echo "Problem: disk=$DISK,Insufficient disk space, at least 50G required"
+    exit 3
 fi
 
 
