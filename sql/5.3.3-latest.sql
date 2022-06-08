277d9c36a576cae5b9b5f3a6890d963826f11f7f
ZhangSetSail <zqh15131121078@126.com>
2022-06-08 16:49:10 +0800

    feat: change model type

--- sql/5.3.3-latest.sql
+++ sql/5.3.3-latest.sql
@@ -13,5 +13,9 @@ ALTER TABLE tenant_service_delete ADD COLUMN `k8s_component_name` VARCHAR(100);
 ALTER TABLE `service_domain` ADD COLUMN `path_rewrite` bool DEFAULT false NOT NULL;
 ALTER TABLE `service_domain` ADD COLUMN `rewrites` longtext NOT NULL;
 
--- 5.5.1 - 5.5.2 sql
+-- 5.6.0 - 5.7.0 sql
 ALTER TABLE `rainbond_center_app_version` ADD COLUMN `is_plugin` bool DEFAULT false NOT NULL;
+ALTER TABLE `plugin_config_items` MODIFY `attr_name` varchar(64) NOT NULL;
+ALTER TABLE `plugin_config_items` MODIFY `attr_alt_value` LONGTEXT NOT NULL;
+ALTER TABLE `plugin_config_items` MODIFY `attr_default_value` LONGTEXT;
+ALTER TABLE `service_plugin_config_var` MODIFY `attrs` LONGTEXT NOT NULL;
