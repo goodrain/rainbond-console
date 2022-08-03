9e463321fe1f4a131aaa7104596b13e4e13c846b
zhangsetsail <zqh15131121078@126.com>
2022-08-03 19:59:50 +0800

    perf: make format

--- www/apiclient/regionapi.py
+++ www/apiclient/regionapi.py
@@ -353,8 +353,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
     def get_dynamic_services_pods(self, region, tenant_name, services_ids):
         url, token = self.__get_region_access_info(tenant_name, region)
         tenant_region = self.__get_tenant_region_info(tenant_name, region)
-        url = url + "/v2/tenants/" + tenant_region.region_tenant_name + "/pods?service_ids={}".format(
-            ",".join(services_ids))
+        url = url + "/v2/tenants/" + tenant_region.region_tenant_name + "/pods?service_ids={}".format(",".join(services_ids))
         self._set_headers(token)
         res, body = self._get(url, self.default_headers, region=region, timeout=15)
         return body
@@ -708,8 +707,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
         """获取组件日志"""
         url, token = self.__get_region_access_info(tenant_name, region)
         tenant_region = self.__get_tenant_region_info(tenant_name, region)
-        url = url + "/v2/tenants/{0}/services/{1}/logs?rows={2}".format(tenant_region.region_tenant_name, service_alias,
-                                                                        rows)
+        url = url + "/v2/tenants/{0}/services/{1}/logs?rows={2}".format(tenant_region.region_tenant_name, service_alias, rows)
         self._set_headers(token)
         res, body = self._get(url, self.default_headers, region=region)
         return body
@@ -739,8 +737,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
     def get_target_events_list(self, region, tenant_name, target, target_id, page, page_size):
         """获取作用对象事件日志列表"""
         url, token = self.__get_region_access_info(tenant_name, region)
-        url = url + "/v2/events" + "?target={0}&target-id={1}&page={2}&size={3}".format(target, target_id, page,
-                                                                                        page_size)
+        url = url + "/v2/events" + "?target={0}&target-id={1}&page={2}&size={3}".format(target, target_id, page, page_size)
         self._set_headers(token)
         res, body = self._get(url, self.default_headers, region=region, timeout=20)
         return res, body
@@ -1094,8 +1091,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
         url = url + "/v2/tenants/" + tenant_region.region_tenant_name + "/event"
 
         self._set_headers(token)
-        res, body = self._get(url, self.default_headers, region=region, body=json.dumps({"event_ids": event_ids}),
-                              timeout=10)
+        res, body = self._get(url, self.default_headers, region=region, body=json.dumps({"event_ids": event_ids}), timeout=10)
         return body
 
     def get_events_by_event_ids(self, region_name, event_ids):
@@ -1704,8 +1700,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
         if not region_info:
             raise ServiceHandleException("region not found")
         url = region_info.url
-        url += "/v2/cluster/convert-resource?eid={0}&content={1}&namespace={2}".format(enterprise_id, content,
-                                                                                       namespace)
+        url += "/v2/cluster/convert-resource?eid={0}&content={1}&namespace={2}".format(enterprise_id, content, namespace)
         res, body = self._get(url, self.default_headers, region=region_info.region_name, timeout=10)
         return res, body
 
@@ -1714,8 +1709,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
         if not region_info:
             raise ServiceHandleException("region not found")
         url = region_info.url
-        url += "/v2/cluster/convert-resource?eid={0}&content={1}&namespace={2}".format(enterprise_id, content,
-                                                                                       namespace)
+        url += "/v2/cluster/convert-resource?eid={0}&content={1}&namespace={2}".format(enterprise_id, content, namespace)
         res, body = self._post(url, self.default_headers, body="", region=region_info.region_name, timeout=20)
         return res, body
 
@@ -1725,8 +1719,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/yaml_resource_name?eid={0}".format(enterprise_id)
-        res, body = self._get(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                              timeout=20)
+        res, body = self._get(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=20)
         return res, body
 
     def yaml_resource_detailed(self, enterprise_id, region, data):
@@ -1735,8 +1728,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/yaml_resource_detailed?eid={0}".format(enterprise_id)
-        res, body = self._get(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                              timeout=20)
+        res, body = self._get(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=20)
         return res, body
 
     def yaml_resource_import(self, enterprise_id, region, data):
@@ -1745,8 +1737,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/yaml_resource_import?eid={0}".format(enterprise_id)
-        res, body = self._post(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                               timeout=300)
+        res, body = self._post(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=300)
         return res, body
 
     def add_resource(self, enterprise_id, region, data):
@@ -1755,8 +1746,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/convert-resource?eid={0}".format(enterprise_id)
-        res, body = self._post(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                               timeout=10)
+        res, body = self._post(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=10)
         return res, body
 
     def list_tenants(self, enterprise_id, region, page=1, page_size=10):
@@ -1988,9 +1978,8 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
     def check_app_governance_mode(self, region_name, tenant_name, region_app_id, query):
         url, token = self.__get_region_access_info(tenant_name, region_name)
         tenant_region = self.__get_tenant_region_info(tenant_name, region_name)
-        url = url + "/v2/tenants/{}/apps/{}/governance/check?governance_mode={}".format(
-            tenant_region.region_tenant_name,
-            region_app_id, query)
+        url = url + "/v2/tenants/{}/apps/{}/governance/check?governance_mode={}".format(tenant_region.region_tenant_name,
+                                                                                        region_app_id, query)
 
         self._set_headers(token)
         _, _ = self._get(url, self.default_headers, region=region_name)
@@ -2045,8 +2034,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
 
     def sync_config_groups(self, tenant_name, region_name, app_id, body):
         url, token = self.__get_region_access_info(tenant_name, region_name)
-        url = url + "/v2/tenants/{tenant_name}/apps/{app_id}/app-config-groups".format(tenant_name=tenant_name,
-                                                                                       app_id=app_id)
+        url = url + "/v2/tenants/{tenant_name}/apps/{app_id}/app-config-groups".format(tenant_name=tenant_name, app_id=app_id)
         self._set_headers(token)
         self._post(url, self.default_headers, body=json.dumps(body), region=region_name)
 
@@ -2105,8 +2093,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/k8s-resource?eid={0}".format(enterprise_id)
-        res, body = self._post(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                               timeout=10)
+        res, body = self._post(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=10)
         return res, body
 
     def update_app_resource(self, enterprise_id, region, data):
@@ -2115,8 +2102,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/k8s-resource"
-        res, body = self._put(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                              timeout=10)
+        res, body = self._put(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=10)
         return res, body
 
     def delete_app_resource(self, enterprise_id, region, data):
@@ -2125,8 +2111,7 @@ class RegionInvokeApi(RegionApiBaseHttpClient):
             raise ServiceHandleException("region not found")
         url = region_info.url
         url += "/v2/cluster/k8s-resource"
-        res, body = self._delete(url, self.default_headers, body=json.dumps(data), region=region_info.region_name,
-                                 timeout=10)
+        res, body = self._delete(url, self.default_headers, body=json.dumps(data), region=region_info.region_name, timeout=10)
         return res, body
 
     def create_component_k8s_attribute(self, tenant_name, region_name, service_alias, body):
