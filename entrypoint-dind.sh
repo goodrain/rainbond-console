d23132b6e6c1187a9c24e584b75a7dd98cb00810
Qi Zhang <smallqi1@163.com>
2022-09-12 00:16:24 +0800

    fix: optimize dind startup log
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- entrypoint-dind.sh
+++ entrypoint-dind.sh
@@ -4,6 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[32;1m'
 YELLOW='\033[33;1m'
 NC='\033[0m' # No Color
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
 
 ########################################
 # Basic Configuration
@@ -14,23 +15,26 @@ function basic_check {
     DISK=$(df -m / |sed -n '2p'|awk '{print $4}')
 
     if [ ! "$EIP" ];then
-        echo -e "${RED} EIP is required, please execute the following command and restart Rainbond ${NC}"
-        echo -e "${RED} export EIP= IP address ${NC}"
+        echo -e "${RED}${DATE} ERROR: EIP is required, please execute the following command and restart Rainbond ${NC}"
+        echo -e "${RED}${DATE} ERROR: export EIP= IP address ${NC}"
         exit 1
     fi
 
     if [ "$FREE" -lt 4096 ]; then
-        echo -e "${YELLOW} WARN: Too little memory, recommended memory configuration is 4G ${NC}"
+        echo -e "${YELLOW}${DATE} WARN: Too little memory, recommended memory configuration is 4G ${NC}"
     fi
     if [ "$CPUS" -lt 2 ];then
-        echo -e "${YELLOW} WARN: Too few CPUs, recommended CPU configuration is 2 cores ${NC}"
+        echo -e "${YELLOW}${DATE} WARN: Too few CPUs, recommended CPU configuration is 2 cores ${NC}"
     fi
     if [ "$DISK" -lt 51200 ];then
-        echo -e "${YELLOW} WARN: Too little free disk space, recommended disk space greater than 50G ${NC}"
+        echo -e "${YELLOW}${DATE} WARN: Too little free disk space, recommended disk space greater than 50G ${NC}"
     fi
-    echo -e "${GREEN} INFO: Memory: $FREE MB, CPUs: $CPUS, Disk: $DISK MB ${NC}"
+    echo -e "${GREEN}${DATE} INFO: Memory: $FREE MB, CPUs: $CPUS, Disk: $DISK MB ${NC}"
     ## Do domain name resolution
     echo "127.0.0.1  rbd-api-api" >> /etc/hosts
+
+    # explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
+    find /run /var/run -iname 'docker*.pid' -delete || :
 }
 
 ########################################
@@ -39,17 +43,17 @@ function basic_check {
 
 function start_docker {
     while_num=0
-    supervisorctl start docker
+    supervisorctl start docker > /dev/null 2>&1
+    echo -e "${GREEN}${DATE} INFO: Docker is starting, please wait ············································${NC}"
     while true; do
         if docker ps > /dev/null 2>&1; then
-            echo -e "${GREEN} INFO: Docker started successfully ${NC}"
+            echo -e "${GREEN}${DATE} INFO: Docker started successfully ${NC}"
             break
         fi
         sleep 5
-        echo -e "${YELLOW} WARN: Waiting for Docker to start ${NC}"
         (( while_num++ )) || true
         if [ $(( while_num )) -gt 12 ]; then
-            echo -e "${RED} ERROR: Docker failed to start. Please use the command to view the docker log 'docker exec rainbond-allinone /bin/cat /app/logs/dind.log'${NC}"
+            echo -e "${RED}${DATE} ERROR: Docker failed to start. Please use the command to view the docker log 'docker exec rainbond-allinone /bin/cat /app/logs/dind.log'${NC}"
             exit 1
         fi
     done
@@ -61,12 +65,12 @@ function start_docker {
 
 function load_images {
     if docker images | grep -q "rbd-api"; then
-        echo -e "${GREEN} INFO: Docker images loaded ${NC}"
+        echo -e "${GREEN}${DATE} INFO: Docker images loaded ${NC}"
     else
-        echo -e "${GREEN} INFO: Loading images ${NC}"
+        echo -e "${GREEN}${DATE} INFO: Loading images ${NC}"
         while true; do
             if docker load -i /app/ui/rainbond-"${VERSION}".tar; then
-                echo -e "${GREEN} INFO: Docker images success ${NC}"
+                echo -e "${GREEN}${DATE} INFO: Docker images success ${NC}"
                 break
             fi
         done
@@ -84,20 +88,21 @@ function start_k3s {
         xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
         sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
     fi
-
-    echo -e "${GREEN} INFO: Starting K3s ${NC}"
-    supervisorctl start k3s
+    supervisorctl start k3s > /dev/null 2>&1
+    echo -e "${GREEN}${DATE} INFO: K3s is starting, please wait ············································${NC}"
     while_num=0
     while true; do
-        if kubectl get node | grep Ready > /dev/null 2>&1; then
-            echo -e "${GREEN} INFO: K3s started successfully ${NC}"
-            break
+        K3S_STATUS=$(netstat -nltp | grep k3s | grep -c -E "6443|6444|10248|10249|10250|10251|10256|10257|10258|10259")
+        if [[ "${K3S_STATUS}" == "10" ]]; then
+            if kubectl get node | grep Ready > /dev/null 2>&1; then
+                echo -e "${GREEN}${DATE} INFO: K3s started successfully ${NC}"
+                break
+            fi
         fi
         sleep 5
-        echo -e "${YELLOW} WARN: Waiting for K3s to start ${NC}"
         (( while_num++ )) || true
         if [ $(( while_num )) -gt 12 ]; then
-            echo -e "${RED} ERROR: K3s failed to start. Please use the command to view the k3s log 'docker exec rainbond-allinone /bin/cat /app/logs/k3s.log' ${NC}"
+            echo -e "${RED}${DATE} ERROR: K3s failed to start. Please use the command to view the k3s log 'docker exec rainbond-allinone /bin/cat /app/logs/k3s.log' ${NC}"
             exit 1
         fi
     done
@@ -112,16 +117,16 @@ IMAGE_NAMESPACE=${IMAGE_NAMESPACE:-rainbond}
 function start_rainbond {
     # create namespace
     if kubectl get ns | grep rbd-system > /dev/null 2>&1; then
-        echo -e "${GREEN} INFO: Namespace rbd-system already exists ${NC}"
+        echo -e "${GREEN}${DATE} INFO: Namespace rbd-system already exists ${NC}"
     else
         kubectl create ns rbd-system
-        echo -e "${GREEN} INFO: Create namespace rbd-system ${NC}"
+        echo -e "${GREEN}${DATE} INFO: Create namespace rbd-system ${NC}"
     fi
 
     # create helm rainbond-operator
     HELM_RAINBOND_OPERATOR=$(helm list -n rbd-system | grep rainbond-operator | awk '{print $1}')
     if [[ "$HELM_RAINBOND_OPERATOR" == "rainbond-operator" ]]; then
-        echo -e "${GREEN} INFO: Helm rainbond-operator already exists ${NC}"
+        echo -e "${GREEN}${DATE} INFO: Helm rainbond-operator already exists ${NC}"
     else
         RBD_LIST=(rbd-api rbd-chaos rbd-eventlog rbd-gateway rbd-monitor rbd-mq rbd-node rbd-resource-proxy rbd-webcli rbd-worker rbdcluster)
         for item in "${RBD_LIST[@]}"; do
@@ -131,7 +136,7 @@ function start_rainbond {
         helm install rainbond-operator /app/chart -n rbd-system --kubeconfig /root/.kube/config \
             --set operator.image.name="${IMAGE_DOMAIN}"/"${IMAGE_NAMESPACE}"/rainbond-operator \
             --set operator.image.tag="${VERSION}"
-        echo -e "${GREEN} INFO: Helm rainbond-operator installed ${NC}"
+        echo -e "${GREEN}${DATE} INFO: Helm rainbond-operator installed ${NC}"
 
         # setting rainbondcluster
         NODE_NAME=$(kubectl get node | sed -n '2p' | awk '{print $1}')
@@ -143,24 +148,27 @@ function start_rainbond {
         sed -i "s/single_node_internal_ip/$IIP/" /app/ui/rainbond-operator/config/single_node_cr/rbdcluster.yml
 
         if kubectl apply -f /app/ui/rainbond-operator/config/single_node_cr/ -n rbd-system > /dev/null 2>&1; then
-            echo -e "${GREEN} INFO: Rainbond Region installed ${NC}"
+            echo -e "${GREEN}${DATE} INFO: Rainbond Region installed ${NC}"
         else
-            echo -e "${RED} ERROR: Rainbond Region failed to install ${NC}"
+            echo -e "${RED}${DATE} ERROR: Rainbond Region failed to install ${NC}"
             exit 1
         fi
     fi
+    echo -e "${GREEN}${DATE} INFO: Rainbond Region is starting, please wait ············································${NC}"
     while true; do
-        if curl http://"$(kubectl get svc -n rbd-system | grep rbd-api-api-inner | awk '{print $3}')":8888/v2/health 2>&1 | grep health > /dev/null 2>&1; then
-            echo -e "${GREEN} INFO: Rainbond Region started successfully ${NC}"
-            break
+        if netstat -nltp | grep 8443 > /dev/null 2>&1; then
+            if curl http://"$(kubectl get svc -n rbd-system | grep rbd-api-api-inner | awk '{print $3}')":8888/v2/health 2>&1 | grep health > /dev/null 2>&1; then
+                echo -e "${GREEN}${DATE} INFO: Rainbond Region started successfully ${NC}"
+                break
+            fi
         fi
         sleep 5
-        echo -e "${YELLOW} WARN: Waiting for Rainbond start ${NC}"
     done
-    supervisorctl start console
+    supervisorctl start console > /dev/null 2>&1
+    echo -e "${GREEN}${DATE} INFO: Rainbond console is starting, please wait ············································${NC}"
     while true; do
         if curl http://127.0.0.1:7070 > /dev/null 2>&1; then
-            echo -e "${GREEN} INFO: Rainbond started successfully, Please pass http://$EIP:7070 Access Rainbond ${NC}"
+            echo -e "${GREEN}${DATE} INFO: Rainbond started successfully, Please pass http://$EIP:7070 Access Rainbond ${NC}"
             break
         fi
         sleep 5
@@ -168,9 +176,9 @@ function start_rainbond {
 }
 
 function stop_container {
-    echo -e "${GREEN} INFO: Stopping K3s ${NC}"
+    echo -e "${GREEN}${DATE} INFO: Stopping K3s ${NC}"
     supervisorctl stop k3s
-    echo -e "${GREEN} INFO: Stopping Docker ${NC}"
+    echo -e "${GREEN}${DATE} INFO: Stopping Docker ${NC}"
     systemctl stop docker
     exit 1
 }
