c9eaab74e6675179cc1b40bd145ef8f8ca49c1bc
Qi Zhang <smallqi1@163.com>
2022-11-06 21:16:22 +0800

    fix: arm image not found and add dind install script
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- build_dind_package.sh
+++ build_dind_package.sh
@@ -6,7 +6,6 @@ VERSION=${VERSION:-'v5.6.0-release'}
 OFFLINE=${OFFLINE:-"false"}
 BUILDER=${BUILDER:-"v5.8.1-release"}
 RUNNER=${RUNNER:-"v5.8.1-release"}
-ARCH=$(uname -m)
 
 image_list="rainbond/kubernetes-dashboard:v2.6.1
 rainbond/registry:2.6.2
@@ -26,23 +25,18 @@ ${IMAGE_DOMAIN}/${IMAGE_NAMESPACE}/rbd-node:${VERSION}
 ${IMAGE_DOMAIN}/${IMAGE_NAMESPACE}/rbd-monitor:${VERSION}
 ${IMAGE_DOMAIN}/${IMAGE_NAMESPACE}/rbd-gateway:${VERSION}
 ${IMAGE_DOMAIN}/${IMAGE_NAMESPACE}/rbd-api:${VERSION}
-registry.cn-hangzhou.aliyuncs.com/goodrain/smallimage:latest
+rainbond/mirrored-pause:3.1
 rancher/local-path-provisioner:v0.0.20
-rancher/mirrored-coredns-coredns:1.8.4
-rancher/mirrored-metrics-server:v0.5.0
 rancher/mirrored-library-busybox:1.32.1"
 
+docker pull rainbond/mirrored-metrics-server:v0.5.0
+docker pull rainbond/mirrored-coredns-coredns:1.8.4
+docker tag rainbond/mirrored-metrics-server:v0.5.0 rancher/mirrored-metrics-server:v0.5.0
+docker tag rainbond/mirrored-coredns-coredns:1.8.4 rancher/mirrored-coredns-coredns:1.8.4
 
 for image in ${image_list}; do
     docker pull "${image}"
 done
 
-# mirrored-pause 3.1 not arm64 image
-if [[ "${ARCH}" == "x86_64" ]]; then
-  docker pull rancher/mirrored-pause:3.1
-else
-  docker pull rancher/mirrored-pause:3.2
-  docker tag rancher/mirrored-pause:3.2 rancher/mirrored-pause:3.1
-fi
 
-docker save -o rainbond-"${VERSION}".tar ${image_list} rancher/mirrored-pause:3.1
+docker save -o rainbond-"${VERSION}".tar ${image_list} rancher/mirrored-metrics-server:v0.5.0 rancher/mirrored-coredns-coredns:1.8.4
