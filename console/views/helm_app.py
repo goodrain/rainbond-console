a2f8104b2e35ecded2f0dfc9a3f1109181a4aef9
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2022-11-16 16:46:11 +0800

    fix: Incorrect value transfer (#1197)

--- console/views/helm_app.py
+++ console/views/helm_app.py
@@ -100,7 +100,7 @@ class HelmChart(RegionTenantHeaderView):
         if not data:
             ret = dict()
             ret["repo_exist"] = False
-            result = general_message(200, "success", "查询成功", bean=json.dumps(ret))
+            result = general_message(200, "success", "查询成功", bean=ret)
             return Response(result, status=status.HTTP_200_OK)
         chart_information = helm_app_service.get_helm_chart_information(self.region_name, self.tenant_name, data["repo_url"],
                                                                         chart_name)
