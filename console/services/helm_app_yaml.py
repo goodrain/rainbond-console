a6365d64b42a321effc71176cb40d13887524215
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2023-01-04 10:42:26 +0800

    fix: length of automatically obtained helm repo name is too long (#1239)

--- console/services/helm_app_yaml.py
+++ console/services/helm_app_yaml.py
@@ -281,7 +281,7 @@ class HelmAppService(object):
             if cmd_list[i] == "--repo" and i + 1 != len(cmd_list):
                 repo_url = cmd_list[i + 1]
                 repo_name = repo_url.split("/")[-1]
-                if repo_name == "":
+                if repo_name == "" or len(repo_name) > 32:
                     repo_name = "rainbond"
             if cmd_list[i] == "--username" and i + 1 != len(cmd_list):
                 username = cmd_list[i + 1]
