c864b523dc7e0d2ecd43b3c9402fd795dddc13af
zhangsetsail <zqh15131121078@126.com>
2023-06-19 09:54:02 +0800

    feat: upgrade v5.14.2 sql

--- sql/5.10.1-latest-sqlite.sql
+++ sql/5.10.1-latest-sqlite.sql
@@ -1,3 +1,7 @@
 ------5.10.1 -> 5.11.0 sqlite
 
 ALTER TABLE `tenant_services_port` ADD COLUMN `name` varchar(64) NULL;
+
+-- 5.14.1 - 5.14.2 sql
+
+ALTER TABLE `tenant_service` ADD COLUMN `arch` varchar(32) NULL;
