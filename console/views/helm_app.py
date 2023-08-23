a2ccf577507d2de857cbab6d63f1bdd6026383ee
zhangxiaoyuan <1666888816@qq.com>
2023-08-23 10:36:43 +0800

    feat: helm only one click install

--- console/views/helm_app.py
+++ console/views/helm_app.py
@@ -154,8 +154,9 @@ class HelmRepoAdd(RegionTenantHeaderView):
         """
         command = request.data.get("command")
 
-        repo_name, repo_url, username, password = helm_app_service.parse_cmd_add_repo(command)
+        repo_name, repo_url, username, password,status = helm_app_service.parse_cmd_add_repo(command)
         data = {
+            "status":status,
             "repo_name":repo_name,
             "repo_url":repo_url,
             "username":username,
