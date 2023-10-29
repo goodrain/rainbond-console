1a71594c1e1e1fe8f21675d3a0f7a3c49814aa2b
banghao <46078945+zxwsclcmxbh@users.noreply.github.com>
2023-10-29 19:14:55 +0800

    Update app_build.py

--- console/views/app_create/app_build.py
+++ console/views/app_create/app_build.py
@@ -6,19 +6,15 @@ import logging
 
 from console.cloud.services import check_memory_quota
 from console.exception.bcode import ErrComponentBuildFailed
-from console.exception.main import (ErrInsufficientResource,
-                                    ServiceHandleException)
+from console.exception.main import (ErrInsufficientResource, ServiceHandleException)
 from console.repositories.deploy_repo import deploy_repo
 from console.services.app import app_service
 from console.services.app_actions import app_manage_service, event_service
-from console.services.app_config import (dependency_service, env_var_service,
-                                         port_service, probe_service,
-                                         volume_service)
+from console.services.app_config import (dependency_service, env_var_service, port_service, probe_service, volume_service)
 from console.services.app_config.arch_service import arch_service
 from console.services.compose_service import compose_service
 from console.views.app_config.base import AppBaseView
-from console.views.base import (CloudEnterpriseCenterView,
-                                RegionTenantHeaderCloudEnterpriseCenterView)
+from console.views.base import (CloudEnterpriseCenterView, RegionTenantHeaderCloudEnterpriseCenterView)
 from django.db import transaction
 from django.views.decorators.cache import never_cache
 from rest_framework.response import Response
@@ -33,7 +29,6 @@ logger = logging.getLogger("default")
 
 
 class AppBuild(AppBaseView, CloudEnterpriseCenterView):
-
     @never_cache
     @transaction.atomic
     def post(self, request, *args, **kwargs):
@@ -58,20 +53,16 @@ class AppBuild(AppBaseView, CloudEnterpriseCenterView):
         # 获取需要被调度的边缘节点
         edge_node = request.data.get("edge_node", None)
         try:
-            if not check_memory_quota(
-                    self.oauth_instance, self.tenant.enterprise_id,
-                    self.service.min_memory, self.service.min_node):
-                raise ServiceHandleException(msg="not enough quota",
-                                             error_code=20002)
+            if not check_memory_quota(self.oauth_instance, self.tenant.enterprise_id, self.service.min_memory,
+                                      self.service.min_node):
+                raise ServiceHandleException(msg="not enough quota", error_code=20002)
             if self.service.service_source == "third_party":
                 is_deploy = False
                 # create third component from region
-                new_service = app_service.create_third_party_service(
-                    self.tenant, self.service, self.user.nick_name)
+                new_service = app_service.create_third_party_service(self.tenant, self.service, self.user.nick_name)
             else:
                 # 数据中心创建组件
-                new_service = app_service.create_region_service(
-                    self.tenant, self.service, self.user.nick_name)
+                new_service = app_service.create_region_service(self.tenant, self.service, self.user.nick_name)
 
             self.service = new_service
             if is_deploy:
@@ -125,42 +116,34 @@ class AppBuild(AppBaseView, CloudEnterpriseCenterView):
                     result = general_message(err.error_code, e, err.msg_show)
                     return Response(result, status=400)
                 # 添加组件部署关系
-                deploy_repo.create_deploy_relation_by_service_id(
-                    service_id=self.service.service_id)
+                deploy_repo.create_deploy_relation_by_service_id(service_id=self.service.service_id)
 
             result = general_message(200, "success", "构建成功")
             return Response(result, status=result["code"])
         except HttpClient.CallApiError as e:
             logger.exception(e)
             if e.status == 403:
-                result = general_message(10407, "no cloud permission",
-                                         e.message)
+                result = general_message(10407, "no cloud permission", e.message)
                 status = e.status
             elif e.status == 400:
                 if "is exist" in e.message.get("body", ""):
-                    result = general_message(400,
-                                             "the service is exist in region",
-                                             "该组件在数据中心已存在，你可能重复创建？")
+                    result = general_message(400, "the service is exist in region", "该组件在数据中心已存在，你可能重复创建？")
                 else:
-                    result = general_message(400, "call cloud api failure",
-                                             e.message)
+                    result = general_message(400, "call cloud api failure", e.message)
                 status = e.status
             else:
-                result = general_message(400, "call cloud api failure",
-                                         e.message)
+                result = general_message(400, "call cloud api failure", e.message)
                 status = 400
         # 删除probe
         # 删除region端数据
         if probe:
-            probe_service.delete_service_probe(self.tenant, self.service,
-                                               probe.probe_id)
+            probe_service.delete_service_probe(self.tenant, self.service, probe.probe_id)
         if self.service.service_source != "third_party":
             event_service.delete_service_events(self.service)
             port_service.delete_region_port(self.tenant, self.service)
             volume_service.delete_region_volumes(self.tenant, self.service)
             env_var_service.delete_region_env(self.tenant, self.service)
-            dependency_service.delete_region_dependency(
-                self.tenant, self.service)
+            dependency_service.delete_region_dependency(self.tenant, self.service)
             app_manage_service.delete_region_service(self.tenant, self.service)
         self.service.create_status = "checked"
         self.service.save()
@@ -178,7 +161,6 @@ class AppBuild(AppBaseView, CloudEnterpriseCenterView):
 
 
 class ComposeBuildView(RegionTenantHeaderCloudEnterpriseCenterView):
-
     @never_cache
     def post(self, request, *args, **kwargs):
         """
@@ -207,26 +189,19 @@ class ComposeBuildView(RegionTenantHeaderCloudEnterpriseCenterView):
         try:
             compose_id = request.data.get("compose_id", None)
             if not compose_id:
-                return Response(general_message(400, "params error", "参数异常"),
-                                status=400)
-            group_compose = compose_service.get_group_compose_by_compose_id(
-                compose_id)
+                return Response(general_message(400, "params error", "参数异常"), status=400)
+            group_compose = compose_service.get_group_compose_by_compose_id(compose_id)
             services = compose_service.get_compose_services(compose_id)
             # 数据中心创建组件
             new_app_list = []
             for service in services:
-                new_service = app_service.create_region_service(
-                    self.tenant, service, self.user.nick_name)
+                new_service = app_service.create_region_service(self.tenant, service, self.user.nick_name)
                 new_app_list.append(new_service)
             group_compose.create_status = "complete"
             group_compose.save()
             for s in new_app_list:
                 try:
-                    app_manage_service.deploy(
-                        self.tenant,
-                        s,
-                        self.user,
-                        oauth_instance=self.oauth_instance)
+                    app_manage_service.deploy(self.tenant, s, self.user, oauth_instance=self.oauth_instance)
                 except ErrInsufficientResource as e:
                     result = general_message(e.error_code, e.msg, e.msg_show)
                     return Response(result, status=e.status_code)
@@ -242,16 +217,13 @@ class ComposeBuildView(RegionTenantHeaderCloudEnterpriseCenterView):
                 for service in services:
                     if probe_map:
                         probe_id = probe_map.get(service.service_id)
-                        probe_service.delete_service_probe(
-                            self.tenant, service, probe_id)
+                        probe_service.delete_service_probe(self.tenant, service, probe_id)
                     event_service.delete_service_events(service)
                     port_service.delete_region_port(self.tenant, service)
                     volume_service.delete_region_volumes(self.tenant, service)
                     env_var_service.delete_region_env(self.tenant, service)
-                    dependency_service.delete_region_dependency(
-                        self.tenant, service)
-                    app_manage_service.delete_region_service(
-                        self.tenant, service)
+                    dependency_service.delete_region_dependency(self.tenant, service)
+                    app_manage_service.delete_region_service(self.tenant, service)
                     service.create_status = "checked"
                     service.save()
             raise e
