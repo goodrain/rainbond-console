5b40e8278b4cf4855ba62d1c570f1646dfa6630d
逆流而上 <1666888816@qq.com>
2023-10-30 17:08:03 +0800

    feat: openapi add ports list

--- openapi/urls.py
+++ openapi/urls.py
@@ -34,7 +34,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
