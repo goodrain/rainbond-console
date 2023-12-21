a93f769a72e0dfcfbce4f75ac8995db920e0d906
逆流而上 <1666888816@qq.com>
2023-12-21 17:29:32 +0800

    fix: enable oauth

--- console/services/config_service.py
+++ console/services/config_service.py
@@ -125,10 +125,11 @@ class ConfigService(object):
 
     def update_config_enable_status(self, key, enable):
         self.init_base_config_value()
-        config = ConsoleSysConfig.objects.get(key=key, enterprise_id=self.enterprise_id)
-        if config.enable != enable:
-            config.enable = enable
-            config.save()
+        configs = ConsoleSysConfig.objects.filter(key=key)
+        for config in configs:
+            if config.enable != enable:
+                config.enable = enable
+                config.save()
         if key in self.base_cfg_keys:
             return {key.lower(): {"enable": enable, "value": self.base_cfg_keys_value[key]["value"]}}
         return {key.lower(): {"enable": enable, "value": (eval(config.value) if config.type == "json" else config.value)}}
