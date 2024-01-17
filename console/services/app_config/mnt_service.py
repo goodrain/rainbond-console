65d9c53704694370f1e2a59798b67a30ece73aaa
逆流而上 <1666888816@qq.com>
2024-01-17 11:35:00 +0800

    :wq

--- console/services/app_config/mnt_service.py
+++ console/services/app_config/mnt_service.py
@@ -99,8 +99,8 @@ class AppMntService(object):
             gs_rel = group_service_relation_repo.get_group_by_service_id(volume.service_id)
             group = group_repo.get_group_by_pk(tenant.tenant_id, service.service_region, gs_rel.group_id)
             group_name = group.group_name if group else '未分组'
-            if (dep_app_group == "" or dep_app_group == group_name) and (config_name == ""
-                                                                         or config_name == volume.volume_name):
+            if (dep_app_group == "" or dep_app_group == group_name) and (config_name == "" or config_name in volume.volume_name
+                                                                         or config_name in volume.volume_path):
                 un_mount_dependencies.append({
                     "dep_app_name": services.get(service_id=volume.service_id).service_cname,
                     "dep_app_group": group_name,
