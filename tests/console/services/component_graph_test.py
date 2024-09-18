4da480a44a952d40fe95c13aff4ad89c90542de6
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2024-09-18 14:18:07 +0800

    perf: change commit
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- tests/console/services/component_graph_test.py
+++
@@ -1,9 +0,0 @@
-from console.services.app_config.component_graph import ComponentGraphService
-
-
-def test_add_or_update_label():
-    component_graph_service = ComponentGraphService()
-    promsql = component_graph_service.add_or_update_label(
-        "foobar", "container_memory_rss{name=~\"k8s_1aa695008a85f6aacf3b9ed6342279c5.*\"}/1024/1024")
-    want = "container_memory_rss{name=~\"k8s_1aa695008a85f6aacf3b9ed6342279c5.*\",service_id=\"foobar\"} / 1024 / 1024"
-    assert promsql == want
--- tests/helper.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.test import Client
-
-
-def get_client():
-    return Client(HTTP_AUTHORIZATION='6923642f067b3cc8e4b7f2194cb25917')
