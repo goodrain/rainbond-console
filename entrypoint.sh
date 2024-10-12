02c144ee9e97bf38843725caf89e926bd6bffb19
Qi Zhang <smallqi1@163.com>
2024-10-12 18:52:43 +0800

    fix: rbd-app-ui auto init
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,84 +4,76 @@ RED='\033[0;31m'
 GREEN='\033[32;1m'
 NC='\033[0m' # No Color
 
-function database_ready() {
-    if [ "${DB_TYPE}" == "mysql" ]; then
-        if mysql -h${MYSQL_HOST:-127.0.0.1} -P${MYSQL_PORT:-3306} -u${MYSQL_USER} -p${MYSQL_PASS} -e "use ${MYSQL_DB};" >/dev/null; then
-            return 0 # 0 = true
-        fi
-        return 1 # 1 = false
+function database_empty() {
+  # Check if database is empty
+  if [ "${DB_TYPE}" == "mysql" ]; then
+    tables=$(mysql -h"${MYSQL_HOST:-127.0.0.1}" -P"${MYSQL_PORT:-3306}" -u"${MYSQL_USER}" -p"${MYSQL_PASS}" -D"${MYSQL_DB}" -e "SHOW TABLES;" 2>/dev/null | wc -l)
+    if [ "$tables" -le 1 ]; then
+      return 0 # Database is empty
     fi
-    sqlite3 /app/data/db.sqlite3 <<EOF
-PRAGMA journal_mode = WAL;
-EOF
-    return 0
+    return 1 # Database is not empty
+  else
+    tables=$(sqlite3 /app/data/db.sqlite3 "SELECT count(*) FROM sqlite_master WHERE type='table';")
+    if [ "$tables" -eq 0 ]; then
+      return 0 # Database is empty
+    fi
+    return 1 # Database is not empty
+  fi
 }
 
 function init_database() {
-    for i in {1..4}; do
-        if ! (database_ready); then
-            echo -e "${RED}Database not ready, will waiting${NC}"
-            sleep 3
-        else
-            break
-        fi
+  # Wait for database to be ready
+  if [ "${DB_TYPE}" == "mysql" ]; then
+    while true; do
+      if mysql -h"${MYSQL_HOST:-127.0.0.1}" -P"${MYSQL_PORT:-3306}" -u"${MYSQL_USER}" -p"${MYSQL_PASS}" -e "use ${MYSQL_DB};" >/dev/null; then
+        break
+      else
+        echo -e "${RED}ERROR: Database not ready, will waiting${NC}"
+        sleep 3
+      fi
     done
-    if ! (database_ready); then
-        echo -e "${RED}Database not ready, will exit.${NC}"
-        return 1
-    fi
+  else
+    sqlite3 /app/data/db.sqlite3 "PRAGMA journal_mode = WAL;"
+  fi
 
-    echo -e "${GREEN}Start initializing database${NC}"
-    if ! (python manage.py makemigrations www 2>/dev/null); then
-        echo -e "${RED}failed to makemigrations www${NC}"
-        return 1
-    fi
-    if ! (python manage.py makemigrations console 2>/dev/null); then
-        echo -e "${RED}failed to makemigrations console${NC}"
-        return 1
-    fi
-    if ! (python manage.py migrate >/dev/null); then
-        echo -e "${RED}failed to migrate${NC}"
-        return 1
-    fi
-    echo -e "${GREEN}Database initialization completed${NC}"
-    return 0
-}
-
-use_sqlite() {
-    # shellcheck disable=SC1035
-    if !(python default_region_sqlite.py 2> /dev/null); then
-      echo -e "${RED}failed to default_region${NC}"
+  # Initialize database schema
+  echo -e "${GREEN}INFO: Start initializing database${NC}"
+  if ! (python manage.py makemigrations www 2>/dev/null); then
+    echo -e "${RED}ERROR: failed to makemigrations www${NC}"
+    exit 1
+  fi
+  if ! (python manage.py makemigrations console 2>/dev/null); then
+    echo -e "${RED}ERROR: failed to makemigrations console${NC}"
+    exit 1
+  fi
+  if ! (python manage.py migrate >/dev/null); then
+    echo -e "${RED}ERROR: failed to migrate${NC}"
+    exit 1
+  fi
+  echo -e "${GREEN}INFO: Database initialization completed${NC}"
+  
+  # Initialize default region data
+  if [ "${DB_TYPE}" == "mysql" ]; then
+    if ! (python default_region.py 2> /dev/null); then
+      echo -e "${RED}ERROR: failed to default_region${NC}"
       exit 1
     fi
-}
-
-use_mysql() {
-    if !(python default_region.py 2> /dev/null); then
-      echo -e "${RED}failed to default_region${NC}"
+  else
+    if ! (python default_region_sqlite.py 2> /dev/null); then
+      echo -e "${RED}ERROR: failed to default_region${NC}"
       exit 1
     fi
+  fi
 }
 
 if [ "$1" = "debug" -o "$1" = "bash" ]; then
-    exec /bin/bash
+  exec /bin/bash
 elif [ "$1" = "version" ]; then
-    echo "${RELEASE_DESC}"
-elif [ "$1" = "init" ]; then
-    if ! (init_database); then
-      exit 1
-    fi
-    if [ "${INSTALL_TYPE}" != "allinone" ]; then
-      if [ "$DB_TYPE" != "mysql" ]; then
-        use_sqlite
-      else
-        use_mysql
-      fi
-    fi
+  echo "${RELEASE_DESC}"
 else
-    if ! (init_database); then
-      exit 1
-    fi
-    # python upgrade.py
-    exec gunicorn goodrain_web.wsgi -b 0.0.0.0:${PORT:-7070} --max-requests=5000 -k gevent --reload --workers=2 --timeout=75 --log-file - --access-logfile - --error-logfile -
+  if (database_empty); then
+    init_database
+  fi
+  # python upgrade.py
+  exec gunicorn goodrain_web.wsgi -b 0.0.0.0:${PORT:-7070} --max-requests=5000 -k gevent --reload --workers=2 --timeout=75 --log-file - --access-logfile - --error-logfile -
 fi
