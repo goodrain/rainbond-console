0079599786e1315281383e0c43301dc408fe15df
Qi Zhang <smallqi1@163.com>
2024-11-15 15:10:10 +0800

    fix: remove old version of sql
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- sql/5.1.9-5.1.10.sql
+++
@@ -1,2 +0,0 @@
-
-update console.console_sys_config set `value`="5.1.10" where `key`="RAINBOND_VERSION";
--- sql/5.3.2-5.3.3.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE app_service_extend_method ADD COLUMN `container_cpu` int DEFAULT 0;
-ALTER TABLE tenant_service_volume ADD COLUMN `mode` int;
