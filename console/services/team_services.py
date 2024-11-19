d84cbc0e5ff92553ed80952cf6517fc27ceb2e92
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2024-11-19 14:24:40 +0800

    fix: sum tenant app failure
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- console/services/team_services.py
+++ console/services/team_services.py
@@ -382,7 +382,7 @@ class TeamService(object):
                 tenants.get(tenant_id)["set_limit_memory"] = region_tenant.get("LimitMemory", 0)
                 tenants.get(tenant_id)["set_limit_cpu"] = region_tenant.get("LimitCPU", 0)
                 tenants.get(tenant_id)["set_limit_storage"] = region_tenant.get("LimitStorage", 0)
-                tenants.get(tenant_id)["running_apps"] = running_apps + region_tenant.get("running_applications", 0),
+                tenants.get(tenant_id)["running_apps"] = running_apps + region_tenant.get("running_applications", 0)
                 tenants.get(tenant_id)["memory_request"] = region_tenant.get("memory_request", 0)
                 tenants.get(tenant_id)["cpu_request"] = region_tenant.get("cpu_request", 0)
         return tenants.values()
