d1d665b06a646ddd67a20a993a084f5133084553
Qi Zhang <smallqi1@163.com>
2024-12-31 21:37:03 +0800

    fix: user_id create failure (#1595)
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>
    Co-authored-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- console/models/main.py
+++ console/models/main.py
@@ -977,7 +977,7 @@ class OAuthServices(BaseModel):
     is_console = models.NullBooleanField(null=True, default=False, help_text="is_console")
     is_auto_login = models.NullBooleanField(null=True, default=False, help_text="is_auto_login")
     is_git = models.BooleanField(default=True, help_text="是否为git仓库")
-    user_id = models.IntegerField(max_length=10, help_text="用户ID", default=0)
+    user_id = models.IntegerField(help_text="用户ID", default=0)
     system = models.BooleanField(default=False, help_text="是否后台创建")
 
 
@@ -1124,7 +1124,7 @@ class TeamRegistryAuth(BaseModel):
     password = models.CharField(max_length=255, help_text="password")
     region_name = models.CharField(max_length=255, help_text="region_name")
     hub_type = models.CharField(max_length=32, help_text="hub type", default="Docker")
-    user_id = models.IntegerField(max_length=10, help_text="用户ID", default=0)
+    user_id = models.IntegerField(help_text="用户ID", default=0)
 
 
 class RKECluster(BaseModel):
