4cdc17bcc87900ca33fe69cf9dbfd2841c0b31a7
Qi Zhang <smallqi1@163.com>
2026-08-10 13:11:59 +0800

    fix: throttle login activity updates
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- console/repositories/login_event.py
+++ console/repositories/login_event.py
@@ -7,10 +7,7 @@ from console.models.main import LoginEvent
 class LoginEventRepo(object):
     @staticmethod
     def get_last_one(enterprise_id: str, username: str) -> Optional[LoginEvent]:
-        events = LoginEvent.objects.filter(enterprise_id=enterprise_id, username=username).order_by("-ID")
-        if not events:
-            return None
-        return events[0]
+        return LoginEvent.objects.filter(enterprise_id=enterprise_id, username=username).order_by("-ID").first()
 
     @staticmethod
     def create(**data: Any) -> None:
